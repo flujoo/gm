d57c456b2d7303aedd5d0ff4e81f4d9465c25b1f
flujoo <tnerrotana@gmail.com>
2020-07-15 21:55:40 +0400

    Follow MusicXML type values
    https://usermanuals.musicxml.com/MusicXML/Content/ST-MusicXML-note-type-value.htm

--- R/duration.R
+++ R/duration.R
@@ -27,7 +27,7 @@ split.duration_notation <- function(duration_notation) {
 
 
 duration_types <- c(
-  "maxima", "long", "breve", "whole", "half", "quarter", "8th",
+  "maxima", "long", "breve", "whole", "half", "quarter", "eighth",
   "16th", "32nd", "64th", "128th", "256th", "512th", "1024th"
 )
 
