aa4276f9a726e45af96de407fdf868eeea1b4164
flujoo <tnerrotana@gmail.com>
2020-07-24 11:11:47 +0400

    Re-design module "duration.R"

--- R/duration.R
+++ R/duration.R
@@ -1,28 +1,65 @@
+#' @title Duration Types
+#' @description Values to MusicXML "type" element.
+#' @details See \url{https://usermanuals.musicxml.com/MusicXML/
+#' Content/EL-MusicXML-type.htm}.
 duration_types <- c(
   "maxima", "long", "breve", "whole", "half", "quarter", "eighth",
   "16th", "32nd", "64th", "128th", "256th", "512th", "1024th"
 )
 
 
-validate.duration_notations <- function(duration_notations) {
-  reg_type <- paste0("(", paste(duration_types, collapse = "|"), ")")
+#' @title Convert Duration Type to Value
+#' @param duration_type A character.
+#' @return A numeric.
+to_value.duration_type <- function(duration_type) {
+  v_quarter = 1
+  i_quarter <- which(duration_types == "quarter")
+  i_type <- which(duration_types == duration_type)
+  v_quarter * 2^(i_quarter - i_type)
+}
+
+
+#' @title Convert Dot to Value
+#' @param dot A numeric between 0 and 4, or a character consisting of
+#' 0 to 4 periods.
+#' @return A numeric.
+to_value.dot <- function(dot) {
+  if (is.character(dot)) {
+    dot <- nchar(dot)
+  }
+  sum(2^(-(0:dot)))
+}
+
+
+#' @title Validate Duration Notations
+#' @param duration_notations A character vector of duration notations.
+#' @param dot,tuplet,slur Bools indicating if duration notations can
+#' contain dot part, tuplet part, and slur part respectively.
+#' @return A logical vector.
+validate.duration_notations <- function(
+  duration_notations, dot = TRUE, tuplet = TRUE, slur = TRUE) {
   reg <- paste0(
     "^",
     # always starts with a type
-    reg_type,
+    paste0("(", paste(duration_types, collapse = "|"), ")"),
     # maybe followed by a dot block
-    "(\\.{1,4})?",
-    # followed by 0-n tuplet operators
-    "(/[1-9][0-9]*)*",
+    ifelse(dot, "(\\.{1,4})?", ""),
+    # followed by 0-n tuplet notations
+    ifelse(tuplet, "(/([2-9]|[1-9][0-9]+))*", ""),
     # maybe followed by a slur
-    "-?",
+    ifelse(slur, "-?", ""),
     "$"
   )
   grepl(reg, duration_notations)
 }
 
 
-split.duration_notation <- function(duration_notation) {
+#' @title Analyze Duration Notation
+#' @description Split a duration notation into four parts representing
+#' type, dot, tuplet(s) and slur.
+#' @param duration_notation A character.
+#' @return A list.
+analyze.duration_notation <- function(duration_notation) {
   core <- function(reg) {
     ks <- gregexpr(reg, duration_notation)[[1]]
     out <- c()
@@ -40,114 +77,214 @@ split.duration_notation <- function(duration_notation) {
   list(
     type = core(paste(duration_types, collapse = "|")),
     dot = core("\\.{1,4}"),
-    tuplet = core("/[1-9][0-9]*"),
+    ns = as.numeric(sapply(
+      strsplit(core("/[1-9][0-9]*"), "/"),
+      function(x) x[2]
+    )),
     slur = core("-")
   )
 }
 
 
-to_value.duration_types <- function(quarter = 1, as_fraction = TRUE) {
-  l <- length(duration_types)
-  vs <- rep(0, l)
-  names(vs) <- duration_types
-
-  i_q <- which(duration_types == "quarter")
-  vs[i_q] <- quarter
-  vs[(i_q - 1):1] <- quarter * 2^(1:(i_q - 1))
-  vs[(i_q + 1):l] <- quarter / 2^(1:(l - i_q))
-
-  if (as_fraction) {
-    MASS::fractions(vs)
-  } else {
-    vs
-  }
+#' @title Infer Tuplet's Type
+divide_type <- function(type, n) {
+  i_type <- which(duration_types == type)
+  d <- floor(log2(n))
+  i <- i_type + d
+  duration_types[i]
 }
 
 
-to_value.duration_notation <- function(duration_notation,
-                                       quarter = 1, as_fraction = TRUE) {
-  dn <- split.duration_notation(duration_notation)
-  vs <- to_value.duration_types(quarter, as_fraction)
-
-  type <- unname(vs[dn$type])
-
-  dot <- dn$dot
-  dot[dot == ""] <- 1
-  dot[dot == "."] <- 1.5
-  dot[dot == ".."] <- 1.75
-  dot[dot == "..."] <- 1.875
-  dot[dot == "...."] <- 1.9375
-  dot <- as.numeric(dot)
-
-  tuplet <- dn$tuplet
-  tuplet[tuplet == ""] <- 1
-  tuplet <- as.numeric(tuplet)
-
-  sum(type * dot / tuplet)
+#' @title Validate Duration of Argument \code{take} in Function \code{Tuplet}
+#' @description Used in function \code{Tuplet}.
+validate.take <- function(n, unit_type, unit_dot, take_type, take_dot) {
+  v_take <- to_value.duration_type(take_type) * to_value.dot(take_dot)
+  v_unit <- to_value.duration_type(unit_type) * to_value.dot(unit_dot)
+  v_take / v_unit <= n
 }
 
 
-#' @title Create Duration Object
-#'
-#' @description Create an object of S3 class "Duration",
-#' which is to represent the durational aspect of music.
-#'
+#' @title Create Tuplet Object
+#' @description Used in function \code{DurationNote} to specify a complex
+#' tuplet.
+#' @param n A numeric which is an integer larger than 1.
+#' @param unit A character representing a duration notation indicating
+#' the duration of the basic unit of the tuplet.
+#' @param take A character representing a duration notation indicating
+#' how many units the tuplet lasts.
+#' @return A list with \code{"n"}, \code{"unit"} and \code{"take"} as names,
+#' whose class is \code{"Tuplet"}.
 #' @export
-Duration <- function(object) {
-
-  l <- length(object)
-  # error message
-  m <- "invalid input to Duration"
-
-  if (is.character(object) && l == 1 &&
-      validate.duration_notation(object)) {
-    class(object) <- c("DurationNote", "Duration")
-    return(object)
+Tuplet <- function(n, unit, take) {
+  v_n <- n > 1 && as.integer(n) == n
+  if (!v_n) {
+    stop('argument "n" should be an integer larger than 1')
   }
 
-  if (is.list(object) && l > 0) {
+  v_unit <- is.character(unit) &&
+    length(unit) == 1 &&
+    validate.duration_notations(unit, tuplet = FALSE, slur = FALSE)
+  if (!v_unit) {
+    m <- paste(
+      'argument "unit" should be a character starting with',
+      "a duration type followed by 0-4 dots"
+    )
+    stop(m)
+  }
 
-    if (all(sapply(object, is.character))) {
-      for (i in 1:l) {
-        object[[i]] <- Duration(object[[i]])
-      }
-      class(object) <- c("DurationVoice", "Duration")
-      return(object)
+  v_take <- is.character(take) &&
+    length(take) == 1 &&
+    validate.duration_notations(take, tuplet = FALSE, slur = FALSE)
+  if (!v_take) {
+    m <- paste(
+      'argument "take" should be a character starting with',
+      "a duration type followed by 0-4 dots"
+    )
+    stop(m)
+  }
+
+  unit <- analyze.duration_notation(unit)
+  unit_type <- unit$type
+  unit_dot <- unit$dot
+
+  take <- analyze.duration_notation(take)
+  take_type <- take$type
+  take_dot <- take$dot
+
+  if (!validate.take(n, unit_type, unit_dot, take_type, take_dot)) {
+    stop('the duration of "take" is too long')
+  }
+
+  t_ <- list(
+    n = n,
+    unit = c(unit_type, unit_dot),
+    take = c(take_type, take_dot)
+  )
+  class(t_) <- "Tuplet"
+  t_
+}
 
 
-    } else if (all(sapply(object, is.list))) {
-      for (i in 1:l) {
-        o <- object[[i]]
-        if (all(sapply(o, is.character))) {
-          object[[i]] <- Duration(o)
-        } else {
-          stop(m, call. = FALSE)
+#' @title Generate Tuplets from Numeric Vector
+to_Tuplets.ns <- function(type, dot, ns) {
+  t_ <- list()
+  for (i in 1:length(ns)) {
+    n <- ns[i]
+    type <- divide_type(type, n)
+    unit <- c(type, dot)
+    t_i <- list(n = n, unit = unit, take = unit)
+    class(t_i) <- "Tuplet"
+    t_[[i]] <- t_i
+  }
+  t_
+}
+
+
+#' @title Validate \code{unit} Part of Tuplet
+validate.unit <- function(type, dot, unit_type, unit_dot) {
+  i_type <- which(duration_types == type)
+  i_unit_type <- which(duration_types == unit_type)
+
+  con <- unit_dot == dot && i_unit_type >= i_type
+  if (dot == ".") {
+    # 3/2
+    con <- con ||
+      unit_dot == "" && i_unit_type > i_type
+  } else if (dot == "..") {
+    # 7/4
+    con <- con ||
+      unit_dot == "" && i_unit_type > (i_type + 1)
+  } else if (dot == "...") {
+    # 15/8
+    con <- con ||
+      unit_dot == "." && i_unit_type > (i_type + 1)
+  } else if (dot == "....") {
+    # 31/16
+    con <- con ||
+      unit_dot == "" && i_unit_type > (i_type + 3)
+  }
+
+  con
+}
+
+
+#' @title Validate List of Dependent Tuplets
+validate.Tuplets <- function(type, dot, Tuplets) {
+  l <- length(Tuplets)
+  vs <- logical(l)
+  for (i in 1:l) {
+    to <- Tuplets[[i]]
+    unit <- to$unit
+    unit_type <- unit[1]
+    unit_dot <- unit[2]
+    v <- validate.unit(type, dot, unit_type, unit_dot)
+    vs[i] <- v
+    take <- to$take
+    type <- take[1]
+    dot <- take[2]
+  }
+  vs
+}
+
+
+#' @title Create DurationNote Object
+#' @description DurationNote is to represent the durational aspect of
+#' musical note.
+#' @param duration_notation A character representing a duration notation.
+#' @param ... 0 or more Tuplet objects.
+#' @return A list with \code{"type"}, \code{"dot"}, \code{"slur"} and
+#' \code{"tuplets"} as names, whose class is \code{"DurationNote"}.
+#' @export
+DurationNote <- function(duration_notation, ...) {
+
+  # validity of duration_notation
+  v_dn <- is.character(duration_notation) &&
+    length(duration_notation) == 1 &&
+    validate.duration_notations(duration_notation)
+
+  if (v_dn) {
+    dn <- analyze.duration_notation(duration_notation)
+
+    ns <- dn$ns
+    type <- dn$type
+    dot <- dn$dot
+
+    if (!is.na(ns[1])) {
+      # tuplet notations
+      tns <- to_Tuplets.ns(type, dot, ns)
+      take <- tns[[length(tns)]]$take
+      type <- take[1]
+      dot <- take[2]
+    } else {
+      tns <- list()
+    }
+
+    # tupleTupleterators
+    tos <- list(...)
+    if (length(tos)) {
+      vs <- validate.Tuplets(type, dot, tos)
+
+      if (!all(vs)) {
+        pos <- which(vs == FALSE)
+        l <- length(pos)
+        if (l == 1) {
+          m <- paste("invalid tuplet operator at position", pos)
+        } else if (l > 1) {
+          m <- paste(
+            "invalid tuplet operators at positions",
+            paste(pos, collapse = ", ")
+          )
         }
+        stop(m)
       }
-      class(object) <- c("DurationVoices", "Duration")
-      return(object)
     }
+
+    ts_ <- append(tns, tos)
+    dn$ns <- NULL
+    dn$tuplets <- ts_
+    return(dn)
+
+  } else {
+    stop("invalid duration notation")
   }
-
-  stop(m, call. = FALSE)
-}
-
-
-#' @export
-print.Duration <- function(x, ...) {
-
-  if (is.atomic(x)) {
-    s <- paste0(x, "\n")
-
-  } else if (is.list(x)) {
-    if (all(sapply(x, is.atomic))) {
-      s <- to_string.vector(x, c("[", "]"))
-
-    } else if (all(sapply(x, is.list))) {
-      s <- to_string.list(x)
-    }
-  }
-
-  cat(s)
-  invisible(s)
 }
