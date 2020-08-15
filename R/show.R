1da0e773d1046faf05906d2c0ded9f181506f9a2
flujoo <tnerrotana@gmail.com>
2020-08-15 11:35:49 +0400

    Add "to_Duration.value"

--- R/show.R
+++ R/show.R
@@ -12,3 +12,26 @@ preface.MusicXML <- function(musicxml) {
   class(m) <- "MusicXML"
   m
 }
+
+
+#' @title Convert Value to Duration
+#' @details Only apply to non-tuplets.
+to_Duration.value <- function(value) {
+  # values of all types
+  vs_type <- sapply(duration_types, to_value.duration_type)
+  # values of 0-4 dots
+  vs_dot <- sapply(0:4, to_value.dot)
+  # "undot" the given value
+  vs_undot <- value / vs_dot
+  # infer the number of dots
+  n_dot <- which(vs_undot %in% vs_type) - 1
+  # n -> dot notation
+  dot <- paste(rep(".", n_dot), collapse = "")
+  # infer the type
+  i_type <- which(vs_type == value / vs_dot[n_dot + 1])
+  type <- duration_types[i_type]
+
+  d <- list(type = type, dot = dot, tie = "", tuplets = list())
+  class(d) <- "Duration"
+  d
+}
