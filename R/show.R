1983e6d05bf51a2694eba770029ca1793ef41a43
flujoo <tnerrotana@gmail.com>
2020-08-18 23:00:01 +0400

    Add functions for splitting duration value

--- R/show.R
+++ R/show.R
@@ -33,3 +33,48 @@ to_type_dot.value <- function(value) {
 
   list(type = type, dot = dot)
 }
+
+
+#' @title Partition Duration Value
+#' @description Partition a duration value into many units.
+partition.value <- function(value, unit) {
+  ratio <- value / unit
+  ratio_l <- floor(ratio)
+  if (ratio == ratio_l) {
+    v <- rep(unit, ratio)
+  } else {
+    d <- ratio - ratio_l
+    v <- c(unit * d, rep(unit, ratio_l))
+  }
+  v
+}
+
+
+#' @title Untie Duration Value
+#' @details Only apply to non-tuplets.
+untie.value <- function(value) {
+  # sort duration types in an ascending order
+  ts_ <- rev(duration_types)
+  # convert duration types and their dotted durations to values
+  vs <- unlist(lapply(ts_,
+    # convert a duration type and its dotted durations
+    function(t_) {
+      to_value.duration_type(t_) *
+        sapply(0:4, to_value.dot)
+    }
+  ))
+
+  # recursively
+  core <- function(value) {
+    if (value %in% vs) {
+      return(value)
+    } else {
+      is_ <- which(vs < value)
+      i <- is_[length(is_)]
+      v_i <- vs[i]
+      return(c(v_i, core(value - v_i)))
+    }
+  }
+
+  core(value)
+}
