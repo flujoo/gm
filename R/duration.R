7d78f959f1b528b0251c8730cc79d084c53b651f
flujoo <tnerrotana@gmail.com>
2020-08-28 22:25:35 +0400

    Add "to_type_dot.value"

--- R/duration.R
+++ R/duration.R
@@ -41,6 +41,27 @@ to_value.dot <- function(dot) {
 }
 
 
+#' @title Convert Value to Duration Type and Dot Notation
+#' @details Only applies to non-tuplets and untied durations.
+to_type_dot.value <- function(value) {
+  # values of all types
+  vs_type <- sapply(duration_types, to_value.duration_type)
+  # values of 0-4 dots
+  vs_dot <- sapply(0:4, to_value.dot)
+  # "undot" the given value
+  vs_undot <- value / vs_dot
+  # infer the number of dots
+  i_dot <- which(vs_undot %in% vs_type)
+  # n -> dot notation
+  dot <- paste(rep(".", i_dot - 1), collapse = "")
+  # infer the type
+  i_type <- which(vs_type == value / vs_dot[i_dot])
+  type <- duration_types[i_type]
+
+  list(type = type, dot = dot)
+}
+
+
 #' @title Validate Duration Notations
 #' @param duration_notations A character vector or a list of
 #' duration notations.
