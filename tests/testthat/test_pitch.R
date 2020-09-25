14ad2d80fc4ee02b4f9f0a1130317d694df59065
flujoo <tnerrotana@gmail.com>
2020-09-25 16:33:19 +0400

    Add tests for "PitchChord" and "PitchLine"

--- tests/testthat/test_pitch.R
+++ tests/testthat/test_pitch.R
@@ -17,3 +17,58 @@ test_that("midi -> Pitch", {
   # C#4 is the sharp 5th of key F
   expect_equal(to_Pitch.midi(midi, -1), c_)
 })
+
+
+test_that("PitchChord", {
+  # empty list is valid as input and will be converted to NA
+  expect_equal(PitchChord(list(), 0, NULL), NA)
+  # but empty lists as items are invalid
+  expect_error(PitchChord(list(list()), 0, NULL))
+  # valid items:
+  pc <- list(to_Pitch.midi(60), to_Pitch.midi(61))
+  class(pc) <- "PitchChord"
+  ps <- list(
+    # midis and pitch notations
+    "60", 60:61, "C4", c("C4", "C5"),
+    # lists as items, Pitches,
+    list(60, 61, to_Pitch.midi(60), pc)
+  )
+  out <- PitchChord(ps, 0, NULL)
+  expected <- list(
+    to_Pitch.midi(60), to_Pitch.midi(60), to_Pitch.midi(61),
+    to_Pitch.pitch_notation("C4"), to_Pitch.pitch_notation("C4"),
+    to_Pitch.pitch_notation("C5"), to_Pitch.midi(60),
+    to_Pitch.midi(61), to_Pitch.midi(60), to_Pitch.midi(60),
+    to_Pitch.midi(61)
+  )
+  class(expected) <- "PitchChord"
+  expect_equal(out, expected)
+})
+
+
+test_that("PitchLine", {
+  ps <- list(
+    # NAs
+    NULL, NaN, list(), NA_character_, NA,
+    # midis
+    "60", 61, 62:63,
+    # pitch notations
+    "C4", c("C5", "C6"),
+    # Pitches and PitchChords
+    to_Pitch.midi(70), PitchChord(71:72, 0, NULL),
+    # lists
+    list(90, 91, 92:93, list(94, list(95, 96))),
+    list(97)
+  )
+  out <- PitchLine(ps)
+  expected <- list(
+    NA, NA, NA, NA, NA,
+    to_Pitch.midi(60), to_Pitch.midi(61), PitchChord(62:63, 0, NULL),
+    to_Pitch.pitch_notation("C4"), PitchChord(c("C5", "C6"), 0, NULL),
+    to_Pitch.midi(70), PitchChord(71:72, 0, NULL),
+    PitchChord(90:96, 0, NULL),
+    to_Pitch.midi(97)
+  )
+  class(expected) <- "PitchLine"
+  expect_equal(out, expected)
+})
