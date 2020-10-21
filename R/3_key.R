4f1d0be90b3be022a22ab57c89453c8d3d6601d8
flujoo <tnerrotana@gmail.com>
2020-10-21 22:05:15 +0400

    Update 3_key.R

--- R/3_key.R
+++ R/3_key.R
@@ -3,6 +3,7 @@ validate.fifths <- function(fifths) {
 }
 
 
+#' @export
 Key <- function(fifths, position = 1) {
   if (!validate.fifths(fifths)) {
     stop('argument "fifths" is invalid')
@@ -11,6 +12,110 @@ Key <- function(fifths, position = 1) {
   position <- PositionLine(position, "note")
 
   k <- list(fifths = fifths, position = position)
-  class(k) <- c("Key", "Voice_")
+  class(k) <- "Key"
   k
+}
+
+
+KeyLine <- function(key, voice_length) {
+  k <- key$fifths
+  ps <- unclass(key$position)
+
+  for (i in 1:length(ps)) {
+    p <- ps[[i]]
+    # drop those beyond voice length
+    if (p <= voice_length) {
+      ps[[i]] <- c(p, k)
+    } else {
+      if (i == 1) {
+        ps <- list()
+      } else {
+        ps <- ps[1:(i - 1)]
+      }
+      break
+    }
+  }
+
+  class(ps) <- "KeyLine"
+  ps
+}
+
+
+to_string.KeyLine <- function(key_line) {
+  kl <- unclass(key_line)
+  l <- length(kl)
+  if (l == 0) {
+    return("")
+  }
+  for (i in 1:l) {
+    kl[[i]] <- paste0("(", paste(kl[[i]], collapse = ", "), ")")
+  }
+  paste(kl, collapse = ", ")
+}
+
+
+#' @export
+print.KeyLine <- function(key_line) {
+  s <- to_string.KeyLine(key_line)
+  cat(s, "\n")
+}
+
+
+merge.KeyLine <- function(line_1, line_2) {
+  i2s <- c()
+
+  # replace Key Points in line_1 with ones from line_2
+  # the order can not be changed
+  l1 <- length(line_1)
+  l2 <- length(line_2)
+  if (l1 > 0 && l2 > 0) {
+    # don't need to always start from the 1st
+    i <- 1
+    for (i2 in 1:l2) {
+      kp2 <- line_2[[i2]]
+      p2 <- kp2[1]
+      k2 <- kp2[2]
+      if (i > l1) {
+        break
+      }
+      for (i1 in i:l1) {
+        kp1 <- line_1[[i1]]
+        p1 <- kp1[1]
+        if (p1 == p2) {
+          i2s <- c(i2s, i2)
+          line_1[[i1]][2] <- k2
+          i <- i1 + 1
+          break
+        }
+      }
+    }
+  }
+
+  # and delete those from line_2
+  line_2[i2s] <- NULL
+
+  # merge two lines
+  l <- append(line_1, line_2)
+
+  # sort
+  js <- sapply(l, function(x) x[1])
+  kl <- l[order(js)]
+
+  class(kl) <- "KeyLine"
+  kl
+}
+
+
+#' @export
+add.Key <- function(key, voice) {
+  kl0 <- voice$key
+  kl1 <- KeyLine(key, length(voice$pitch))
+
+  if (is.null(kl0)) {
+    voice$key <- kl1
+  } else {
+    voice$key <- merge.KeyLine(kl0, kl1)
+  }
+
+  voice
 }
