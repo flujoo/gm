1884eb095ece22d5d1e7602d419d57360565de54
flujoo <tnerrotana@gmail.com>
2020-12-11 11:32:37 +0400

    Move code

---
+++ R/bar_add_on.R
@@ -0,0 +1,83 @@
+# Meter/KeyLine -----------------------------------------------------
+
+BarAddOnLine <- function(x, add_on_class) {
+  add_on_class %>%
+    paste0("Line") %>%
+    c("BarAddOnLine", "Printable") %>%
+    `class<-`(x, .)
+}
+
+
+#' @keywords internal
+#' @export
+to_string.BarAddOnLine <- function(x, ...) {
+  x %>%
+    sapply(function(m) paste0("(", m$bar, ", ", to_string(m), ")")) %>%
+    paste(collapse = ", ")
+}
+
+
+
+# + Meter/Key -------------------------------------------------------
+
+`+.BarAddOnLine` <- function(add_on_line, add_on) {
+  constructor <- function(add_on_line) {
+    add_on %>%
+      class() %>%
+      .[1] %>%
+      BarAddOnLine(add_on_line, .)
+  }
+
+  l <- length(add_on_line)
+
+  if (l == 0) {
+    add_on_line[[1]] <- add_on
+    add_on_line
+
+  } else {
+    b <- add_on$bar
+
+    for (i in 1:l) {
+      m <- add_on_line[[i]]
+      b_i <- m$bar
+
+      if (b_i > b) {
+        add_on_line <- add_on_line %>%
+          append(list(add_on), i - 1) %>%
+          constructor()
+        return(add_on_line)
+
+      } else if (b_i == b) {
+        add_on_line[[i]] <- add_on
+        return(add_on_line)
+
+      } else if (b_i < b) {
+        if (i == l) {
+          add_on_line <- add_on_line %>%
+            append(list(add_on)) %>%
+            constructor()
+          return(add_on_line)
+
+        } else {
+          next
+        }
+      }
+    }
+  }
+}
+
+
+add.BarAddOn <- function(term, music) {
+  name <- term %>%
+    class() %>%
+    .[1] %>%
+    tolower() %>%
+    paste0("s")
+
+  if (is.null(music[[name]])) {
+    music[[name]] <- BarAddOnLine(list(), class(term)[1])
+  }
+
+  music[[name]] <- music[[name]] + term
+  music
+}
--- R/line.R
+++ R/line.R
@@ -47,7 +47,7 @@ Line <- function(pitches, durations, name, as = "part", to = NULL,
 
 
 
-# validators --------------------------------------------------------
+# validators in `Line` ----------------------------------------------
 
 check_line_name <- function(name) {
   check_type(supplied = name, valid = "character", name = "name")
@@ -113,4 +113,140 @@ check_line_offset <- function(offset) {
     valid = expression(supplied == 0 || is_tied_value(supplied)),
     general = "`offset` must be 0, a duration value or sum of ones."
   )
+}
+
+
+
+# + Line ------------------------------------------------------------
+
+add.Line <- function(term, music) {
+  name <- term$name
+  to <- term$to
+  as <- term$as
+  after <- term$after
+  lns <- music$line_names
+  parts <- music$parts
+  l <- length(parts)
+
+  check_add_line_name(name, lns)
+  check_add_line_to(to, lns)
+
+  music$line_names <- c(lns, name)
+
+  # initialize `music$parts`
+  if (l == 0) {
+    music$parts <- list(list(list(term)))
+    return(music)
+  }
+
+  # add to the end of `music`, if `to` is not specified
+  if (is.null(to)) {
+    if (as == "part") {
+      music$parts[[l + 1]] <- list(list(term))
+
+    } else {
+      part <- music$parts[[l]]
+      m <- length(part)
+
+      if (as == "staff") {
+        music$parts[[l]][[m + 1]] <- list(term)
+
+      } else if (as == "voice") {
+        staff <- part[[m]]
+        n <- length(staff)
+        music$parts[[l]][[m]][[n + 1]] <- term
+      }
+    }
+
+    return(music)
+  }
+
+  # add to specified `to`
+  for (i in 1:l) {
+    part <- parts[[i]]
+    m <- length(part)
+
+    for (j in 1:m) {
+      staff <- part[[j]]
+      n <- length(staff)
+
+      for (k in 1:n) {
+        voice <- staff[[k]]
+        voice_name <- voice$name
+
+        if (to == voice_name) {
+
+          if (as == "part") {
+            music$parts <- append(
+              parts,
+              list(list(list(term))),
+              ifelse(after, i, i - 1)
+            )
+
+          } else if (as == "staff") {
+            music$parts[[i]] <- append(
+              part,
+              list(list(term)),
+              ifelse(after, j, j - 1)
+            )
+
+          } else if (as == "voice") {
+            check_voice_number(n, voice_name)
+
+            music$parts[[i]][[j]] <- append(
+              staff,
+              list(term),
+              ifelse(after, k, k - 1)
+            )
+          }
+
+          return(music)
+        }
+      }
+    }
+  }
+}
+
+
+
+# validators in `add.Line` ------------------------------------------
+
+check_add_line_name <- function(name, line_names) {
+  con <-
+    name %in% line_names
+
+  if (con) {
+    glue::glue(
+      "Each Line in a Music object must have a unique name.",
+      "\n\n",
+      '* Name "{name}" has been used.'
+    ) %>% rlang::abort()
+  }
+}
+
+
+check_add_line_to <- function(to, line_names) {
+  con <-
+    !is.null(to) &&
+    !(to %in% line_names)
+
+  if (con) {
+    glue::glue(
+      "If `to` is specified in the Line object,", " ",
+      "it must refer to the name of a Line in the Music object.",
+      "\n\n",
+      '* Can\'t find Line with name "{to}".'
+    ) %>% rlang::abort()
+  }
+}
+
+
+check_voice_number <- function(voice_number, voice_name) {
+  if (voice_number == 4) {
+    glue::glue(
+      "Each staff in a Music can have 4 voices at most.",
+      "\n\n",
+      '* Staff containing voice "{voice_name}" already has 4 voices.'
+    ) %>% rlang::abort()
+  }
 }
