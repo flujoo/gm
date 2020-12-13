fcb6bb4845e886cc927e2b5d7b1cafd25b91c4d7
flujoo <tnerrotana@gmail.com>
2020-12-13 16:08:01 +0400

    Rename `accidental_number` "key"

--- R/key.R
+++ R/key.R
@@ -1,33 +1,20 @@
 #' @export
-Key <- function(accidental_number, bar = 1) {
-  check_accidental_number(accidental_number)
+Key <- function(key, bar = 1) {
+  check_key(key)
   check_n(bar, "bar")
 
-  list(
-    accidental_number = accidental_number,
-    bar = bar
-  ) %>% `class<-`(c("Key", "BarAddOn", "Printable"))
+  list(key = key, bar = bar) %>% `class<-`(c("Key", "BarAddOn", "Printable"))
 }
 
 
-check_accidental_number <- function(accidental_number) {
-  check_type(
-    supplied = accidental_number,
-    valid = c("double", "integer"),
-    name = "accidental_number"
-  )
-
-  check_length(
-    supplied = accidental_number,
-    valid = 1,
-    name = "accidental_number",
-    type = "numeric"
-  )
+check_key <- function(key) {
+  check_type(supplied = key, valid = c("double", "integer"), name = "key")
+  check_length(supplied = key, valid = 1, name = "key", type = "numeric")
 
   check_content(
-    supplied = accidental_number,
+    supplied = key,
     valid = -7:7,
-    general = "`accidental_number` must be any integer between -7 and 7."
+    general = "`key` must be any integer between -7 and 7."
   )
 }
 
@@ -35,5 +22,5 @@ check_accidental_number <- function(accidental_number) {
 #' @keywords internal
 #' @export
 to_string.Key <- function(x, ...) {
-  x$accidental_number
+  x$key
 }
