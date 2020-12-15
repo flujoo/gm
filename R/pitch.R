279215a38ba6c9ec14532a41dfeff0d955bef0c7
flujoo <tnerrotana@gmail.com>
2020-12-15 11:34:36 +0400

    Add `to_Pitch.PitchValue` and related functions and organize code

--- R/pitch.R
+++ R/pitch.R
@@ -1,3 +1,14 @@
+# globals -----------------------------------------------------------------
+
+note_names <- c("C", "D", "E", "F", "G", "A", "B")
+note_name_values <- c(0, 2, 4, 5, 7, 9, 11)
+accidentals <- c("--", "-", "", "#", "##")
+alters <- -2:2
+
+
+
+# PitchLine ---------------------------------------------------------------
+
 PitchLine <- function(pitches) {
   pitches %T>%
     check_pitch_line() %>%
@@ -6,7 +17,7 @@ PitchLine <- function(pitches) {
 
 
 
-# validator ---------------------------------------------------------
+# PitchLine validators ----------------------------------------------------
 
 check_pitch_line <- function(pitches) {
   l <- length(pitches)
@@ -168,7 +179,7 @@ is_pitch_value <- function(value) {
 
 
 
-# constructors ------------------------------------------------------
+# PitchLine constructors --------------------------------------------------
 
 normalize_pitch_line <- function(pitch_line) {
   pitch_line %>%
@@ -298,7 +309,7 @@ PitchChord <- function(pitches) {
 
 
 
-# -> string ---------------------------------------------------------
+# * -> string -------------------------------------------------------------
 
 #' @keywords internal
 #' @export
@@ -307,8 +318,28 @@ to_string.PitchRest <- function(x, ...) {
 }
 
 
+#' @keywords internal
+#' @export
+to_string.Pitch <- function(x, ...) {
+  x$alter %>%
+    {which(. == alters)} %>%
+    accidentals[.] %>%
+    paste0(x$step, ., x$octave)
+}
 
-# notation -> Pitch -------------------------------------------------
+
+
+# Pitch -------------------------------------------------------------------
+
+# when `octave` is NULL, the output Pitch is a pitch class
+Pitch <- function(step, alter, octave = NULL) {
+  list(
+    step = step,
+    alter = alter,
+    octave = octave
+  ) %>% `class<-`(c("Pitch", "Printable"))
+}
+
 
 #' @keywords internal
 #' @export
@@ -316,6 +347,9 @@ to_Pitch <- function(x, ...) {
   UseMethod("to_Pitch")
 }
 
+
+
+# PitchNotation -> Pitch --------------------------------------------------
 
 #' @keywords internal
 #' @export
@@ -328,8 +362,8 @@ to_Pitch.PitchNotation <- function(x, ...) {
 
   alter <- x %>%
     substr(2, l - 1) %>%
-    {which(. == c("--", "-", "", "#", "##"))} %>%
-    (-2:2)[.]
+    {which(. == accidentals)} %>%
+    alters[.]
 
   octave <- x %>%
     substr(l, l) %>%
@@ -340,11 +374,207 @@ to_Pitch.PitchNotation <- function(x, ...) {
 }
 
 
+
+# PitchValue -> Pitch -----------------------------------------------------
+
+# convert a pitch value (or PitchValue) to equivalent Pitches
+to_Pitches <- function(pitch_value) {
+  ps <- list()
+  v <- unclass(pitch_value)
+  pc <- v %% 12
+  o <- v %/% 12 - 1
+
+  for (alter in alters) {
+    pc_ <- pc - alter
+
+    if (pc_ < 0) {
+      o_ <- o - 1
+    } else if (pc_ > 11) {
+      o_ <- o + 1
+    } else {
+      o_ <- o
+    }
+
+    step <-
+      (pc_ %% 12) %>%
+      {which(. == note_name_values)} %>%
+      note_names[.]
+
+    if (length(step) == 1) {
+      ps[[length(ps) + 1]] <- Pitch(step, alter, o_)
+    }
+  }
+
+  ps
+}
+
+
+# get the pitch classes making the major scale of a given key
+get_scale <- function(key) {
+  # not the same with the global `note_names`
+  note_names <- c("F", "C", "G", "D", "A", "E", "B")
+
+  if (key >= 0) {
+    alters <- c(rep(1, key), rep(0, 7 - key))
+  } else {
+    alters <- c(rep(0, 7 + key), rep(-1, -key))
+  }
+
+  lapply(1:7, function(i) Pitch(note_names[i], alters[i]))
+}
+
+
+# get the pitch class of the sharp fifth degree of a given key
+get_sharp_5th <- function(key) {
+  i <- which(-7:7 == key)
+
+  step <- (((i - 1) %% 7) + 1) %>%
+    # not the same with the global `note_names`
+    c("G", "D", "A", "E", "B", "F", "C")[.]
+
+  alter <- (i - 1 + 2) %/% 7
+
+  Pitch(step, alter)
+}
+
+
 #' @keywords internal
 #' @export
-to_string.Pitch <- function(x, ...) {
-  x$alter %>%
-    {which(. == -2:2)} %>%
-    c("--", "-", "", "#", "##")[.] %>%
-    paste0(x$step, ., x$octave)
+to_value.Pitch <- function(x, ...) {
+  x$step %>%
+    {which(. == note_names)} %>%
+    note_name_values[.] %>%
+    {. + x$alter + (x$octave + 1) * 12}
+}
+
+
+# get a Pitch's previous pitch classes
+# in a descending and ascending chromatic scale
+get_chromatic_previous <- function(pitch) {
+  step <- pitch$step
+  alter <- pitch$alter
+  i <- which(step == note_names)
+  v_step <- note_name_values[i]
+
+  pcs <- list()
+
+  for (d in c(-1, 1)) {
+    step_ <- note_names[((i + d - 1) %% 7) + 1]
+    v_step_ <- step_ %>%
+      {which(. == note_names)} %>%
+      note_name_values[.]
+
+    if (abs(v_step_ - v_step) == 2) {
+      alter_ <- alter - d
+    } else {
+      alter_ <- alter
+    }
+
+    pcs[[length(pcs) + 1]] <- Pitch(step_, alter_)
+  }
+
+  pcs
+}
+
+
+#' @keywords internal
+#' @export
+`==.Pitch` <- function(pitch_1, pitch_2) {
+  all(
+    pitch_1$step == pitch_2$step,
+    pitch_1$alter == pitch_2$alter,
+    # if any `octave` is NULL, the result is `logical(0)`,
+    # which has no impact on the outcome,
+    # which means you can compare a Pitch with a pitch class
+    pitch_1$octave == pitch_2$octave
+  )
+}
+
+
+# return Pitch in `candidate` matching any Pitch/pitch class in `domain`
+find_Pitch <- function(candidates, domain) {
+  for (pitch in candidates) {
+    for (p in domain) {
+      if (pitch == p) {
+        return(pitch)
+      }
+    }
+  }
+}
+
+
+# a better version will make use of the surrounding Pitches of `x`,
+# including those from other Lines, not only `after`,
+# to infer the background harmony or scale
+#' @keywords internal
+#' @export
+to_Pitch.PitchValue <- function(x, key = 0, after = NULL, ...) {
+  # all equivalent Pitches
+  ps <- to_Pitches(x)
+
+  # return if any Pitch fits `key`
+  p <- key %>%
+    get_scale() %>%
+    find_Pitch(ps, .)
+
+  if (!is.null(p)) {
+    return(p)
+  }
+
+  # return if any Pitch ascend or descend to `after` chromatically
+  if (!is.null(after)) {
+    # check if `x` and `after` are chromatically adjacent
+    con <- after %>%
+      to_value() %>%
+      {. - x} %>%
+      abs(.) %>%
+      {. == 1}
+
+    if (con) {
+      p <- after %>%
+        get_chromatic_previous() %>%
+        find_Pitch(ps, .)
+
+      if (!is.null(p)) {
+        return(p)
+      }
+    }
+  }
+
+  # return if any Pitch is the sharp 5th
+  p <- key %>%
+    get_sharp_5th() %>%
+    list() %>%
+    find_Pitch(ps, .)
+
+  if (!is.null(p)) {
+    return(p)
+  }
+
+  # if no Pitch is found, return one with minimal absolute alter
+  i <- ps %>%
+    sapply(function(p) p$alter) %>%
+    abs() %>%
+    {which(. == min(.))}
+
+  l <- length(i)
+
+  if (l == 1) {
+    return(ps[[i]])
+  }
+
+  # if more than one Pitch is found,
+  # return the Pitch with alter matching key's positivity
+  if (l > 1) {
+    ps <- ps[i]
+
+    if (key >= 0) {
+      f <- function(p) p$alter >= 0
+    } else {
+      f <- function(p) p$alter < 0
+    }
+
+    p <- Filter(f, ps)[[1]]
+    return(p)
+  }
 }
