d7f68ce2584c69e1cf7c8872d4fa65961373f886
flujoo <tnerrotana@gmail.com>
2020-12-20 22:30:09 +0400

    Modify "key.R"

--- R/key.R
+++ R/key.R
@@ -1,119 +1,194 @@
 # Key ---------------------------------------------------------------------
 
 #' @export
-Key <- function(key, bar = 1, to = NULL, to_part = NULL) {
-  check_key(key)
-  check_positive_integer(bar)
-  check_line_to(to)
-  check_key_to_part(to_part, to)
+Key <- function(key, bar = NULL, to = NULL, scope = NULL) {
+  # check arguments
+  check_key_key(key)
 
-  to_part <- normalize_key_to_part(to_part, to)
+  if (!is.null(bar)) {
+    check_positive_integer(bar)
+  }
 
-  list(key = key, bar = bar, to = to, to_part = to_part) %>%
-    `class<-`(c("Key", "BarAddOn", "Printable"))
+  if (!is.null(to)) {
+    check_name(to)
+  }
+
+  check_key_scope(scope, to)
+
+  # normalize arguments
+  key <- as.integer(key)
+
+  if (!is.null(bar)) {
+    bar <- as.integer(bar)
+  }
+
+  scope <- normalize_key_scope(scope, to)
+
+  list(key = key, bar = bar, to = to, scope = scope) %>%
+    `class<-`(c("Key", "Printable"))
 }
 
 
-check_key <- function(key) {
+
+# Key validators ----------------------------------------------------------
+
+check_key_key <- function(key) {
   check_type(key, c("double", "integer"))
   check_length(key, 1)
-
-  general <- "`key` must be any integer between -7 and 7."
-  check_content(key, -7:7, general = general)
+  check_content(key, -7:7, NULL, "`key` must be any integer between -7 and 7.")
 }
 
 
-# will be deprecated
-check_key_to_part <- function(to_part, to) {
-  if (is.null(to) || is.null(to_part)) {
+check_key_scope <- function(scope, to) {
+  if (is.null(to) || is.null(scope)) {
     return(invisible(NULL))
   }
 
-  check_type(supplied = to_part, valid = "logical", name = "to_part")
-  check_length(
-    supplied = to_part, valid = 1, name = "to_part", type = "logical"
-  )
-  check_na(supplied = to_part, name = "to_part")
+  check_type(scope, "character")
+  check_length(scope, 1)
+  check_content(scope, c("part", "staff"))
 }
 
 
-normalize_key_to_part <- function(to_part, to) {
+
+# Key normalizer ----------------------------------------------------------
+
+normalize_key_scope <- function(scope, to) {
+  # if `to` is not specified, ignore `scope`
   if (is.null(to)) {
-    return(NULL)
+    NULL
+
+  # default value
+  } else if (is.null(scope)) {
+    "part"
 
   } else {
-    if (is.null(to_part)) {
-      return(TRUE)
-
-    } else {
-      return(to_part)
-    }
+    scope
   }
 }
 
 
+
+# Key -> string -----------------------------------------------------------
+
 #' @keywords internal
 #' @export
-to_string.Key <- function(x, ...) {
-  x$key
+to_string.Key <- function(x, form = 1, ...) {
+  steps <- c("F", "C", "G", "D", "A", "E", "B")
+  i <- which(x$key == -7:7)
+
+  # major key
+  major <- Pitch(
+    steps[i %% 7 + 1],
+    i %/% 7 - 1
+  ) %>% to_string()
+
+  # short form
+  if (form == 0) {
+    return(major)
+  }
+
+  # minor key
+  minor <- Pitch(
+    steps[(i + 3) %% 7 + 1],
+    (i - 4) %/% 7
+  ) %>% to_string()
+
+  general <- "Key {major} major ({minor} minor)"
+  specifics <- character(0)
+
+  bar <- x$bar
+  if (!is.null(bar)) {
+    specifics[[length(specifics) + 1]] <- "to be added at bar {bar}"
+  }
+
+  to <- x$to
+  if (!is.null(to)) {
+    specifics[[length(specifics) + 1]] <-
+      'to be added only to the {x$scope} containing Line "{to}"'
+  }
+
+  # long form
+  if (form == 1) {
+    generate_long_form(general, specifics, environment())
+  }
 }
 
 
 
-# + Key -------------------------------------------------------------------
+# KeyLine -----------------------------------------------------------------
+
+KeyLine <- function(key_line = list()) {
+  key_line %>% `class<-`(c("KeyLine", "Printable"))
+}
+
+
+
+# KeyLine -> string -------------------------------------------------------
 
 #' @keywords internal
 #' @export
-add.Key <- function(term, music) {
-  to <- term$to
-
-  if (is.null(to)) {
-    return(add.BarAddOn(term, music))
+to_string.KeyLine <- function(x, ...) {
+  if (length(x) == 1) {
+    s <- x[[1]] %>% to_string(form = 0)
+    return(s)
   }
 
-  line_names <- music$line_names
-  check_add_line_to(to, line_names)
+  f <- function(key) {
+    key %>%
+      to_string(form = 0) %>%
+      paste("at bar {key$bar}") %>%
+      glue::glue() %>%
+      unclass()
+  }
 
-  parts <- music$parts
-  l <- length(parts)
+  x %>%
+    sapply(f) %>%
+    paste(collapse = ", ")
+}
 
-  for (i in 1:l) {
-    part <- parts[[i]]
-    staffs <- part$staffs
-    m <- length(staffs)
 
-    for (j in 1:m) {
-      staff <- staffs[[j]]
-      voices <- staff$voices
-      n <- length(voices)
 
-      for (k in 1:n) {
-        voice <- voices[[k]]
-        voice_name <- voice$name
+# KeyLine + Key -----------------------------------------------------------
 
-        if (to == voice_name) {
-          if (term$to_part == TRUE) {
-            kl <- part$key_line
+#' @keywords internal
+#' @export
+`+.KeyLine` <- function(key_line, key) {
+  if (is.null(key$bar)) {
+    key$bar <- 1L
+  }
 
-            if (is.null(kl)) {
-              kl <- BarAddOnLine(list(), "Key")
-            }
+  merge_bar_add_on(key_line, key) %>%
+    KeyLine()
+}
 
-            music$parts[[i]]$key_line <- kl + term
 
-          } else {
-            kl <- staff$key_line
+# also used in `+.MeterLine`
+merge_bar_add_on <- function(add_on_line, add_on) {
+  l <- length(add_on_line)
 
-            if (is.null(kl)) {
-              kl <- BarAddOnLine(list(), "Key")
-            }
+  if (l == 0) {
+    add_on_line[[1]] <- add_on
 
-            music$parts[[i]]$staffs[[j]]$key_line <- kl + term
-          }
-        }
+  } else {
+    b <- add_on$bar
+
+    for (i in 1:l) {
+      b_i <- add_on_line[[i]]$bar
+
+      if (b_i > b) {
+        add_on_line <- add_on_line %>%
+          append(list(add_on), i - 1)
+
+      } else if (b_i == b) {
+        add_on_line[[i]] <- add_on
+
+      } else if (b_i < b && i == l) {
+        add_on_line <- add_on_line %>%
+          append(list(add_on))
       }
     }
   }
 
-  music
+  return(add_on_line)
 }
