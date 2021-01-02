0324d123658d9769ed056a9875b25a320c1d3cfe
flujoo <tnerrotana@gmail.com>
2021-01-02 18:12:53 +0400

    Update pitch.R

--- R/pitch.R
+++ R/pitch.R
@@ -1,129 +1,120 @@
-# globals -----------------------------------------------------------------
-
-note_names <- c("C", "D", "E", "F", "G", "A", "B")
-note_name_values <- c(0, 2, 4, 5, 7, 9, 11)
-accidentals <- c("--", "-", "", "#", "##")
-alters <- -2:2
-
-
-
-# PitchLine ---------------------------------------------------------------
+# create PitchLine --------------------------------------------------------
 
 PitchLine <- function(pitches) {
-  pitches %T>%
-    check_pitch_line() %>%
-    normalize_pitch_line()
+  # check `pitches`
+  check_type(pitches, "list")
+  check_length(pitches, Inf)
+  check_pitch(pitches)
+
+  # normalize `pitches`
+  pitches %<>% lapply(normalize_pitch)
+
+  # create PitchLine
+  list(pitches = pitches) %>% `class<-`("PitchLine")
 }
 
 
 
-# PitchLine validators ----------------------------------------------------
+# check each item of argument `pitches` in `PitchLine` --------------------
 
-check_pitch_line <- function(pitches) {
-  check_type(pitches, "list")
-  check_length(pitches, Inf)
-
-  m <- paste(
-    "`pitches` must contain only pitch notations, MIDI note numbers and",
-    "single logical NAs. See `?Pitch` for details."
+check_pitch <- function(pitches) {
+  general <- paste(
+    "Each item of `pitches` must be a single logical NA,",
+    "or be a character or numeric vector",
+    "containing pitch notations and MIDI note numbers."
   )
-  ms <- c()
-  ts <- c("character", "integer", "numeric", "logical")
-  cs <- c("Pitch", "PitchValue", "PitchNotation", "PitchRest", "PitchChord")
 
-  l <- length(pitches)
+  specifics <- character(0)
 
-  for (i in 1:l) {
+  for (i in 1:length(pitches)) {
     p <- pitches[[i]]
-    t <- class(p)[1]
-
-    if (t %in% cs) {
-      next
-    }
-
+    t <- typeof(p)
     l <- length(p)
 
-    # check each item's type
-    if (!(t %in% ts)) {
-      ms <- "`pitches[[{i}]]` is of type {t}." %>%
+    # check type
+    if (!(t %in% c("character", "integer", "double", "logical"))) {
+      specifics[[length(specifics) + 1]] <-
+        "`pitches[[{i}]]` is of type {t}." %>%
         glue::glue() %>%
-        unclass() %>%
-        c(ms, .)
+        unclass()
+
       next
     }
 
-    # check each item's length
-    if (l == 0 || (is.logical(p) && l > 1)) {
-      ms <- "`pitches[[{i}]]` is of length {l}." %>%
-        glue::glue() %>%
-        unclass() %>%
-        c(ms, .)
+    # check logical
+    if (is.logical(p)) {
+      # check length of logical
+      if (l != 1) {
+        specifics[[length(specifics) + 1]] <-
+          "`pitches[[{i}]]` is a logical of length {l}." %>%
+          glue::glue() %>%
+          unclass()
+
+      # check content of logical
+      } else if (!is.na(p)) {
+        specifics[[length(specifics) + 1]] <-
+          "`pitches[[{i}]]` is {p}." %>%
+          glue::glue() %>%
+          unclass()
+      }
+
       next
     }
 
-    # check NA
-    if (is.logical(p) && !is.na(p)) {
-      ms <- "`pitches[[{i}]]` is {p}." %>%
+    # check length of non-logical
+    if (!is.logical(p) && l == 0) {
+      # determine article
+      if (is.integer(p)) {
+        article <- "an"
+      } else {
+        article <- "a"
+      }
+
+      specifics[[length(specifics) + 1]] <-
+        "`pitches[[{i}]]` is {article} {t} of length 0." %>%
         glue::glue() %>%
-        unclass() %>%
-        c(ms, .)
+        unclass()
+
       next
     }
 
-    # check pitch notation and pitch value
-    core <- function(p, i, j = NULL) {
-      con <- is_pitch_notation(p) ||
-        is_pitch_value(p) ||
-        (l == 1 && is.na(p) && is.logical(p))
+    # check pitch notations and values
+    for (j in 1:l) {
+      p_j <- p[j]
 
-      if (!con) {
-        # convert `p` to correct string
-        if (is.character(p)) {
-          if (is.na(p)) {
-            p <- "NA_character_"
+      if (!is_pitch_notation(p_j) && !is_pitch_value(p_j)) {
+        # convert `p_j` to correct string
+        if (is.character(p_j)) {
+          if (is.na(p_j)) {
+            p_j <- "NA_character_"
           } else {
-            p <- paste0('"', p, '"')
+            p_j <- paste0('"', p_j, '"')
           }
+
+        } else if (is.integer(p_j) && is.na(p_j)) {
+          p_j <- "NA_integer_"
+
+        } else if (is.double(p_j) && is.na(p_j)) {
+          p_j <- "NA_real_"
         }
 
-        if (is.integer(p) && is.na(p)) {
-          p <- "NA_integer_"
-        }
-
-        if (is.double(p) && is.na(p)) {
-          p <- "NA_real_"
-        }
-
-        # add correct index
-        if (is.null(j)) {
-          m <- "`pitches[[{i}]]` is {p}."
-        } else {
-          m <- "`pitches[[{i}]][{j}]` is {p}."
-        }
-
-        m %>%
+        specifics[[length(specifics) + 1]] <-
+          "`pitches[[{i}]][{j}]` is {p_j}." %>%
           glue::glue() %>%
           unclass()
       }
     }
-
-    if (l == 1) {
-      ms <- c(ms, core(p, i))
-      next
-    } else {
-      for (j in 1:length(p)) {
-        ms <- c(ms, core(p[j], i, j))
-      }
-      next
-    }
   }
 
-  show_errors(m, ms)
+  show_errors(general, specifics, env = environment())
 }
 
 
-is_pitch_notation <- function(notation) {
-  if (!is.character(notation)) {
+
+# two `is_pitch_*` predicate functions ------------------------------------
+
+is_pitch_notation <- function(x) {
+  if (!is.character(x)) {
     return(FALSE)
   }
 
@@ -139,30 +130,30 @@ is_pitch_notation <- function(notation) {
     "$"
   )
 
-  grepl(reg, notation)
+  grepl(reg, x)
 }
 
 
-is_pitch_value <- function(value) {
-  core <- function(value) {
-    !is.na(value) &
-      value >= 12 &
-      value <= 127 &
-      value == as.integer(value)
+is_pitch_value <- function(x) {
+  core <- function(x) {
+    !is.na(x) &
+      x >= 12 &
+      x <= 127 &
+      x == as.integer(x)
   }
 
-  if (is.character(value)) {
+  if (is.character(x)) {
     tryCatch(
       {
-        value %>%
+        x %>%
           as.double() %>%
           core()
       },
       warning = function(w) FALSE
     )
 
-  } else if (is.numeric(value)) {
-    core(value)
+  } else if (is.numeric(x)) {
+    core(x)
 
   } else {
     FALSE
@@ -171,32 +162,25 @@ is_pitch_value <- function(value) {
 
 
 
-# PitchLine constructors --------------------------------------------------
+# normalize each item of argument `pitches` in `PitchLine` ----------------
 
-normalize_pitch_line <- function(pitch_line) {
-  pitch_line %>%
-    lapply(PitchPoint) %>%
-    `class<-`(c("PitchLine", "List", "Printable"))
+#' @keywords internal
+#' @export
+normalize_pitch <- function(pitch) {
+  UseMethod("normalize_pitch")
 }
 
 
 #' @keywords internal
 #' @export
-PitchPoint <- function(pitch_point) {
-  UseMethod("PitchPoint")
-}
+normalize_pitch.numeric <- function(pitch) {
+  pitch %<>% as.integer()
 
-
-#' @keywords internal
-#' @export
-PitchPoint.integer <- function(pitch_point) {
-  l <- length(pitch_point)
-
-  if (l == 1) {
-    PitchValue(pitch_point)
+  if (length(pitch) == 1) {
+    pitch %>% PitchValue()
 
   } else {
-    pitch_point %>%
+    pitch %>%
       lapply(PitchValue) %>%
       PitchChord()
   }
@@ -205,16 +189,7 @@ PitchPoint.integer <- function(pitch_point) {
 
 #' @keywords internal
 #' @export
-PitchPoint.numeric <- function(pitch_point) {
-  pitch_point %>%
-    as.integer() %>%
-    PitchPoint()
-}
-
-
-#' @keywords internal
-#' @export
-PitchPoint.character <- function(pitch_point) {
+normalize_pitch.character <- function(pitch) {
   core <- function(pitch) {
     if (is_pitch_notation(pitch)) {
       pitch %>%
@@ -228,13 +203,11 @@ PitchPoint.character <- function(pitch_point) {
     }
   }
 
-  l <- length(pitch_point)
-
-  if (l == 1) {
-    core(pitch_point)
+  if (length(pitch) == 1) {
+    core(pitch)
 
   } else {
-    pitch_point %>%
+    pitch %>%
       lapply(core) %>%
       PitchChord()
   }
@@ -243,93 +216,106 @@ PitchPoint.character <- function(pitch_point) {
 
 #' @keywords internal
 #' @export
-PitchPoint.logical <- function(pitch_point) {
+normalize_pitch.logical <- function(pitch) {
   PitchRest()
 }
 
 
-#' @keywords internal
-#' @export
-PitchPoint.PitchValue <- function(pitch_point) {
-  pitch_point
+
+# some handy constructors -------------------------------------------------
+# to make printing PitchLine easy
+
+PitchValue <- function(x) {
+  x %>% `class<-`("PitchValue")
 }
 
 
-#' @keywords internal
-#' @export
-PitchPoint.PitchNotation <- function(pitch_point) {
-  pitch_point
-}
-
-
-#' @keywords internal
-#' @export
-PitchPoint.PitchRest <- function(pitch_point) {
-  pitch_point
-}
-
-
-#' @keywords internal
-#' @export
-PitchPoint.PitchChord <- function(pitch_point) {
-  pitch_point
-}
-
-
-PitchValue <- function(value) {
-  c("PitchValue", "Printable") %>%
-    `class<-`(value, .)
-}
-
-
-PitchNotation <- function(notation) {
-  c("PitchNotation", "Printable") %>%
-    `class<-`(notation, .)
+PitchNotation <- function(x) {
+  x %>% `class<-`("PitchNotation")
 }
 
 
 PitchRest <- function() {
-  c("PitchRest", "Printable") %>%
-    `class<-`(NA, .)
+  NA %>% `class<-`("PitchRest")
 }
 
 
-PitchChord <- function(pitches) {
-  c("PitchChord", "Tuple", "Printable") %>%
-    `class<-`(pitches, .)
+PitchChord <- function(x) {
+  x %>% `class<-`("PitchChord")
 }
 
 
 
-# * -> string -------------------------------------------------------------
+# print PitchLine ---------------------------------------------------------
 
 #' @keywords internal
 #' @export
-to_string.PitchRest <- function(x, ...) {
+print.PitchLine <- function(x, silent = FALSE, ...) {
+  s <-
+    x$pitches %>%
+    sapply(print, silent = TRUE) %>%
+    paste(collapse = ", ")
+
+  if (silent) {
+    s
+  } else {
+    cat(s, "\n")
+  }
+}
+
+
+#' @keywords internal
+#' @export
+print.PitchNotation <- function(x, ...) {
+  unclass(x)
+}
+
+
+#' @keywords internal
+#' @export
+print.PitchValue <- print.PitchNotation
+
+
+#' @keywords internal
+#' @export
+print.PitchRest <- function(x, ...) {
   "_"
 }
 
 
 #' @keywords internal
 #' @export
-to_string.Pitch <- function(x, ...) {
-  x$alter %>%
-    {which(. == alters)} %>%
-    accidentals[.] %>%
-    paste0(x$step, ., x$octave)
+print.PitchChord <- function(x, ...) {
+  x %>%
+    sapply(print, silent = TRUE) %>%
+    paste(collapse = ", ") %>%
+    paste0("(", ., ")")
 }
 
 
 
 # Pitch -------------------------------------------------------------------
 
-# when `octave` is NULL, the output Pitch is a pitch class
 Pitch <- function(step, alter, octave = NULL) {
-  list(
-    step = step,
-    alter = alter,
-    octave = octave
-  ) %>% `class<-`(c("Pitch", "Printable"))
+  # the output can be considered as a pitch class, if `octave` is `NULL`
+
+  list(step = step, alter = alter, octave = octave ) %>% `class<-`("Pitch")
+}
+
+
+#' @keywords internal
+#' @export
+print.Pitch <- function(x, silent = FALSE, ...) {
+  s <-
+    which(x$alter == -2:2) %>%
+    c("--", "-", "", "#", "##")[.] %>%
+    paste0(x$step, ., x$octave)
+
+  if (silent) {
+    s
+  } else {
+    cat(s, "\n")
+  }
 }
 
 
@@ -354,146 +340,19 @@ to_Pitch.PitchNotation <- function(x, ...) {
 
   alter <- x %>%
     substr(2, l - 1) %>%
-    {which(. == accidentals)} %>%
-    alters[.]
+    {which(. == c("--", "-", "", "#", "##"))} %>%
+    (-2:2)[.]
 
   octave <- x %>%
     substr(l, l) %>%
     as.integer()
 
-  list(step = step, alter = alter, octave = octave) %>%
-    `class<-`(c("Pitch", "Printable"))
+  Pitch(step, alter, octave)
 }
 
 
 
 # PitchValue -> Pitch -----------------------------------------------------
-
-# convert a pitch value (or PitchValue) to equivalent Pitches
-to_Pitches <- function(pitch_value) {
-  ps <- list()
-  v <- unclass(pitch_value)
-  pc <- v %% 12
-  o <- v %/% 12 - 1
-
-  for (alter in alters) {
-    pc_ <- pc - alter
-
-    if (pc_ < 0) {
-      o_ <- o - 1
-    } else if (pc_ > 11) {
-      o_ <- o + 1
-    } else {
-      o_ <- o
-    }
-
-    step <-
-      (pc_ %% 12) %>%
-      {which(. == note_name_values)} %>%
-      note_names[.]
-
-    if (length(step) == 1) {
-      ps[[length(ps) + 1]] <- Pitch(step, alter, o_)
-    }
-  }
-
-  ps
-}
-
-
-# get the pitch classes making the major scale of a given key
-get_scale <- function(key) {
-  # not the same with the global `note_names`
-  note_names <- c("F", "C", "G", "D", "A", "E", "B")
-
-  if (key >= 0) {
-    alters <- c(rep(1, key), rep(0, 7 - key))
-  } else {
-    alters <- c(rep(0, 7 + key), rep(-1, -key))
-  }
-
-  lapply(1:7, function(i) Pitch(note_names[i], alters[i]))
-}
-
-
-# get the pitch class of the sharp fifth degree of a given key
-get_sharp_5th <- function(key) {
-  i <- which(-7:7 == key)
-
-  step <- (((i - 1) %% 7) + 1) %>%
-    # not the same with the global `note_names`
-    c("G", "D", "A", "E", "B", "F", "C")[.]
-
-  alter <- (i - 1 + 2) %/% 7
-
-  Pitch(step, alter)
-}
-
-
-#' @keywords internal
-#' @export
-to_value.Pitch <- function(x, ...) {
-  x$step %>%
-    {which(. == note_names)} %>%
-    note_name_values[.] %>%
-    {. + x$alter + (x$octave + 1) * 12}
-}
-
-
-# get a Pitch's previous pitch classes
-# in a descending and ascending chromatic scale
-get_chromatic_previous <- function(pitch) {
-  step <- pitch$step
-  alter <- pitch$alter
-  i <- which(step == note_names)
-  v_step <- note_name_values[i]
-
-  pcs <- list()
-
-  for (d in c(-1, 1)) {
-    step_ <- note_names[((i + d - 1) %% 7) + 1]
-    v_step_ <- step_ %>%
-      {which(. == note_names)} %>%
-      note_name_values[.]
-
-    if (abs(v_step_ - v_step) == 2) {
-      alter_ <- alter - d
-    } else {
-      alter_ <- alter
-    }
-
-    pcs[[length(pcs) + 1]] <- Pitch(step_, alter_)
-  }
-
-  pcs
-}
-
-
-#' @keywords internal
-#' @export
-`==.Pitch` <- function(pitch_1, pitch_2) {
-  all(
-    pitch_1$step == pitch_2$step,
-    pitch_1$alter == pitch_2$alter,
-    # if any `octave` is NULL, the result is `logical(0)`,
-    # which has no impact on the outcome,
-    # which means you can compare a Pitch with a pitch class
-    pitch_1$octave == pitch_2$octave
-  )
-}
-
-
-# return Pitch in `candidate` matching any Pitch/pitch class in `domain`
-find_Pitch <- function(candidates, domain) {
-  for (pitch in candidates) {
-    for (p in domain) {
-      if (pitch == p) {
-        return(pitch)
-      }
-    }
-  }
-}
-
 
 # a better version will make use of the surrounding Pitches of `x`,
 # including those from other Lines, not only `after`,
@@ -568,5 +427,134 @@ to_Pitch.PitchValue <- function(x, key = 0, after = NULL, ...) {
 
     p <- Filter(f, ps)[[1]]
     return(p)
+  }
+}
+
+
+
+# PitchValue -> Pitch utils -----------------------------------------------
+
+# convert a pitch value (or PitchValue) to equivalent Pitches
+to_Pitches <- function(pitch_value) {
+  ps <- list()
+  v <- unclass(pitch_value)
+  pc <- v %% 12
+  o <- v %/% 12 - 1
+
+  for (alter in -2:2) {
+    pc_ <- pc - alter
+
+    if (pc_ < 0) {
+      o_ <- o - 1
+    } else if (pc_ > 11) {
+      o_ <- o + 1
+    } else {
+      o_ <- o
+    }
+
+    step <-
+      (pc_ %% 12) %>%
+      {which(. == c(0, 2, 4, 5, 7, 9, 11))} %>%
+      c("C", "D", "E", "F", "G", "A", "B")[.]
+
+    if (length(step) == 1) {
+      ps[[length(ps) + 1]] <- Pitch(step, alter, o_)
+    }
+  }
+
+  ps
+}
+
+
+# get the pitch classes making the major scale of a given key
+get_scale <- function(key) {
+  note_names <- c("F", "C", "G", "D", "A", "E", "B")
+
+  if (key >= 0) {
+    alters <- c(rep(1, key), rep(0, 7 - key))
+  } else {
+    alters <- c(rep(0, 7 + key), rep(-1, -key))
+  }
+
+  lapply(1:7, function(i) Pitch(note_names[i], alters[i]))
+}
+
+
+# get the pitch class of the sharp fifth degree of a given key
+get_sharp_5th <- function(key) {
+  i <- which(-7:7 == key)
+
+  step <- (((i - 1) %% 7) + 1) %>%
+    c("G", "D", "A", "E", "B", "F", "C")[.]
+
+  alter <- (i - 1 + 2) %/% 7
+
+  Pitch(step, alter)
+}
+
+
+#' @keywords internal
+#' @export
+to_value.Pitch <- function(x, ...) {
+  x$step %>%
+    {which(. == c("C", "D", "E", "F", "G", "A", "B"))} %>%
+    c(0, 2, 4, 5, 7, 9, 11)[.] %>%
+    {. + x$alter + (x$octave + 1) * 12}
+}
+
+
+# get a Pitch's previous pitch classes
+# in a descending and ascending chromatic scale
+get_chromatic_previous <- function(pitch) {
+  note_names <- c("C", "D", "E", "F", "G", "A", "B")
+
+  step <- pitch$step
+  alter <- pitch$alter
+  i <- which(step == note_names)
+  v_step <- c(0, 2, 4, 5, 7, 9, 11)[i]
+
+  pcs <- list()
+
+  for (d in c(-1, 1)) {
+    step_ <- note_names[((i + d - 1) %% 7) + 1]
+    v_step_ <- step_ %>%
+      {which(. == note_names)} %>%
+      c(0, 2, 4, 5, 7, 9, 11)[.]
+
+    if (abs(v_step_ - v_step) == 2) {
+      alter_ <- alter - d
+    } else {
+      alter_ <- alter
+    }
+
+    pcs[[length(pcs) + 1]] <- Pitch(step_, alter_)
+  }
+
+  pcs
+}
+
+
+#' @keywords internal
+#' @export
+`==.Pitch` <- function(pitch_1, pitch_2) {
+  all(
+    pitch_1$step == pitch_2$step,
+    pitch_1$alter == pitch_2$alter,
+    # if any `octave` is NULL, the result is `logical(0)`,
+    # which has no impact on the outcome,
+    # which means you can compare a Pitch with a pitch class
+    pitch_1$octave == pitch_2$octave
+  )
+}
+
+
+# return Pitch in `candidate` matching any Pitch/pitch class in `domain`
+find_Pitch <- function(candidates, domain) {
+  for (pitch in candidates) {
+    for (p in domain) {
+      if (pitch == p) {
+        return(pitch)
+      }
+    }
   }
 }
