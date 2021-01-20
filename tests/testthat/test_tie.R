c1b6a09fc403b446ba4528dd62aac75fee9905ca
flujoo <tnerrotana@gmail.com>
2021-01-20 22:01:42 +0400

    Add test for `normalize_tie`

--- tests/testthat/test_tie.R
+++ tests/testthat/test_tie.R
@@ -32,3 +32,21 @@ test_that("check tie", {
   expect_equal(class(e)[1], "no_equivalent_pitch_complicated")
   expect_match(e$message, "(1, 2)")
 })
+
+
+test_that("normalize tie", {
+  ps <- list(
+    c(90, 91, 92, 93), c(91, 92),
+    "C4", "C4",
+    NA,
+    c(90, 90, 91), 90
+  )
+
+  ds <- rep(list(1), length(ps))
+  tie <- list(1, c(3, 1), 6)
+  line <- Line(ps, ds, tie)
+
+  out <- line$tie$positions
+  expected <- list(c(1, 2), c(1, 3), 3, c(6, 1))
+  expect_equal(out, expected)
+})
