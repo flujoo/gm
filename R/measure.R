c402e4658791ee21d128d3c79d1e2142ffc716d1
flujoo <tnerrotana@gmail.com>
2021-02-12 23:46:20 +0400

    Create measure.R

---
+++ R/measure.R
@@ -0,0 +1,248 @@
+# constructors ------------------------------------------------------------
+
+# generalization of MusicXML elements backup and forward
+# `direction` is "backup" or "forward"
+# `...` includes `staff` and `voice`
+Move <- function(duration, direction, ...) {
+  list(duration = duration, direction = direction, ...) %>%
+    `class<-`("Move")
+}
+
+
+#' @keywords internal
+#' @export
+print.Move <- function(x, silent = FALSE, ...) {
+  # convert `x$direction`
+  s_direction <- switch(
+    x$direction,
+    "backup" = "<-",
+    "forward" = "->"
+  )
+
+  # convert `x`
+  s <- paste0(s_direction, x$duration)
+
+  # print or return
+  if (silent) {
+    s
+  } else {
+    cat(s, "\n")
+  }
+}
+
+
+# to represent MusicXML element note
+# `...` includes `invisible` and `measure`
+# it's more convenient to add marks in Pitches rather than in Notes,
+# since a Note may contain more than one Pitch at its early stage
+Note <- function(duration, pitch = PitchRest(), ...) {
+  list(duration = duration, pitch = pitch, ...) %>%
+    `class<-`("Note")
+}
+
+
+#' @keywords internal
+#' @export
+print.Note <- function(x, silent = FALSE, ...) {
+  s_duration <- print(x$duration, "inside", TRUE)
+  s_pitch <- print(x$pitch, TRUE)
+
+  s <- "({s_duration}, {s_pitch})" %>%
+    glue::glue() %>%
+    unclass()
+
+  # print or return
+  if (silent) {
+    s
+  } else {
+    cat(s, "\n")
+  }
+}
+
+
+Measure <- function(notes, number) {
+  list(notes = notes, number = number) %>%
+    `class<-`("Measure")
+}
+
+
+#' @keywords internal
+#' @export
+print.Measure <- function(x, silent = FALSE, ...) {
+  s <-
+    sapply(x$notes, print, silent = TRUE) %>%
+    paste(collapse = ", ") %>%
+    paste0(x$number, ": ", .)
+
+  # print or return
+  if (silent) {
+    s
+  } else {
+    cat(s, "\n")
+  }
+}
+
+
+
+# pack --------------------------------------------------------------------
+
+# convert (tied) duration value to Notes
+to_Notes <- function(value, ...) {
+  value %>%
+    untie_duration_value(decreasing = FALSE) %>%
+    lapply(to_Duration) %>%
+    lapply(Note, ...)
+}
+
+
+# convert offset to forward or rests
+normalize_offset <- function(offset, number) {
+  n3 <- number[3]
+
+  # convert `offset` to rests when the Line is not a voice
+  if (n3 == 1) {
+    to_Notes(offset, invisible = TRUE)
+
+  # convert `offset` to a forward when the Line is a voice
+  } else if (n3 > 1) {
+    Move(offset, "forward") %>% list()
+    # add it to list for convenience of `segment`
+  }
+}
+
+
+# combine pitches and Durations to Notes in Line, convert offset also,
+# and put them into Measures
+segment <- function(line, meters) {
+  # unpack
+  number <- line$number
+  bar <- line$bar
+  offset <- line$offset
+  pitches <- line$pitches$pitches
+  durations <- line$durations$durations
+  l <- length(durations)
+
+  # accumulated value of current measure
+  v_accum <- offset
+  # meter value for current measure
+  v_meter <- find_meter(bar, meters) %>% to_value()
+  # converted Measures
+  ms <- list()
+
+  # current measure
+  if (offset == 0) {
+    m <- list()
+  } else {
+    # convert `offset` and add it to current measure
+    m <- normalize_offset(offset, number)
+  }
+
+  for (i in 1:l) {
+    # unpack
+    d <- durations[[i]]
+    v <- to_value(d)
+    p <- pitches[[i]]
+    c_ <- class(p)
+
+    # if to untie `v`
+    untie <- FALSE
+
+    repeat {
+      v_temp <- v_accum + v
+
+      if (v_temp > v_meter) {
+        ds <-
+          # get rest value of current measure,
+          (v_meter - v_accum) %>%
+          # untie it,
+          untie_duration_value(decreasing = FALSE) %>%
+          # and convert it to Durations
+          lapply(to_Duration)
+
+        # mark tie and convert `ds` to Notes
+        for (j in 1:length(ds)) {
+          ds[[j]] %<>% Note(mark_tie_in_segment(p, c_, j))
+        }
+
+        # mark tie in `p` for next measure
+        p %<>% mark_tie_in_segment(c_)
+      }
+
+      # add `d` or whatever to `m`
+      if (v_temp <= v_meter) {
+        if (isFALSE(untie)) {
+          # generate Note and add it to `m`
+          m %<>% c(list(Note(d, p)))
+        } else {
+          m %<>% c(to_Notes(v, pitch = p))
+          untie <- FALSE
+        }
+      } else {
+        m %<>% c(ds)
+      }
+
+      # complete the last measure with rests or forward
+      if (v_temp < v_meter && i == l) {
+        m %<>% c(normalize_offset(v_meter - v_temp, number))
+      }
+
+      # add `m` to `ms`
+      if (v_temp >= v_meter || i == l) {
+        ms %<>% c(list(Measure(m, bar)))
+      }
+
+      # update and reset variables
+      if (v_temp < v_meter) {
+        v_accum <- v_temp
+      } else if (v_temp == v_meter) {
+        v_accum <- 0
+      } else if (v_temp > v_meter) {
+        v <- v_temp - v_meter
+        # note that `v` may not be a duration value
+        untie <- TRUE
+        v_accum <- 0
+      }
+
+      if (v_temp >= v_meter) {
+        m <- list()
+        bar <- bar + 1
+        v_meter <- find_meter(bar, meters) %>% to_value()
+      }
+
+      # break
+      if (v_temp <= v_meter) {
+        break
+      }
+    }
+  }
+
+  ms
+}
+
+
+mark_tie_in_segment <- function(pitch, type, i = NULL) {
+  if (type == "Pitch") {
+    if (!is.null(i)) {
+      pitch$tie_start <- TRUE
+    }
+
+    if (is.null(i) || i != 1) {
+      pitch$tie_stop <- TRUE
+    }
+
+  } else if (type == "PitchChord") {
+    if (!is.null(i)) {
+      for (j in 1:length(pitch)) {
+        pitch[[j]]$tie_start <- TRUE
+      }
+    }
+
+    if (is.null(i) || i != 1) {
+      for (j in 1:length(pitch)) {
+        pitch[[j]]$tie_stop <- TRUE
+      }
+    }
+  }
+
+  pitch
+}
