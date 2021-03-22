cd45e4574ffe70b3e90ceb57eca1ac07dc912d52
flujoo <tnerrotana@gmail.com>
2021-03-22 18:19:03 +0400

    Create logo

---
+++ logo.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(hexSticker)
+
+p <- ggplot() +
+  theme_void() +
+  coord_cartesian(ylim = c(-2, 2))
+
+for (i in -2:2) {
+  p <- p + geom_hline(yintercept = i)
+}
+
+s <- sticker(
+  subplot = p,
+  s_y = 1.1,
+  s_width = 2.5,
+
+  h_color = "black",
+  h_fill = "white",
+
+  package = "gm",
+  p_color = "black",
+  p_size = 15,
+  p_y = 1.09,
+
+  filename = "./man/figures/logo.png"
+)
