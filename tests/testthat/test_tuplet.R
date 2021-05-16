62c2bdc26d55ae97676510cfc1e45a9968ba633b
flujoo <tnerrotana@gmail.com>
2021-05-16 15:23:10 +0400

    Create test_tuplet.R

---
+++ tests/testthat/test_tuplet.R
@@ -0,0 +1,25 @@
+library(gm)
+
+
+test_that("check `$unit$type`", {
+  e <- expect_error(tuplet("1024", Tupler(3)))
+  expect_s3_class(e, "check_tuplet_unit")
+})
+
+
+test_that("check `$take`", {
+  e <- expect_error(tuplet("q", Tupler(3, take = "w")))
+  expect_s3_class(e, "check_tuplet_take")
+})
+
+
+test_that("check divisible", {
+  e <- expect_error(tuplet("w", Tupler(3, "q.")))
+  expect_s3_class(e, "divisible")
+})
+
+
+test_that("check output duration", {
+  e <- expect_error(tuplet("512", Tupler(3)))
+  expect_s3_class(e, "check_tuplet_out")
+})
