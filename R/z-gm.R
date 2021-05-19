b893ff55a1ff2e2c1df45fe08c23af32e8fa32c2
flujoo <tnerrotana@gmail.com>
2021-05-19 16:18:30 +0400

    Create z-gm.R

---
+++ R/z-gm.R
@@ -0,0 +1,14 @@
+#' @title gm: Generate Music Easily and Show Them Anywhere
+#'
+#' @description Provides a simple and intuitive high-level language,
+#' with which you can create music easily. Takes care of all the dirty
+#' technical details in converting your music to musical scores and audio
+#' files. Works in R Markdown documents, R Jupyter Notebooks and RStudio,
+#' so you can embed generated music anywhere.
+#'
+#' @section Author:
+#' Renfei Mao <renfeimao@gmail.com>
+#'
+#' @docType package
+#' @name gm
+NULL
