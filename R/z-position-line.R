d907ea042abb07de8c62e2d86593867951b59f81
flujoo <tnerrotana@gmail.com>
2021-05-21 10:45:04 +0400

    Add `check_to_exist`

--- R/z-position-line.R
+++ R/z-position-line.R
@@ -10,3 +10,35 @@ check_to <- function(to) {
     erify::check_n(to, general = general)
   }
 }
+
+
+check_to_exist <- function(to, music) {
+  lines <- music$lines
+  l <- ifelse(is.null(lines), 0, nrow(lines))
+
+  pass <- (is.character(to) && to %in% lines$name) ||
+    (is.numeric(to) && to <= l)
+
+  if (pass) {
+    return(invisible())
+  }
+
+  general <- "`to` must refer to a Line in the Music."
+
+  if (is.character(to)) {
+    specific <- 'Can\'t find Line of name "{to}".'
+
+  } else if (is.numeric(to)) {
+    if (l == 0) {
+      s_l <- "no Line"
+    } else if (l == 1) {
+      s_l <- "only 1 Line"
+    } else {
+      s_l <- glue::glue("only {l} Lines")
+    }
+
+    specific <- 'Can\'t find Line {to}, the Music contains {s_l}.'
+  }
+
+  erify::throw(general, specific, environment())
+}
