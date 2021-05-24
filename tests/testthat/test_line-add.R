6e18ce86ebb80101a786744d20f91b1484901e8b
flujoo <tnerrotana@gmail.com>
2021-05-24 17:06:48 +0400

    Create test_line-add.R

---
+++ tests/testthat/test_line-add.R
@@ -0,0 +1,66 @@
+library(gm)
+
+
+m <- Music()
+
+# add first Line
+m <- m + Line("C4")
+
+test_that("add first Line", {
+  # check the Music's `$lines`
+  expected <- tibble::tibble(
+    name = NA_character_,
+    part = 1L,
+    staff = 1L,
+    voice = 1L,
+    segment = 1L,
+    bar = 1L,
+    offset = 0
+  )
+
+  expect_identical(m$lines, expected)
+
+  # check the Music's `$notes`
+  notes <- m$notes
+
+  expect_equal(nrow(notes), 1L)
+  expect_identical(notes$line[1], 1L)
+  expect_identical(notes$pn[1], "C4")
+})
+
+
+# add more Lines
+m <- m + Line("D4", as = "voice", name = "D4", bar = 2) +
+  Line("E4", as = "staff", after = FALSE, offset = 10) +
+  Line("F4", to = 1, after = FALSE, name = "F4")
+
+test_that("add more Lines", {
+  # check the Music's `$lines`
+  expected <- tibble::tibble(
+    name = c(NA_character_, "D4", NA_character_, "F4"),
+    part = c(2L, 2L, 2L, 1L),
+    staff = c(2L, 2L, 1L, 1L),
+    voice = c(1L, 2L, 1L, 1L),
+    segment = c(1L, 1L, 1L, 1L),
+    bar = c(1L, 2L, 1L, 1L),
+    offset = c(0, 0, 10, 0)
+  )
+
+  expect_identical(m$lines, expected)
+})
+
+
+test_that("check `name` and `to`", {
+  expect_error(m + Line(90, name = "D4"), "D4")
+  expect_error(m + Line(90, to = "xx"), "xx")
+  expect_error(m + Line(90, to = 100), "100")
+})
+
+
+test_that("check voice limit", {
+  m <- m + Line(90, as = "voice") +
+    Line(90, as = "voice") +
+    Line(90, as = "voice")
+
+  expect_error(m + Line(90, as = "voice"), "at most")
+})
