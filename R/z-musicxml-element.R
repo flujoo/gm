4a06d5bca143e33eda72e16367d0d67ef4e007ad
flujoo <tnerrotana@gmail.com>
2021-05-26 12:46:32 +0400

    Rename and organize files related to element

--- R/z-musicxml-element.R
+++ R/z-musicxml-element.R
@@ -1,3 +1,33 @@
+Element <- function(tag, contents = NULL, attributes = NULL) {
+  list(
+    tag = tag,
+    contents = contents,
+    attributes = attributes
+  ) %>% `class<-`("Element")
+}
+
+
+#' @keywords internal
+#' @export
+print.Element <- function(x, ...) {
+  cat(to_musicxml(x), "\n")
+}
+
+
+#' @keywords internal
+#' @export
+to_Element <- function(x, ...) {
+  UseMethod("to_Element")
+}
+
+
+#' @keywords internal
+#' @export
+to_Element.default <- function(x, ...) {
+  x
+}
+
+
 to_semi_musicxml <- function(element) {
   tag <- element$tag
   contents <- element$contents
