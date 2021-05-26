f65cb2942d78c72948c2ace21866ab233a4b3e0e
flujoo <tnerrotana@gmail.com>
2021-05-26 16:07:06 +0400

    Add extra enter between components in `print.Music`

--- R/z-music.R
+++ R/z-music.R
@@ -91,19 +91,19 @@ print.Music <- function(x, ...) {
 
   cat("* of lines:\n\n")
   print(x$lines)
-  cat("\n")
+  cat("\n\n")
 
   cat("* of notes:\n\n")
   print(x$notes)
-  cat("\n")
+  cat("\n\n")
 
   cat("* of objects added to notes:\n\n")
   print(x$local)
-  cat("\n")
+  cat("\n\n")
 
   cat("* of objects added globally:\n\n")
   print(x$global)
-  cat("\n")
+  cat("\n\n")
 
   cat("* of meta-information:\n\n")
   print(x$meta)
