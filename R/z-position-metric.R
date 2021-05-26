f58ab2fc6253a5799071055e9399b1ce114daeb2
flujoo <tnerrotana@gmail.com>
2021-05-26 12:12:33 +0400

    Add `check_bar`

--- R/z-position-metric.R
+++ R/z-position-metric.R
@@ -20,6 +20,15 @@ check_offset <- function(offset) {
 }
 
 
+check_bar <- function(bar) {
+  if (is.null(bar)) {
+    return(invisible())
+  }
+
+  erify::check_n(bar)
+}
+
+
 normalize_bar <- function(bar) {
   ifelse(is.null(bar), 1L, as.integer(bar))
 }
