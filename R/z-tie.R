b59dd69204c5218b9a4bf78efeee019a59be5d9a
flujoo <tnerrotana@gmail.com>
2021-06-07 15:04:48 +0400

    Move `check_ij`

--- R/z-tie.R
+++ R/z-tie.R
@@ -17,6 +17,22 @@ Tie <- function(to, position, above = NULL) {
 }
 
 
+# check `position` in `Tie()`
+check_ij <- function(position) {
+  general <-
+    "`position` must be a numeric vector of one or two positive integers."
+
+  erify::check_type(position, c("double", "integer"), NULL, general)
+  erify::check_length(position, c(1, 2), NULL, general)
+
+  if (length(position) == 1) {
+    erify::check_n(position, general = general)
+  } else {
+    erify::check_contents(position, erify::is_n, NULL, general)
+  }
+}
+
+
 #' @export
 print.Tie <- function(x, ...) {
   cat("Tie\n\n")
