8ace0a8d7ebc1590767e77b3882498ec9bf41a9d
flujoo <tnerrotana@gmail.com>
2021-06-12 21:39:41 +0400

    Add tests for Music + Tie

--- tests/testthat/test_tie-add.R
+++ tests/testthat/test_tie-add.R
@@ -42,3 +42,56 @@ test_that("stop_position_used", {
     Music() + Line(list(c(90, 90), 90)) + Tie(1, 1) + Tie(1, 1, 2))
   expect_s3_class(e, "stop_position_used")
 })
+
+
+m <- Music() + Line(list(c(90, 91), c(91, 90)))
+
+
+test_that("tie a note in a chord", {
+  t <- Tie(1, 1, 1)
+  out <- (m + t)$ties
+
+  t1 <- t
+  t1$type <- "start"
+
+  t2 <- t
+  t2$type <- "stop"
+
+  expected <- tibble::tibble(
+    line = c(1L, 1L),
+    i = c(1L, 2L),
+    j = c(1L, 2L),
+    type = c("start", "stop"),
+    tie = list(t1, t2)
+  )
+
+  expect_identical(out, expected)
+
+  out <- (m + t + t)$ties
+  expect_identical(out, expected)
+})
+
+
+test_that("tie a chord", {
+  t <- Tie(1, 1)
+  out <- (m + t)$ties
+
+  t1 <- t
+  t1$type <- "start"
+
+  t2 <- t
+  t2$type <- "stop"
+
+  expected <- tibble::tibble(
+    line = rep(1L, 4),
+    i = rep(c(1L, 2L), 2),
+    j = c(1L, 2L, 2L, 1L),
+    type = rep(c("start", "stop"), 2),
+    tie = list(t1, t2, t1, t2)
+  )
+
+  expect_identical(out, expected)
+
+  out <- (m + t + Tie(1, 1, 2))$ties
+  expect_identical(out, expected)
+})
