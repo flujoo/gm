4234a5537ffd96f43e57b2db291c101b62d69e68
flujoo <tnerrotana@gmail.com>
2021-06-12 10:58:54 +0400

    Replace `position` with `i` and `j` in `Tie`

--- R/z-tie.R
+++ R/z-tie.R
@@ -1,8 +1,12 @@
 #' @export
-Tie <- function(to, position, above = NULL) {
+Tie <- function(to, i, j = NULL, above = NULL) {
   # check arguments
   check_to(to)
-  check_tie_position(position)
+  erify::check_n(i)
+
+  if (!is.null(j)) {
+    erify::check_n(j)
+  }
 
   if (!is.null(above)) {
     erify::check_bool(above)
@@ -11,72 +15,32 @@ Tie <- function(to, position, above = NULL) {
   # create Tie
   list(
     to = to,
-    position = position,
+    i = i,
+    j = j,
     above = above
   ) %>% `class<-`("Tie")
 }
 
 
-check_tie_position <- function(position) {
-  general <-
-    "`position` must be a numeric vector of one or two positive integers."
-
-  erify::check_type(position, c("double", "integer"), NULL, general)
-  erify::check_length(position, c(1, 2), NULL, general)
-
-  if (length(position) == 1) {
-    erify::check_n(position, general = general)
-  } else {
-    erify::check_contents(position, erify::is_n, NULL, general)
-  }
-}
-
-
-#' @keywords internal
-#' @export
-signify.Tie <- function(x, ...) {
-  type <- x$type
-
-  if (!is.null(type)) {
-    paste0("Tie ", "(", type, ")")
-
-  } else {
-    "Tie"
-  }
-}
-
-
 #' @export
 print.Tie <- function(x, ...) {
-  cat(signify(x), "\n")
+  # `$type`
+  type <- x$type
+  s_type <- ifelse(is.null(type), "", paste0("(", type, ")"))
 
-  # unpack
-  to <- x$to
-  position <- x$position
-  above <- x$above
-
-  # if to print each component
-  print_to <- !is.null(to)
-  print_position <- !is.null(position)
-  print_above <- !is.null(above)
-
-  # if to add enter
-  if (print_to || print_position || print_above) {
-    cat("\n")
-  }
+  # header
+  cat("Tie", s_type, "\n\n")
 
   # `$to`
-  if (print_to) {
-    cat("* to be added to Line", signify_to(to), "\n")
-  }
+  cat("* to be added to Line", signify_to(x$to), "\n")
 
-  # `$position`
-  if (print_position) {
-    cat("* to be added at position", signify_position(position), "\n")
-  }
+  # `$i` and `$j`
+  cat("* to be added at position", signify_position(c(x$i, x$j)), "\n")
 
   # `$above`
-  if (print_above) {
-    cat("* to be placed", ifelse(above, "above", "below"), "the notes\n")
+  above <- x$above
+
+  if (!is.null(above)) {
+    cat("* to be placed", ifelse(above, "above", "below"), "the notes", "\n")
   }
 }
