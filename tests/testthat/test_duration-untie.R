9c902470e3b70c60571ccfa6d4c30c6391061d9c
flujoo <tnerrotana@gmail.com>
2021-06-13 21:09:15 +0400

    Modify "untie" tests

--- tests/testthat/test_duration-untie.R
+++ tests/testthat/test_duration-untie.R
@@ -3,12 +3,5 @@ library(gm)
 
 test_that("untie", {
   expect_equal(untie(5.25), c(0.25, 1, 4))
-  expect_error(untie(1.1))
-})
-
-
-test_that("is_tied", {
-  expect_true(is_tied(1))
-  expect_true(is_tied(5.25))
-  expect_false(is_tied(1.1))
+  expect_equal(untie(5.25, TRUE), c(4, 1, 0.25))
 })
