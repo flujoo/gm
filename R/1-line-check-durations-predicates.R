ef7de5d9d7f46c9f38be1a6c8e486b0030afdf05
flujoo <tnerrotana@gmail.com>
2022-10-10 17:35:08 +0200

    Accept ties in `is_duration_notation()`

--- R/1-line-check-durations-predicates.R
+++ R/1-line-check-durations-predicates.R
@@ -1,40 +1,53 @@
 #' Check If Object Is Duration Notation
 #'
-#' @description A valid duration notation has two parts:
+#' @description A **duration notation** can be a string of tied
+#' atomic duration notations, e.g. "q / 3 - w - half..".
+#'
+#' An **atomic duration notation** has two parts:
 #'
 #' 1. a base
 #' 2. zero or more tuplet notations
 #'
-#' A base has two parts:
+#' A **base** has two parts:
 #'
-#' 1. a duration type or its abbreviation
+#' 1. a **duration type** or its abbreviation
 #' 2. zero to four dots
 #'
-#' A tuplet notation has two parts:
+#' A **tuplet notation** has two parts:
 #'
 #' 1. a "/" followed by a positive integer
-#' 2. an optional "* ({base} / {base})"
+#' 2. an optional "* (base / base)"
 #'
 #' @noRd
 is_duration_notation <- function(x) {
   if (!is.character(x)) return(FALSE)
 
   base <- paste0(
+    # a duration type or its abbreviation
     "(",
     paste(c(duration_types$name, duration_types$abbr), collapse = "|"),
     ")",
+    # 0-4 dots
     "\\.{0,4}"
   )
   tuplet <- paste0(
     "(",
+    # the basic tuplet notation, e.g. "/3"
     "\\s*", "/", "\\s*", "[1-9][0-9]*", "\\s*",
     "(",
+    # for complex tuplets, e.g. "* (h / q)"
     "\\*", "\\s*", "\\(", "\\s*", base, "\\s*", "/",
     "\\s*", base, "\\s*", "\\)",
     ")?",
     ")*"
   )
-  notation <- paste0("^", "\\s*", base, tuplet, "\\s*", "$")
+  notation <- paste0(
+    "^", "\\s*",
+    base, tuplet,
+    # tied duration notations
+    "(", "\\s*", "-", "\\s*", base, tuplet, ")*",
+    "\\s*", "$"
+  )
   grepl(notation, x)
 }
 
