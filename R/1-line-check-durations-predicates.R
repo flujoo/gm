174b1116a2b21b7150679215e9495f0b17ab469e
flujoo <tnerrotana@gmail.com>
2022-10-10 11:50:24 +0200

    Add `is_duration_notation()`

--- R/1-line-check-durations-predicates.R
+++ R/1-line-check-durations-predicates.R
@@ -1,3 +1,28 @@
+#' Check If Object Is Duration Notation
+#' @noRd
+is_duration_notation <- function(x) {
+  if (!is.character(x)) return(FALSE)
+
+  base <- paste0(
+    "(",
+    paste(c(duration_types$name, duration_types$abbr), collapse = "|"),
+    ")",
+    "\\.{0,4}"
+  )
+  tuplet <- paste0(
+    "(",
+    "\\s*", "/", "\\s*", "[1-9][0-9]*", "\\s*",
+    "(",
+    "\\*", "\\s*", "\\(", "\\s*", base, "\\s*", "/",
+    "\\s*", base, "\\s*", "\\)",
+    ")?",
+    ")*"
+  )
+  notation <- paste0("^", "\\s*", base, tuplet, "\\s*", "$")
+  grepl(notation, x)
+}
+
+
 duration_types <- data.frame(
   name = c(
     "maxima", "long", "breve", "whole", "half", "quarter", "eighth",
