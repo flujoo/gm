9a813bebd2a987cd589df359c7c3a43688a23dc2
flujoo <tnerrotana@gmail.com>
2022-10-12 17:15:27 +0200

    Add `to_string.Duration()` and `print.Duration()`

--- R/duration-.R
+++ R/duration-.R
@@ -5,3 +5,35 @@
 Duration <- function(x, ...) {
   UseMethod("Duration")
 }
+
+
+#' @keywords internal
+#' @export
+to_string.Duration <- function(x, short = FALSE, ...) {
+  s_atomics <- character()
+  space <- if (short) "" else " "
+
+  for (atomic in x) {
+    s_base <- to_string_duration_base(atomic, short)
+    tuplets <- atomic$tuplets
+
+    if (length(tuplets) == 0) {
+      s_atomic <- s_base
+    } else {
+      s_tuplets <- sapply(tuplets, to_string, short = short)
+      s_atomic <- paste0(c(s_base, s_tuplets), collapse = space)
+    }
+
+    s_atomics <- c(s_atomics, s_atomic)
+  }
+
+  s_tie <- paste0(space, "-", space)
+  paste(s_atomics, collapse = s_tie)
+}
+
+
+#' @keywords internal
+#' @export
+print.Duration <- function(x, ...) {
+  cat(to_string(x), "\n")
+}
