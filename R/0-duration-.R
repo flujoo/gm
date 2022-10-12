f1922832a34cbc3e554079f268fce142bea80662
flujoo <tnerrotana@gmail.com>
2022-10-12 11:37:34 +0200

    Move `duration_types`

--- R/0-duration-.R
+++ R/0-duration-.R
@@ -1,16 +1,3 @@
-duration_types <- data.frame(
-  name = c(
-    "maxima", "long", "breve", "whole", "half", "quarter", "eighth",
-    "16th", "32nd", "64th", "128th", "256th", "512th", "1024th"
-  ),
-  abbr = c(
-    "m", "l", "b", "w", "h", "q", "8", "16", "32", "64", "128", "256",
-    "512", "1024"
-  ),
-  value = 2^(6 - 1:14)
-)
-
-
 #' Check If Object Is Duration Notation
 #'
 #' @description A **duration notation** can be a string of tied
