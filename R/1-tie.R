a5c389d232bea4b6825b79059c729f946a7fcc87
flujoo <tnerrotana@gmail.com>
2022-10-14 15:14:19 +0200

    Create 1-tie.R

---
+++ R/1-tie.R
@@ -0,0 +1,28 @@
+#' @export
+Tie <- function(to, i, j = NULL) {
+  # validation
+  check_to(to)
+  erify::check_n(i)
+  if (!is.null(j)) erify::check_n(j)
+
+  # construction
+  tie <- list(to = to, i = i, j = j)
+  class(tie) <- "Tie"
+  tie
+}
+
+
+#' @export
+print.Tie <- function(x, ...) {
+  cat("Tie", "\n\n")
+
+  to <- x$to
+  i <- x$i
+  j <- x$j
+
+  s_to <- if (is.character(to)) paste0('"', to, '"') else to
+  cat("* to be added to Line", s_to, "\n")
+
+  s_ij <- if (is.null(j)) i else paste0("(", i, ", ", j, ")")
+  cat("* to be added at position", s_ij, "\n")
+}
