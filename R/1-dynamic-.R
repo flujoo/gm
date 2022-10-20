77ba9afe64b3eab402c13f04659f4672454761b4
flujoo <tnerrotana@gmail.com>
2022-10-20 23:38:10 +0200

    Add `print.Dynamic()`

--- R/1-dynamic-.R
+++ R/1-dynamic-.R
@@ -30,3 +30,43 @@ Dynamic <- function(symbol = NULL,
   class(dynamic) <- "Dynamic"
   dynamic
 }
+
+
+#' @export
+print.Dynamic <- function(x, ...) {
+  symbol <- x$symbol
+  velocity <- x$velocity
+  to <- x$to
+  i <- x$i
+  j <- x$j
+  scope <- x$scope
+
+  cat("Dynamic", symbol, "\n\n")
+  cat("* of velocity", velocity, "\n")
+
+  if (!is.null(to)) {
+    cat(
+      "* to be added to Line",
+      if (is.character(to)) paste0('"', to, '"') else to,
+      "\n"
+    )
+  }
+
+  if (!is.null(j)) {
+    cat("* to be added at position", paste0("(", i, ", ", j, ")"), "\n")
+  } else if (!is.null(i)) {
+    cat("* to be added at position", i, "\n")
+  }
+
+  if (is.null(scope)) {
+
+  } else if (scope == "note") {
+    cat("* to be applied only to the note at the position", "\n")
+  } else if (scope == "chord") {
+    cat("* to be applied to the note(s) at position", i, "\n")
+  } else if (scope %in% c("voice", "staff", "part")) {
+    cat("* to be applied to the", scope, "containing the Line", "\n")
+  } else if (scope == "score") {
+    cat("* to be applied to the whole score", "\n")
+  }
+}
