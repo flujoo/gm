95bab5d83fef059680ac1d769d0d86dbf31499e8
flujoo <tnerrotana@gmail.com>
2022-10-21 22:35:50 +0200

    Create 1-pedal.R

---
+++ R/1-pedal.R
@@ -0,0 +1,33 @@
+#' @export
+Pedal <- function(to, i, j) {
+  # validation
+  check_to(to)
+  erify::check_n(i)
+  erify::check_n(j)
+
+  # normalization
+  . <- sort(c(i, j))
+  i <- .[1]
+  j <- .[2]
+
+  # construction
+  pedal <- list(to = to, i = i, j = j)
+  class(pedal) <- "Pedal"
+  pedal
+}
+
+
+#' @export
+print.Pedal <- function(x, ...) {
+  to <- x$to
+  i <- x$i
+  j <- x$j
+
+  cat("Pedal", "\n\n")
+  cat(
+    "* to be added to Line",
+    if (is.character(to)) paste0('"', to, '"') else to,
+    "\n"
+  )
+  cat("* from note", i, "to", j, "\n")
+}
