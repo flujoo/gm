20aa989a8f95b0d5425073d5ebd953750bf5a960
flujoo <tnerrotana@gmail.com>
2022-10-24 21:04:19 +0200

    Normalize `bar` in `Line()`

--- R/1-line-.R
+++ R/1-line-.R
@@ -24,6 +24,7 @@ Line <- function(pitches = NULL,
 
   # normalization
   notes <- normalize_notes(pitches, durations)
+  if (!is.null(bar)) bar <- as.integer(bar)
 
   # construction
   line <- list(
