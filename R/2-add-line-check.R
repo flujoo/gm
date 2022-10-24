351d0af1d54027d0bd89022e0d11a7cca36b7172
flujoo <tnerrotana@gmail.com>
2022-10-24 21:26:52 +0200

    Add `check_voice_limit()`

--- R/2-add-line-check.R
+++ R/2-add-line-check.R
@@ -56,3 +56,24 @@ check_to_exist <- function(to, lines, class) {
   )
   erify::throw(general, specifics)
 }
+
+
+#' Check Number Limit of Voices
+#' @noRd
+check_voice_limit <- function(lines, target, to) {
+  filter <- lines$part == target$part & lines$staff == target$staff
+  n_voices <- nrow(lines[filter, ])
+  if (n_voices < 4) return(invisible())
+
+  if (is.null(to)) {
+    s_to <- "the Line is added to"
+  } else if (is.character(to)) {
+    s_to <- sprintf('containing Line "%s"', to)
+  } else if (is.numeric(to)) {
+    s_to <- sprintf("containing Line %s", to)
+  }
+
+  general <- "Any staff in a Music can contain at most four voices."
+  specifics <- paste("The staff", s_to, "already has four voices.")
+  erify::throw(general, specifics)
+}
