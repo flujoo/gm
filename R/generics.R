7a87c11110588131ded86c4e800dc0ac92622abd
flujoo <tnerrotana@gmail.com>
2022-10-30 09:46:52 +0100

    Add `to_case()`

--- R/generics.R
+++ R/generics.R
@@ -19,6 +19,18 @@ add <- function(object, music) {
 }
 
 
+#' Convert Object to Case in Component of Music
+#'
+#' For example, `to_case.Meter()` converts a Meter to a case in
+#' `meters` of a Music.
+#'
+#' @keywords internal
+#' @export
+to_case <- function(object, ...) {
+  UseMethod("to_case")
+}
+
+
 data_frame <- if (requireNamespace("tibble", quietly = TRUE)) {
   tibble::tibble
 } else {
