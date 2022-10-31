3615edb26a4b36f49773a23911b41053139178a6
flujoo <tnerrotana@gmail.com>
2022-10-31 17:54:47 +0100

    Modify `Tie()` and `print.Tie()`

--- R/1-tie.R
+++ R/1-tie.R
@@ -1,9 +1,15 @@
 #' @export
 Tie <- function(to, i, j = NULL) {
+  is_j <- !is.null(j)
+
   # validation
   check_to(to)
   erify::check_n(i)
-  if (!is.null(j)) erify::check_n(j)
+  if (is_j) erify::check_n(j)
+
+  # normalization
+  i <- as.integer(i)
+  j <- if (is_j) as.integer(j) else NA_integer_
 
   # construction
   tie <- list(to = to, i = i, j = j)
@@ -23,6 +29,6 @@ print.Tie <- function(x, ...) {
   s_to <- if (is.character(to)) paste0('"', to, '"') else to
   cat("* to be added to Line", s_to, "\n")
 
-  s_ij <- if (is.null(j)) i else paste0("(", i, ", ", j, ")")
+  s_ij <- if (is.na(j)) i else paste0("(", i, ", ", j, ")")
   cat("* to be added at position", s_ij, "\n")
 }
