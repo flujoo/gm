e9081dd38a71a67139b7464355cc2805cb2314be
flujoo <tnerrotana@gmail.com>
2022-10-31 11:45:36 +0100

    Simplify `add.Instrument()`

--- R/1-instrument-.R
+++ R/1-instrument-.R
@@ -1,16 +1,19 @@
 #' @export
 Instrument <- function(instrument, to, volume = NULL, pan = NULL) {
+  is_volume <- !is.null(volume)
+  is_pan <- !is.null(pan)
+
   # validation
   erify::check_interval(instrument, c(1L, 128L))
   check_to(to)
-  if (!is.null(volume)) erify::check_interval(volume, c(0L, 127L))
-  if (!is.null(pan)) erify::check_interval(pan, c(0L, 127L))
+  if (is_volume) erify::check_interval(volume, c(0L, 127L))
+  if (is_pan) erify::check_interval(pan, c(0L, 127L))
 
   # normalization
   midi <- as.integer(instrument)
   name <- instruments[midi]
-  if (!is.null(volume)) volume <- as.integer(volume)
-  if (!is.null(pan)) pan <- as.integer(pan)
+  volume <- if (is_volume) as.integer(volume) else NA_integer_
+  pan <- if (is_pan) as.integer(pan) else NA_integer_
 
   # construction
   instrument <- list(
@@ -36,6 +39,6 @@ print.Instrument <- function(x, ...) {
   s_to <- if (is.character(to)) paste0('"', to, '"') else to
   cat("* to be added to the part containing Line", s_to, "\n")
 
-  if (!is.null(volume)) cat("* of volume", volume, "\n")
-  if (!is.null(pan)) cat("* of pan", pan, "\n")
+  if (!is.na(volume)) cat("* of volume", volume, "\n")
+  if (!is.na(pan)) cat("* of pan", pan, "\n")
 }
--- R/2-add-instrument.R
+++ R/2-add-instrument.R
@@ -2,10 +2,8 @@
 #' @export
 add.Instrument <- function(object, music) {
   lines <- music$lines
-
   check_to_exist(object, lines)
-
-  instrument <- to_case(object, lines)
+  instrument <- normalize(object, lines)
   music$instruments <- update_cases(music$instruments, instrument, lines)
   music
 }
@@ -13,28 +11,21 @@ add.Instrument <- function(object, music) {
 
 #' @keywords internal
 #' @export
-to_case.Instrument <- function(object, lines, ...) {
-  volume <- object$volume
-  pan <- object$pan
-
-  # normalization
-  if (is.null(volume)) volume <- NA_integer_
-  if (is.null(pan)) pan <- NA_integer_
-
-  instrument <- data_frame(
+normalize.Instrument <- function(object, lines, ...) {
+  instrument <- list(
     midi = object$midi,
     name = object$name,
     line = get_line_row(object$to, lines),
-    volume = volume,
-    pan = pan
+    volume = object$volume,
+    pan = object$pan
   )
-  class(instrument) <- c(class(instrument), "instrument")
+  class(instrument) <- "Instrument"
   instrument
 }
 
 
 #' @keywords internal
 #' @export
-locate.instrument <- function(case, lines, ...) {
-  locate_to(case$line, lines)$part
+locate.Instrument <- function(object, lines, ...) {
+  locate_to(object$line, lines)$part
 }
