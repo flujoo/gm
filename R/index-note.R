cbcb87a35f4bf8a1f6ce29b3b91842a9f49aea63
flujoo <tnerrotana@gmail.com>
2022-11-02 21:34:26 +0100

    Add `check_j()`

--- R/index-note.R
+++ R/index-note.R
@@ -11,3 +11,19 @@ check_i <- function(i, line, notes) {
   specifics <- sprintf("`i` is %s, while the Line length is %s.", i, n)
   erify::throw(general, specifics)
 }
+
+
+#' Check If Index Exceeds Chord Length
+#' @noRd
+check_j <- function(j, line, i, notes) {
+  if (is.na(j)) return(invisible())
+
+  # the length of the chord
+  n <- max(notes[notes$line == line, ][notes$i == i, ]$j)
+  if (is.na(n)) n <- 1L
+  if (j <= n) return(invisible())
+
+  general <- "`j` must not exceed the chord length."
+  specifics <- sprintf("`j` is %s, while the chord length is %s.", j, n)
+  erify::throw(general, specifics)
+}
