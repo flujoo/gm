63d75ea2d396e2ba87dd54b4dea2ce3264337a20
flujoo <tnerrotana@gmail.com>
2022-11-03 10:43:56 +0100

    Add `update_ties()`

--- R/2-add-tie.R
+++ R/2-add-tie.R
@@ -7,3 +7,23 @@ normalize.Tie <- function(object, line, ...) {
 }
 
 
+update_ties <- function(ties, tie) {
+  if (is.null(ties)) {
+    ties <- to_case(tie)
+    return(ties)
+  }
+
+  if (is.na(tie$j)) {
+    ties <- ties[!(ties$line == tie$line & ties$i == tie$i), ]
+    ties <- rbind(ties, to_case(tie))
+    return(ties)
+  }
+
+  cover <- ties[ties$line == tie$line & ties$i == tie$i & is.na(ties$j), ]
+
+  if (nrow(cover) == 0) {
+    rbind(ties, to_case(tie))
+  } else {
+    ties
+  }
+}
