799d539d1147adff061136bad58dadbdd1f2221c
flujoo <tnerrotana@gmail.com>
2022-11-14 17:19:22 +0100

    Modify `add.Dynamic()`

--- R/2-add-dynamic.R
+++ R/2-add-dynamic.R
@@ -2,11 +2,12 @@
 #' @export
 add.Dynamic <- function(object, music) {
   to <- object$to
+  i <- object$i
   lines <- music$lines
 
   check_to_exist(to, lines)
   line <- get_line_row(to, lines)
-  check_i(object$i, line, music$notes)
+  check_i(i, line, music$notes)
 
   dynamic <- normalize(object, line)
   music$dynamics <- update_cases(music$dynamics, dynamic)
