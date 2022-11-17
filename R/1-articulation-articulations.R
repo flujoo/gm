435f89d62576609893d7c1e4814da98fb6f17725
flujoo <tnerrotana@gmail.com>
2022-11-17 11:34:48 +0100

    Align code

--- R/1-articulation-articulations.R
+++ R/1-articulation-articulations.R
@@ -1,16 +1,16 @@
 articulations <- rbind(
   data.frame(musescore = "accent", musicxml = "accent", symbol = ">"),
 
-  c("staccato"       , "staccato"       , ".")          ,
-  c("staccatissimo"  , "staccatissimo"  , "'")          ,
-  c("tenuto"         , "tenuto"         , "-")          ,
-  c("tenuto-staccato", "detached-legato", "-.")         ,
-  c("marcato"        , "strong-accent"  , "^")          ,
+  c("staccato"       , "staccato"       , "."          ),
+  c("staccatissimo"  , "staccatissimo"  , "'"          ),
+  c("tenuto"         , "tenuto"         , "-"          ),
+  c("tenuto-staccato", "detached-legato", "-."         ),
+  c("marcato"        , "strong-accent"  , "^"          ),
   c("scoop"          , "scoop"          , NA_character_),
   c("plop"           , "plop"           , NA_character_),
   c("doit"           , "doit"           , NA_character_),
   c("fall"           , "falloff"        , NA_character_),
-  c("stress"         , "stress"         , ",")          ,
-  c("unstress"       , "unstress"       , "u")          ,
-  c("soft accent"    , "soft-accent"    , "<>")
+  c("stress"         , "stress"         , ","          ),
+  c("unstress"       , "unstress"       , "u"          ),
+  c("soft accent"    , "soft-accent"    , "<>"         )
 )
