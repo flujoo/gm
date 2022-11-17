47db1a0787f50ced885641c54edc77fcf27afbf7
flujoo <tnerrotana@gmail.com>
2022-11-17 11:37:00 +0100

    Align code

--- R/1-dynamic-dynamics.R
+++ R/1-dynamic-dynamics.R
@@ -2,25 +2,25 @@
 dynamics <- rbind(
   data.frame(marking = character(), velocity = integer()),
 
-  list(marking = "pppppp", velocity = 1L)  ,
-  list(marking = "ppppp" , velocity = 5L)  ,
-  list(marking = "pppp"  , velocity = 10L) ,
-  list(marking = "ppp"   , velocity = 16L) ,
-  list(marking = "pp"    , velocity = 33L) ,
-  list(marking = "p"     , velocity = 49L) ,
+  list(marking = "pppppp", velocity = 1L  ),
+  list(marking = "ppppp" , velocity = 5L  ),
+  list(marking = "pppp"  , velocity = 10L ),
+  list(marking = "ppp"   , velocity = 16L ),
+  list(marking = "pp"    , velocity = 33L ),
+  list(marking = "p"     , velocity = 49L ),
 
-  list(marking = "mp"    , velocity = 64L) ,
-  list(marking = "mf"    , velocity = 80L) ,
+  list(marking = "mp"    , velocity = 64L ),
+  list(marking = "mf"    , velocity = 80L ),
 
-  list(marking = "f"     , velocity = 96L) ,
+  list(marking = "f"     , velocity = 96L ),
   list(marking = "ff"    , velocity = 112L),
   list(marking = "fff"   , velocity = 126L),
   list(marking = "ffff"  , velocity = 127L),
   list(marking = "fffff" , velocity = 127L),
   list(marking = "ffffff", velocity = 127L),
 
-  list(marking = "fp"    , velocity = 96L) ,
-  list(marking = "pf"    , velocity = 49L) ,
+  list(marking = "fp"    , velocity = 96L ),
+  list(marking = "pf"    , velocity = 49L ),
   list(marking = "sf"    , velocity = 112L),
   list(marking = "sfz"   , velocity = 112L),
   list(marking = "sff"   , velocity = 126L),
@@ -31,9 +31,9 @@ dynamics <- rbind(
   list(marking = "rf"    , velocity = 112L),
   list(marking = "fz"    , velocity = 112L),
 
-  list(marking = "m"     , velocity = 96L) ,
+  list(marking = "m"     , velocity = 96L ),
   list(marking = "r"     , velocity = 112L),
   list(marking = "s"     , velocity = 112L),
-  list(marking = "z"     , velocity = 80L) ,
-  list(marking = "n"     , velocity = 49L)
+  list(marking = "z"     , velocity = 80L ),
+  list(marking = "n"     , velocity = 49L )
 )
