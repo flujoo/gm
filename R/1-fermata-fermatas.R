07e72fa29008de2af3f34bd67a952ec1829f5b72
flujoo <tnerrotana@gmail.com>
2022-11-17 11:56:47 +0100

    Add `fermatas`

---
+++ R/1-fermata-fermatas.R
@@ -0,0 +1,10 @@
+fermatas <- rbind(
+  data.frame(musescore = "fermata", musicxml = "normal"),
+
+  c("short fermata"        , "angled"       ),
+  c("long fermata"         , "square"       ),
+  c("very short fermata"   , "double-angled"),
+  c("very long fermata"    , "double-square"),
+  c("long fermata (Henze)" , "double-dot"   ),
+  c("short fermata (Henze)", "half-curve"   )
+)
