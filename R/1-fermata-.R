3425332c34d431e0bf5cda01f6ff09b6527844fe
flujoo <tnerrotana@gmail.com>
2022-11-17 15:51:52 +0100

    Modify `Fermata()` and related functions

--- R/1-fermata-.R
+++ R/1-fermata-.R
@@ -1,32 +1,20 @@
 #' @export
-Fermata <- function(to, i, shape = NULL, above = NULL) {
+Fermata <- function(to, i, type = NULL, above = NULL) {
   # validation
   check_to(to)
   erify::check_n(i)
-  check_fermata_shape(shape)
+  if (!is.null(type)) erify::check_content(type, unlist(fermatas))
   if (!is.null(above)) erify::check_bool(above)
 
   # normalization
   i <- as.integer(i)
-  if (is.null(shape)) shape <- NA_character_
+  if (is.null(type)) type <- NA_character_
   if (is.null(above)) above <- NA
 
   # construction
-  fermata <- list(to = to, i = i, shape = shape, above = above)
+  fermata <- list(to = to, i = i, type = type, above = above)
   class(fermata) <- "Fermata"
   fermata
-}
-
-
-check_fermata_shape <- function(shape) {
-  if (is.null(shape)) return(invisible())
-
-  valid <- c(
-    "normal", "angled", "square", "double-angled",
-    "double-square", "double-dot", "half-curve"
-  )
-
-  erify::check_content(shape, valid)
 }
 
 
@@ -34,7 +22,7 @@ check_fermata_shape <- function(shape) {
 print.Fermata <- function(x, ...) {
   to <- x$to
   i <- x$i
-  shape <- x$shape
+  type <- x$type
   above <- x$above
 
   cat("Fermata", "\n\n")
@@ -43,7 +31,7 @@ print.Fermata <- function(x, ...) {
   cat("* to be added to Line", s_to, "\n")
   cat("* to be added at position", i, "\n")
 
-  if (!is.na(shape)) cat(sprintf('* of shape "%s"', shape), "\n")
+  if (!is.na(type)) cat(sprintf('* of type "%s"', type), "\n")
 
   if (!is.na(above)) {
     s_above <- if (above) "above" else "below"
