79e2b20a7de4973644f07db3ed7e8b1509ffcde1
flujoo <tnerrotana@gmail.com>
2022-11-19 23:45:20 +0100

    Add `above` in `Tie()`

--- R/1-tie.R
+++ R/1-tie.R
@@ -1,16 +1,18 @@
 #' @export
-Tie <- function(to, i, j = NULL) {
+Tie <- function(to, i, j = NULL, above = NULL) {
   # validation
   check_to(to)
   erify::check_n(i)
   if (!is.null(j)) erify::check_n(j)
+  if (!is.null(above)) erify::check_bool(above)
 
   # normalization
   i <- as.integer(i)
   j <- if (!is.null(j)) as.integer(j) else NA_integer_
+  if (is.null(above)) above <- NA
 
   # construction
-  tie <- list(to = to, i = i, j = j)
+  tie <- list(to = to, i = i, j = j, above = above)
   class(tie) <- "Tie"
   tie
 }
@@ -23,10 +25,16 @@ print.Tie <- function(x, ...) {
   to <- x$to
   i <- x$i
   j <- x$j
+  above <- x$above
 
   s_to <- if (is.character(to)) paste0('"', to, '"') else to
   cat("* to be added to Line", s_to, "\n")
 
   s_ij <- if (is.na(j)) i else paste0("(", i, ", ", j, ")")
   cat("* to be added at position", s_ij, "\n")
+
+  if (!is.na(above)) {
+    s_above <- if (above) "above" else "below"
+    cat("* to be placed", s_above, "the notes", "\n")
+  }
 }
