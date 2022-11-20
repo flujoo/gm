fb58a1d0d5719e30b13ee8250d5ed3f346ac640a
flujoo <tnerrotana@gmail.com>
2022-11-20 17:14:41 +0100

    Add `update_pedals()` and remove `locate.Pedal()`

--- R/2-add-pedal.R
+++ R/2-add-pedal.R
@@ -13,7 +13,7 @@ add.Pedal <- function(object, music) {
   check_i(j, line, notes)
 
   pedal <- normalize(object, line)
-  music$pedals <- update_cases(music$pedals, pedal)
+  music$pedals <- update_pedals(music$pedals, pedal)
   music
 }
 
@@ -32,8 +32,41 @@ normalize.Pedal <- function(object, line, ...) {
 }
 
 
-#' @keywords internal
-#' @export
-locate.Pedal <- function(object, ...) {
-  c(object$line, object$i, object$j)
+update_pedals <- function(pedals, pedal) {
+  line <- pedal$line
+  i <- pedal$i
+  j <- pedal$j
+
+  ks <- NULL
+
+  for (k in seq_len(NROW(pedals))) {
+    pedal_k <- pedals[k, ]
+
+    if (pedal_k$line != line) next
+
+    i_k <- pedal_k$i
+    j_k <- pedal_k$j
+
+    if (j_k < i || i_k > j) next
+    ks <- c(ks, k)
+
+    # replace the Pedal in the Music that is the same as the incoming Pedal
+    if (i == i_k && j == j_k) break
+
+    # drop the incoming Pedal if it is "covered" by any Pedal in the Music
+    if (i >= i_k && j <= j_k) return(pedals)
+
+    # merge the Pedals in the Music that "touch" the incoming Pedal
+    if (i >= i_k && i <= j_k) {
+      i <- i_k
+      pedal$i <- i
+
+    } else if (j >= i_k && j <= j_k) {
+      j <- j_k
+      pedal$j <- j
+    }
+  }
+
+  if (!is.null(ks)) pedals <- pedals[-ks, ]
+  rbind(pedals, to_case(pedal))
 }
