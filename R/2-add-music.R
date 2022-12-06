cbb13b7021387ca5c59a11e73e2c58d37597dc49
flujoo <tnerrotana@gmail.com>
2022-12-06 11:06:23 +0100

    Register Turn

--- R/2-add-music.R
+++ R/2-add-music.R
@@ -3,7 +3,7 @@
   cs <- c(
     "Line", "Meter", "Key", "Clef", "Tempo", "Tie", "Instrument",
     "Dynamic", "Pedal", "Velocity", "Articulation", "Slur", "Fermata",
-    "Grace", "Trill"
+    "Grace", "Trill", "Turn"
   )
   erify::check_binary_classes(music, object, "Music", cs, "+")
 
