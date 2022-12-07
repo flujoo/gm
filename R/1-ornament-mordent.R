cee717d834b4f632ac3d9f5664e1f3fb5c694fb8
flujoo <tnerrotana@gmail.com>
2022-12-07 12:02:49 +0100

    Add `Mordent()` and related functions

---
+++ R/1-ornament-mordent.R
@@ -0,0 +1,68 @@
+#' @export
+Mordent <- function(to, i, inverted = NULL, long = NULL, ornament = NULL) {
+  # validation
+  check_to(to)
+  erify::check_n(i)
+  if (!is.null(inverted)) erify::check_bool(inverted)
+  if (!is.null(long)) erify::check_bool(long)
+  check_mordent_ornament(ornament, long)
+
+  # normalization
+  i <- as.integer(i)
+  if (is.null(inverted)) inverted <- FALSE
+  long <- normalize_mordent_long(long, ornament)
+  if (is.null(ornament)) ornament <- NA_character_
+
+  # construction
+  mordent <- list(
+    to = to,
+    i = i,
+    inverted = inverted,
+    long = long,
+    ornament = ornament
+  )
+  class(mordent) <- "Mordent"
+  mordent
+}
+
+
+check_mordent_ornament <- function(ornament, long) {
+  if (is.null(ornament)) return(invisible())
+
+  if (isFALSE(long)) {
+    erify::throw("Can not set `ornament` when `long` is `FALSE`.")
+  }
+
+  ornaments <- c("left up", "left down", "right up", "right down")
+  erify::check_content(ornament, ornaments)
+}
+
+
+normalize_mordent_long <- function(long, ornament) {
+  if (!is.null(long)) return(long)
+  if (is.null(ornament)) FALSE else TRUE
+}
+
+
+#' @export
+print.Mordent <- function(x, ...) {
+  to <- x$to
+  i <- x$i
+  inverted <- x$inverted
+  long <- x$long
+  ornament <- x$ornament
+
+  if (long) cat("Long ")
+  if (inverted) cat("Inverted ")
+  cat("Mordent", "\n\n")
+
+  if (!is.na(ornament)) {
+    . <- strsplit(ornament, " ")[[1]]
+    s_vertical <- switch(.[2], "up" = "an upward", "down" = "a downward")
+    cat("* with", s_vertical, "ornament on the", .[1], "\n")
+  }
+
+  s_to <- if (is.character(to)) paste0('"', to, '"') else to
+  cat("* to be added to Line", s_to, "\n")
+  cat("* to be added at position", i, "\n")
+}
