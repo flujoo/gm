0027f4f56949eb1bfed0a299c700b1b6fee45be8
flujoo <tnerrotana@gmail.com>
2022-12-11 21:34:04 +0100

    Register Tremolo

--- R/2-add-music.R
+++ R/2-add-music.R
@@ -3,7 +3,7 @@
   cs <- c(
     "Line", "Meter", "Key", "Clef", "Tempo", "Tie", "Instrument",
     "Dynamic", "Pedal", "Velocity", "Articulation", "Slur", "Fermata",
-    "Grace", "Trill", "Turn", "Mordent", "Schleifer"
+    "Grace", "Trill", "Turn", "Mordent", "Schleifer", "Tremolo"
   )
   erify::check_binary_classes(music, object, "Music", cs, "+")
 
