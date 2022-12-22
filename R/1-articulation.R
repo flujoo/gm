9b2897782a9ed74ab6a027b8d695edb24525a114
flujoo <tnerrotana@gmail.com>
2022-12-22 14:58:59 +0100

    Move code

--- R/1-articulation.R
+++ R/1-articulation.R
@@ -1,3 +1,28 @@
+#' @export
+Articulation <- function(name, to, i) {
+  # validation
+  check_articulation_name(name)
+  check_to(to)
+  erify::check_n(i)
+
+  # normalization
+  name <- articulations$musescore[which(articulations == name, TRUE)[1]]
+  i <- as.integer(i)
+
+  # construction
+  articulation <- list(to = to, i = i, name = name)
+  class(articulation) <- "Articulation"
+  articulation
+}
+
+
+check_articulation_name <- function(name) {
+  valid <- unique(unlist(articulations))
+  valid <- valid[!is.na(valid)]
+  erify::check_content(name, valid)
+}
+
+
 articulations <- rbind(
   data.frame(musescore = "accent", musicxml = "accent", symbol = ">"),
 
@@ -14,3 +39,11 @@ articulations <- rbind(
   c("unstress"       , "unstress"       , "u"          ),
   c("soft accent"    , "soft-accent"    , "<>"         )
 )
+
+
+#' @export
+print.Articulation <- function(x, ...) {
+  cat("Articulation", "\n\n")
+  cat("*", x$name, "\n")
+  print_to_ij(x$to, x$i)
+}
--- R/1-fermata.R
+++ R/1-fermata.R
@@ -17,6 +17,18 @@ Fermata <- function(to, i, type = NULL, above = NULL) {
 }
 
 
+fermatas <- rbind(
+  data.frame(musescore = NA_character_, musicxml = "normal"),
+
+  c("short"        , "angled"       ),
+  c("long"         , "square"       ),
+  c("very short"   , "double-angled"),
+  c("very long"    , "double-square"),
+  c("long (Henze)" , "double-dot"   ),
+  c("short (Henze)", "half-curve"   )
+)
+
+
 #' @export
 print.Fermata <- function(x, ...) {
   to <- x$to
