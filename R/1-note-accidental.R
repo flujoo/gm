d51e9022e261e89ccd82a6a3a790ac7dce3590d9
flujoo <tnerrotana@gmail.com>
2022-12-22 14:30:02 +0100

    Add `Accidental()` and `accidentals`

---
+++ R/1-note-accidental.R
@@ -0,0 +1,50 @@
+#' @export
+Accidental <- function(name, to, i, j = NULL, bracketed = NULL) {
+  # validation
+  erify::check_content(name, accidentals)
+  check_to(to)
+  erify::check_n(i)
+  if (!is.null(j)) erify::check_n(j)
+  if (!is.null(bracketed)) erify::check_bool(bracketed)
+
+  # normalization
+  i <- as.integer(i)
+  j <- if (is.null(j)) NA_integer_ else as.integer(j)
+  if (is.null(bracketed)) bracketed <- NA
+
+  # construction
+  accidental <- list(
+    to = to,
+    i = i,
+    j = j,
+    name = name,
+    bracketed = bracketed
+  )
+  class(accidental) <- "Accidental"
+  accidental
+}
+
+
+accidentals <- c(
+  # not supported
+  # "sharp-sharp", "natural-sharp", "natural-flat",
+  # "sharp-1", "sharp-2", "sharp-3", "sharp-5",
+  # "flat-1", "flat-2", "flat-3", "flat-4",
+
+  # only in Finale
+  "triple-sharp", "triple-flat",
+
+  "sharp", "natural", "flat",
+  "double-sharp", "flat-flat",
+  "quarter-flat", "quarter-sharp",
+  "three-quarters-flat", "three-quarters-sharp",
+  "sharp-down", "sharp-up",
+  "natural-down", "natural-up",
+  "flat-down", "flat-up",
+  "double-sharp-down", "double-sharp-up",
+  "flat-flat-down", "flat-flat-up",
+  "arrow-down", "arrow-up",
+  "slash-quarter-sharp", "slash-sharp",
+  "slash-flat", "double-slash-flat",
+  "sori", "koron"
+)
