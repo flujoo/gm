68ccab9c55721a6d48b8aa83d3aaf2ee6a05c8a6
flujoo <tnerrotana@gmail.com>
2023-01-12 22:38:36 +0100

    Modify `add.Articulation()`

--- R/2-add-articulation.R
+++ R/2-add-articulation.R
@@ -6,22 +6,18 @@ add.Articulation <- function(object, music) {
   lines <- music$lines
   notes <- music$notes
 
+  # validation
   check_to_exist(to, lines)
   line <- get_line_row(to, lines)
   check_i(i, line, notes)
 
-  articulation <- normalize(object, line)
-  music$articulations <- update_cases(music$articulations, articulation)
-  music
-}
-
-
-#' @keywords internal
-#' @export
-normalize.Articulation <- function(object, line, ...) {
+  # normalization
   names(object)[names(object) == "to"] <- "line"
   object$line <- line
-  object
+
+  # construction
+  music$articulations <- update_cases(music$articulations, object)
+  music
 }
 
 
