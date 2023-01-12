d521b6506020d7f213dff897e893511047d9793d
flujoo <tnerrotana@gmail.com>
2023-01-12 23:07:20 +0100

    Modify `add.Meter()`

--- R/2-add-meter.R
+++ R/2-add-meter.R
@@ -1,21 +1,15 @@
 #' @keywords internal
 #' @export
 add.Meter <- function(object, music) {
-  meter <- normalize(object)
-  music$meters <- update_cases(music$meters, meter)
-  music
-}
-
-
-#' @keywords internal
-#' @export
-normalize.Meter <- function(object, ...) {
+  # normalization
   if (is.null(object$actual_number)) object$actual_number <- object$number
   if (is.null(object$actual_unit)) object$actual_unit <- object$unit
   if (is.null(object$bar)) object$bar <- 1L
   if (is.null(object$invisible)) object$invisible <- FALSE
 
-  object
+  # construction
+  music$meters <- update_cases(music$meters, object)
+  music
 }
 
 
