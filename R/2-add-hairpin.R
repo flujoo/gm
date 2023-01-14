e032d7e43855ecdea1cd3c0bb563bbca84085e12
flujoo <tnerrotana@gmail.com>
2023-01-14 18:27:34 +0100

    Add `add.Hairpin()`

---
+++ R/2-add-hairpin.R
@@ -0,0 +1,28 @@
+#' @keywords internal
+#' @export
+add.Hairpin <- function(object, music) {
+  to <- object$to
+  i <- object$i
+  j <- object$j
+  lines <- music$lines
+  notes <- music$notes
+
+  # validation
+  check_to_exist(to, lines)
+  line <- get_line_row(to, lines)
+  check_i(i, line, notes)
+  check_i(j, line, notes)
+
+  # normalization
+  names(object)[names(object) == "to"] <- "line"
+  object$line <- line
+  if (is.null(object$above)) object$above <- FALSE
+
+  . <- sort(c(i, j))
+  object$i <- .[1]
+  object$j <- .[2]
+
+  # construction
+  music$hairpins <- update_hairpins(music$hairpins, object)
+  music
+}
