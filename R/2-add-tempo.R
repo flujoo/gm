dda9127be46880c1e02631ede5f415fa4c30eba5
flujoo <tnerrotana@gmail.com>
2023-01-14 13:56:13 +0100

    Modify `add.Tempo()`

--- R/2-add-tempo.R
+++ R/2-add-tempo.R
@@ -1,20 +1,14 @@
 #' @keywords internal
 #' @export
 add.Tempo <- function(object, music) {
-  tempo <- normalize(object)
-  music$tempos <- update_cases(music$tempos, tempo)
-  music
-}
-
-
-#' @keywords internal
-#' @export
-normalize.Tempo <- function(object, ...) {
+  # normalization
   if (is.null(object$bar)) object$bar <- 1L
   if (is.null(object$offset)) object$offset <- 0
   if (is.null(object$invisible)) object$invisible <- FALSE
 
-  object
+  # construction
+  music$tempos <- update_cases(music$tempos, object)
+  music
 }
 
 
