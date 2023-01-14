66c35cc9147bfe8678d0569b1a875f7a22129034
flujoo <tnerrotana@gmail.com>
2023-01-14 14:05:26 +0100

    Modify `add.Pause()`

--- R/2-add-pause.R
+++ R/2-add-pause.R
@@ -5,22 +5,18 @@ add.Pause <- function(object, music) {
   i <- object$i
   lines <- music$lines
 
+  # validation
   check_to_exist(to, lines)
   line <- get_line_row(to, lines)
   check_i(i, line, music$notes)
 
-  pause <- normalize(object, line)
-  music$pauses <- update_cases(music$pauses, pause)
-  music
-}
-
-
-#' @keywords internal
-#' @export
-normalize.Pause <- function(object, line, ...) {
+  # normalization
   names(object)[names(object) == "to"] <- "line"
   object$line <- line
-  object
+
+  # construction
+  music$pauses <- update_cases(music$pauses, object)
+  music
 }
 
 
