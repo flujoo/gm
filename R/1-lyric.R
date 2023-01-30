7d2290c2ee54d1b88de4a615701197ab26493ab4
flujoo <tnerrotana@gmail.com>
2023-01-30 21:18:54 +0100

    Add `Lyric()` and related functions

---
+++ R/1-lyric.R
@@ -0,0 +1,67 @@
+#' @export
+Lyric <- function(text, to, i, special = NULL, layer = NULL) {
+  # validation
+  erify::check_type(text, "character")
+  check_to(to)
+  erify::check_n(i)
+  if (!is.null(special)) erify::check_content(special, c("-", "_"))
+  if (!is.null(layer)) erify::check_n(layer)
+
+  # normalization
+  text <- normalize_lyric_text(text)
+  i <- as.integer(i)
+  if (is.null(special)) special <- NA_character_
+  if (!is.null(layer)) layer <- as.integer(layer)
+  j <- normalize_lyric_j(text)
+
+  # construction
+  lyric <- list(
+    to = to,
+    layer = layer,
+    i = i,
+    j = j,
+    text = text,
+    special = special
+  )
+
+  class(lyric) <- "Lyric"
+  lyric
+}
+
+
+normalize_lyric_text <- function(text) {
+  l <- length(text)
+
+  if (l == 0) return(" ")
+  text[is.na(text)] <- " "
+  if (l == 1 && text == "") return(" ")
+
+  text
+}
+
+
+normalize_lyric_j <- function(text) {
+  l <- length(text)
+  if (l == 1) NA_integer_ else 1:l
+}
+
+
+#' @export
+print.Lyric <- function(x, ...) {
+  special <- x$special
+  layer <- x$layer
+
+  cat("Lyric", sprintf('"%s"', paste(x$text, collapse = "‿")), "\n\n")
+
+  if (!is.na(special)) {
+    s_special <- switch(special,
+      "-" = "to be connected with the next syllable",
+      "_" = "as a melisma"
+    )
+
+    cat("*", s_special, "\n")
+  }
+
+  print_to_ij(x$to, x$i)
+  if (!is.null(layer)) cat("* to be added to layer", layer, "\n")
+}
---
+++ R/2-add-lyric.R
@@ -0,0 +1,27 @@
+#' @keywords internal
+#' @export
+add.Lyric <- function(object, music) {
+  to <- object$to
+  i <- object$i
+  lines <- music$lines
+  lyrics <- music$lyrics
+
+  # validation
+  check_to_exist(to, lines)
+  line <- get_line_row(to, lines)
+  check_i(i, line, music$notes)
+
+  # normalization
+  names(object)[names(object) == "to"] <- "line"
+  object$line <- line
+  if (is.null(object$layer)) object$layer <- 1L
+
+  # construction
+  to_remove <-
+    lyrics$line == line &
+    lyrics$layer == object$layer &
+    lyrics$i == i
+
+  music$lyrics <- rbind(lyrics[!(to_remove), ], to_case(object))
+  music
+}
