5df25de8ee7141293361c686d9c2fc53c6761c84
flujoo <tnerrotana@gmail.com>
2023-01-31 10:22:00 +0100

    Move code

--- R/duration-predicate.R
+++ R/duration-predicate.R
@@ -45,3 +45,16 @@ is_duration_notation <- function(x) {
 
   grepl(re_tied, x)
 }
+
+
+duration_types <- data.frame(
+  name = c(
+    "maxima", "long", "breve", "whole", "half", "quarter", "eighth",
+    "16th", "32nd", "64th", "128th", "256th", "512th", "1024th"
+  ),
+  abbr = c(
+    "m", "l", "b", "w", "h", "q", "8", "16", "32", "64", "128", "256",
+    "512", "1024"
+  ),
+  value = 2^(6 - 1:14)
+)
