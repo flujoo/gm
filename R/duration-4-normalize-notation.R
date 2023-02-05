de7ee93170a00ad1d8c0e784c50478c38d8ebbc0
flujoo <tnerrotana@gmail.com>
2023-02-05 22:27:12 +0100

    Reorganize code

--- R/duration-4-normalize-notation.R
+++ R/duration-4-normalize-notation.R
@@ -37,3 +37,34 @@ parse_duration_base <- function(base) {
 
   list(type = type, dot = dot)
 }
+
+
+#' Normalize Tuplet Notation to `Tuplet` Object
+#'
+#' @keywords internal
+#' @export
+Tuplet <- function(tuplet_notation) {
+  parts <- strsplit(tuplet_notation, "/|\\*|\\(|\\)|\\s")[[1]]
+  parts <- parts[parts != ""]
+
+  n <- as.integer(parts[1])
+
+  if (length(parts) == 3) {
+    take <- parse_duration_base(parts[2])
+    unit <- parse_duration_base(parts[3])
+  } else {
+    take <- NULL
+    unit <- NULL
+  }
+
+  tuplet <- list(n = n, take = take, unit = unit)
+  class(tuplet) <- "Tuplet"
+  tuplet
+}
+
+
+#' @keywords internal
+#' @export
+print.Tuplet <- function(x, ...) {
+  cat(to_string(x), "\n")
+}
