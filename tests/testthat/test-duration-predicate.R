a3c6827743f78eb29382ac3b305c1feb3c6ec2d0
flujoo <tnerrotana@gmail.com>
2023-02-10 12:48:14 +0100

    Test `is_duration_value()`

--- tests/testthat/test-duration-predicate.R
+++ tests/testthat/test-duration-predicate.R
@@ -4,3 +4,14 @@ test_that("tuplets with invalid takes or units are nevertheless valid", {
   out <- is_duration_notation(notation)
   expect_true(out)
 })
+
+
+test_that("1024th note is the smallest valid duration value", {
+  value <- duration_types[duration_types$abbr == "1024", "value"]
+
+  out <- is_duration_value(value)
+  expect_true(out)
+
+  out <- is_duration_value(value * 0.99)
+  expect_false(out)
+})
