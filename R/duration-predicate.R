942eda9018446fa38fbd2ec2ae9008cf75d1cd3c
flujoo <tnerrotana@gmail.com>
2023-02-13 22:33:05 +0100

    Add `is_tuplet()`

--- R/duration-predicate.R
+++ R/duration-predicate.R
@@ -48,3 +48,60 @@ is_duration_notation <- function(x) {
   re <- paste0("^", re_base, re_ratio, "$")
   grepl(re, x)
 }
+
+
+#' Check If Tuplet Notation Is Valid
+#'
+#' @description `is_duration_notation()` only checks the syntax of
+#' a duration notation. This is not enough for tuplet notations.
+#' For example, the syntactically valid tuplet notation "1024th/3"
+#' implies a duration type shorter than the 1024th note,
+#' which is not semantically valid.
+#'
+#' `is_tuplet()` checks further
+#'
+#' 1. if any duration type shorter than the 1024th note is implied,
+#' 2. if any ratio has a value larger than 1, and
+#' 3. if any unit can not divide its previous duration base.
+#'
+#' @param notation A duration notation validated by `is_duration_notation()`.
+#'
+#' @noRd
+is_tuplet <- function(notation) {
+  # non-tuplets are acceptable for convenience
+  if (!grepl("/", notation)) return(TRUE)
+
+  duration <- parse_duration_notation(notation)
+
+  type <- duration$type
+  dot <- duration$dot
+  ratios <- duration$ratios
+
+  for (i in seq_along(ratios)) {
+    ratio <- ratios[[i]]
+
+    # validation of ratio value
+    if (to_value_tuplet_ratio(ratio) > 1) return(FALSE)
+
+    take <- ratio$take
+
+    if (is.null(take)) {
+      type <- divide_duration_type(type, ratio$n)
+
+      # validation of implied duration type
+      if (is.na(type)) return(FALSE)
+
+    } else {
+      # validation of divisibility
+      remainder <- to_value_duration_base(list(type = type, dot = dot)) %%
+        to_value_duration_base(ratio$unit)
+
+      if (remainder != 0) return(FALSE)
+
+      type <- take$type
+      dot <- take$dot
+    }
+  }
+
+  TRUE
+}
