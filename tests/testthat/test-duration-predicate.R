e23867ab8f9a2f66351dfb46b8b49d8fabf77268
flujoo <tnerrotana@gmail.com>
2023-02-14 11:24:36 +0100

    Move and modify test

--- tests/testthat/test-duration-predicate.R
+++ tests/testthat/test-duration-predicate.R
@@ -1,11 +1,3 @@
-test_that("tuplets with invalid takes or units are nevertheless valid", {
-  notation <- "whole./3*(l/16..)/500"
-
-  out <- is_duration_notation(notation)
-  expect_true(out)
-})
-
-
 test_that("1024th note is the smallest valid duration value", {
   value <- duration_types[duration_types$abbr == "1024", "value"]
 
@@ -14,6 +6,14 @@ test_that("1024th note is the smallest valid duration value", {
 
   out <- is_duration_value(value * 0.99)
   expect_false(out)
+})
+
+
+test_that("syntactically valid complex tuplets", {
+  notation <- "whole./3*(l/16..)/500"
+
+  out <- is_duration_notation(notation)
+  expect_true(out)
 })
 
 
