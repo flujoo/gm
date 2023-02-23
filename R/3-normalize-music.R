4ce8257ec0596674b680d726898e524ab3a4e5f4
flujoo <tnerrotana@gmail.com>
2023-02-23 17:38:30 +0100

    Start `normalize_music()`

---
+++ R/3-normalize-music.R
@@ -0,0 +1,30 @@
+normalize_music <- function(music) {
+  # add missing essential components
+  music <- add_first_bar_meter(music)
+  music <- fill_empty_music(music)
+  music <- add_global_key(music)
+
+  music
+}
+
+
+add_first_bar_meter <- function(music) {
+  if (1 %in% music$meters$bar) return(music)
+  music + Meter(4, 4)
+}
+
+
+fill_empty_music <- function(music) {
+  if (!is.null(music$lines)) return(music)
+  meters <- music$meters
+  meter_bar_1 <- meters[meters$bar == 1, ]
+  music + Line(durations = to_value(meter_bar_1))
+}
+
+
+add_global_key <- function(music) {
+  keys <- music$keys
+  global_key <- keys[is.na(keys$line) & keys$bar == 1, ]
+  if (NROW(global_key) != 0) return(music)
+  music + Key(0)
+}
