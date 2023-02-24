b1a5386c52921867622df01465f55eba5808c524
flujoo <tnerrotana@gmail.com>
2023-02-24 18:38:48 +0100

    Redesign `round_duration_value()`

--- R/duration-normalize-value.R
+++ R/duration-normalize-value.R
@@ -1,7 +1,12 @@
 #' Approximate Duration Value As Multiple of 1024th Note
+#'
+#' The remainder is dropped rather than rounded up,
+#' or some Clefs and Tempos would pass the target positions,
+#' because of their rounded up offsets.
+#'
 #' @noRd
 round_duration_value <- function(value) {
   value_1024 <- 1/256
   remainder <- value %% value_1024
-  value - remainder + value_1024 * round(remainder/value_1024, 0)
+  value - remainder
 }
