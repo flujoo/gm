8634c314d4888720f65fb97c35d322f9ca03c556
flujoo <tnerrotana@gmail.com>
2023-03-03 23:02:10 +0100

    Move code

--- R/2-utils-.R
+++ R/2-utils-.R
@@ -1,3 +1,21 @@
+#' Get Line's Row in `lines` of Music
+#'
+#' Get the row number of the Line that `to` refers to.
+#'
+#' @noRd
+normalize_to <- function(to, lines) {
+  if (is.numeric(to)) {
+    as.integer(to)
+
+  } else if (is.character(to)) {
+    which(lines$name == to)
+
+  } else if (is.null(to)) {
+    nrow(lines)
+  }
+}
+
+
 #' @keywords internal
 #' @export
 locate <- function(object, ...) {
