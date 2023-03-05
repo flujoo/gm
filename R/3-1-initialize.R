9fd374b2235135647a639a32c08f7a5a5d6b79aa
flujoo <tnerrotana@gmail.com>
2023-03-05 14:03:23 +0100

    Document `initialize_global_key()`

--- R/3-1-initialize.R
+++ R/3-1-initialize.R
@@ -28,9 +28,16 @@ initialize_notes_lines <- function(music) {
 }
 
 
+#' Initialize Global Key
+#'
+#' Add C major as the global Key for conversion of MIDI note numbers.
+#'
+#' @noRd
 initialize_global_key <- function(music) {
+  # check if there is already a global Key
   keys <- music$keys
   global_key <- keys[is.na(keys$line) & keys$bar == 1, ]
   if (NROW(global_key) != 0) return(music)
+
   music + Key(0)
 }
