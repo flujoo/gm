02c6f0b14e448205f03634ace202edd38cd83024
flujoo <tnerrotana@gmail.com>
2023-03-05 13:37:11 +0100

    Document `initialize_first_bar_meter()`

--- R/3-1-initialize.R
+++ R/3-1-initialize.R
@@ -1,3 +1,10 @@
+#' Initialize Meter 4/4 at Bar 1
+#'
+#' There has to be a Meter at the first bar, or the bar-offset system would
+#' fail. For example, how to insert a Line at the second bar without
+#' knowing the duration of the first bar?
+#'
+#' @noRd
 initialize_first_bar_meter <- function(music) {
   if (1 %in% music$meters$bar) return(music)
   music + Meter(4, 4)
