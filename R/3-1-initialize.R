4acb6245609b0a814c46425306b1bd46e95cda32
flujoo <tnerrotana@gmail.com>
2023-03-05 14:05:54 +0100

    Modify documentation of `initialize_first_bar_meter()`

--- R/3-1-initialize.R
+++ R/3-1-initialize.R
@@ -4,6 +4,8 @@
 #' fail. For example, how to insert a Line at the second bar without
 #' knowing the duration of the first bar?
 #'
+#' This should be the first step of initialization.
+#'
 #' @noRd
 initialize_first_bar_meter <- function(music) {
   if (1 %in% music$meters$bar) return(music)
