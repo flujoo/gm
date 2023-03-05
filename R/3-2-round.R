0457ffd9b42c774e761ea09ae765c19cca3b6851
flujoo <tnerrotana@gmail.com>
2023-03-05 22:37:22 +0100

    Add `round_duration_values()`

--- R/3-2-round.R
+++ R/3-2-round.R
@@ -79,3 +79,15 @@ round_offset <- function(bar, offset, meters) {
 
   list(bar = bar, offset = offset)
 }
+
+
+round_duration_values <- function(music) {
+  notes <- music$notes
+  lengths <- notes$length
+  filter <- is.na(notes$duration)
+
+  notes[filter, "length"] <- round_duration_value(lengths[filter])
+
+  music$notes <- notes
+  music
+}
