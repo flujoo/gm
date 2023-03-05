34dba39a8cb0009ce6b49460a957a2493be22064
flujoo <tnerrotana@gmail.com>
2023-03-05 13:05:55 +0100

    Update `prepare()`

--- R/3-prepare.R
+++ R/3-prepare.R
@@ -1,8 +1,8 @@
 prepare <- function(music) {
+  # initialization
   music <- initialize_first_bar_meter(music)
   music <- initialize_notes_lines(music)
   music <- initialize_global_key(music)
-  music <- round_offsets(music)
 
   music
 }
