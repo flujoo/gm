f7b10f6032a51a9768839721ddca60244ff512c3
flujoo <tnerrotana@gmail.com>
2023-03-10 15:23:54 +0100

    Reorganize code

---
+++ R/3-utils-round-duration-values.R
@@ -0,0 +1,10 @@
+round_duration_values <- function(music) {
+  notes <- music$notes
+  lengths <- notes$length
+  filter <- is.na(notes$duration)
+
+  notes[filter, "length"] <- round_duration_value(lengths[filter])
+
+  music$notes <- notes
+  music
+}
--- R/3-utils-round-offsets.R
+++ R/3-utils-round-offsets.R
@@ -1,33 +1,3 @@
-#' Approximate Duration Value As Multiple of 1024th Note
-#'
-#' Although any duration values larger than the 1024th note are acceptable,
-#' they needs to be quantized for further processing.
-#'
-#' @param method `"floor"` or `"round"`.
-#'
-#' - `"floor"` is for the offsets of Clefs and Tempos to prevent
-#' some of them passing their target positions.
-#'
-#' - `"round"` is for the lengths of notes and the offsets of Lines.
-#'
-#' @details Also note the floating point issue in R,
-#' which nonetheless seems to have been handled in the process:
-#' https://stackoverflow.com/questions/9508518/why-are-these-numbers-not-equal
-#'
-#' @noRd
-round_duration_value <- function(value, method = "round") {
-  value_1024 <- 1/256
-  m <- value / value_1024
-
-  # deal with "round to even" issue
-  # https://stackoverflow.com/questions/12688717/round-up-from-5/12688836
-  n <- floor(m)
-  n <- ifelse(method == "round" & m - n >= 0.5, n + 1, n)
-
-  value_1024 * n
-}
-
-
 #' Round Up Overflowed Offsets
 #'
 #' Round up offsets that exceed the durations of their target bars.
@@ -78,16 +48,4 @@ round_offset <- function(bar, offset, meters) {
   }
 
   list(bar = bar, offset = offset)
-}
-
-
-round_duration_values <- function(music) {
-  notes <- music$notes
-  lengths <- notes$length
-  filter <- is.na(notes$duration)
-
-  notes[filter, "length"] <- round_duration_value(lengths[filter])
-
-  music$notes <- notes
-  music
 }
