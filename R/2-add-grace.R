6c75f61e0d7f6be6bcf08ceec850b487b69c6456
flujoo <tnerrotana@gmail.com>
2023-03-15 18:16:42 +0100

    Reorganize code

--- R/2-add-grace.R
+++ R/2-add-grace.R
@@ -1,3 +1,33 @@
+#' @keywords internal
+#' @export
+add.Grace <- function(object, music) {
+  to <- object$to
+  i <- object$i
+  lines <- music$lines
+
+  # validation
+  check_add_to(to, lines, object)
+  line <- normalize_to(to, lines)
+  check_grace(i, line, music$notes)
+
+  # normalization
+  if (is.null(object$slash)) object$slash <- TRUE
+  names(object)[names(object) == "to"] <- "line"
+  object$line <- line
+
+  # construction
+  music$graces <- update_cases(music$graces, object)
+  music
+}
+
+
+#' @keywords internal
+#' @export
+locate.Grace <- function(object, ...) {
+  c(object$line, object$i)
+}
+
+
 #' Various Validations for Grace Adding
 #'
 #' 1. Check if `i` is not less than the Line length.
