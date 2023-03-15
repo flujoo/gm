a2189f307786724bf8c2e6c975c88729facc253d
flujoo <tnerrotana@gmail.com>
2023-03-15 18:48:11 +0100

    Remove functions

--- tests/testthat/test-3-utils-grace.R
+++
@@ -1,9 +0,0 @@
-test_that("separate and restore grace notes", {
-  expected <-
-    Music() +
-    Line(list(80, 81:82, 83)) + Grace(2) +
-    Line(list(NA, 91, 92)) + Grace(2)
-
-  out <- restore_grace_notes(separate_grace_notes(expected))
-  expect_identical(out, expected)
-})
