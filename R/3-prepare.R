9e8fcaa34f832bc19ef7be64c458b3b5d56ebf67
flujoo <tnerrotana@gmail.com>
2023-03-15 22:09:43 +0100

    Move function

--- R/3-prepare.R
+++ R/3-prepare.R
@@ -18,3 +18,13 @@ check_first_bar_meter <- function(music) {
   if (1 %in% music$meters$bar) return(invisible())
   erify::throw("`music` must have a Meter at the first bar.")
 }
+
+
+initialize_global_key <- function(music) {
+  # check if there is already a global Key
+  keys <- music$keys
+  global_key <- keys[is.na(keys$line) & keys$bar == 1, ]
+  if (NROW(global_key) != 0) return(music)
+
+  music + Key(0)
+}
