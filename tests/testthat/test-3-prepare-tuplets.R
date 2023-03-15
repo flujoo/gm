74a724620926d62daf4827f0937bb583df08f654
flujoo <tnerrotana@gmail.com>
2023-03-15 22:46:26 +0100

    Test grace note

--- tests/testthat/test-3-prepare-tuplets.R
+++ tests/testthat/test-3-prepare-tuplets.R
@@ -1,7 +1,9 @@
 test_that("group tuplets", {
   durations_1 <- list(
     1, # 0
-    "q/3", "q/3", "q/3", # 1
+    "q/3", "q/3", # 1
+    4, # 0
+    "q/3", # 1
     "q/3", # -2
     "q", # 0
     "q/3/3", # -2
@@ -15,11 +17,16 @@ test_that("group tuplets", {
   )
 
   durations_2 <- "q/3" # -4
-  music <- Music() + Line(NULL, durations_1) + Line(NULL, durations_2)
+
+  music <-
+    Music() +
+    Line(90, durations_1) + Grace(4) +
+    Line(NULL, durations_2)
+
   out <- group_tuplets(music)$notes$group
 
   expected <- as.integer(c(
-    0, rep(1, 3), -2, 0, -2, -2, 0, rep(-3, 2), -2, 0, rep(2, 4), -1, -4
+    0, 1, 1, 0, 1, -2, 0, -2, -2, 0, rep(-3, 2), -2, 0, rep(2, 4), -1, -4
   ))
 
   expect_identical(out, expected)
