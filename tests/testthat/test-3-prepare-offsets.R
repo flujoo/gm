92510dbe6d0d8cacad9da8fe96f5c3d353951e1a
flujoo <tnerrotana@gmail.com>
2023-03-27 12:55:49 +0200

    Restore and adapt test for `round_offsets()`

---
+++ tests/testthat/test-3-prepare-offsets.R
@@ -0,0 +1,39 @@
+test_that("round up offsets in Music", {
+  music <-
+    Music() +
+    Meter(3, 4, 4) + Meter(4, 4, 5) + Meter(3, 4, 1, 2) +
+    Line(90, bar = 3, offset = 10) +
+    Clef("g", bar = 4, offset = 3) +
+    Tempo(90, bar = 10, offset = 5)
+
+  music <- order_meters(music)
+  music <- round_offsets(music)
+
+  lines <- music$lines
+  clefs <- music$clefs
+  tempos <- music$tempos
+
+  out <- lines$bar
+  expected <- 6L
+  expect_identical(out, expected)
+
+  out <- lines$offset
+  expected <- 1
+  expect_identical(out, expected)
+
+  out <- clefs$bar
+  expected <- 5L
+  expect_identical(out, expected)
+
+  out <- clefs$offset
+  expected <- 0
+  expect_identical(out, expected)
+
+  out <- tempos$bar
+  expected <- 11L
+  expect_identical(out, expected)
+
+  out <- tempos$offset
+  expected <- 1
+  expect_identical(out, expected)
+})
