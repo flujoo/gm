8312979970449dab304b55f2924e98e0fdf7b83d
flujoo <tnerrotana@gmail.com>
2023-03-27 12:41:46 +0200

    Restore and redesign `round_offsets()` and `round_offset()`

---
+++ R/3-prepare-offsets.R
@@ -0,0 +1,42 @@
+#' @description In Lines, Clefs, and Tempos, round up the offsets
+#' that exceed the lengths of their target bars.
+#'
+#' @noRd
+round_offsets <- function(music) {
+  meters <- music$meters
+
+  for (name in names(music)) {
+    component <- music[[name]]
+    if (!("offset" %in% names(component))) next
+
+    for (k in seq_len(NROW(component))) {
+      case <- component[k, ]
+      . <- round_offset(case$bar, case$offset, meters)
+      music[[name]][k, ]$bar <- .$bar
+      music[[name]][k, ]$offset <- .$offset
+    }
+  }
+
+  music
+}
+
+
+#' @param meters Already sorted by `bar` in descending order.
+#' @noRd
+round_offset <- function(bar, offset, meters) {
+  bars <- meters$bar
+
+  repeat {
+    # find the Meter for the current bar
+    k <- which(bars <= bar)[1]
+    meter <- meters[k, ]
+
+    value <- to_value(meter)
+    if (offset < value) break
+
+    bar <- bar + 1L
+    offset <- offset - value
+  }
+
+  list(bar = bar, offset = offset)
+}
