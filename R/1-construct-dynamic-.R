0238b5e8b1c9210a0c97ad875456304059033cb0
flujoo <tnerrotana@gmail.com>
2023-12-18 21:19:27 +0100

    Document `Dynamic()`

--- R/1-construct-dynamic-.R
+++ R/1-construct-dynamic-.R
@@ -1,4 +1,47 @@
+#' Create `Dynamic` Object
+#'
+#' Create a `Dynamic` object to represent a dynamic marking.
+#'
+#' Common used dynamic markings and their velocities in MuseScore:
+#' `r document_dynamics()`
+#'
+#' @param marking A single character, which represents the dynamic symbol on
+#' the score. If `marking` is on the list in the *Details* section, and
+#' `velocity` is not specified, the corresponding velocity on the list will
+#' be used. Otherwise, `velocity` must be specified, or the `Dynamic` will
+#' have no sound effect.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the `Dynamic` object in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the `Dynamic`.
+#'
+#' @param velocity Optional. A single integer between `0` and `127`,
+#' which indicates the loudness of the `Dynamic`.
+#'
+#' @param above Optional. A single logical, which indicates whether the
+#' dynamic symbol should appear above or below the staff.
+#'
+#' @returns A list of class `Dynamic`.
+#'
+#' @seealso [gm::+.Music()] for adding an `Dynamic` to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create an `Dynamic`
+#' f <- Dynamic("f", 1)
+#' f
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4")) + f
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Dynamic <- function(
     marking,
     i,
@@ -62,4 +105,10 @@ print.Dynamic <- function(x, ...) {
     s_above <- if (above) "above" else "below"
     cat("* to be placed", s_above, "the staff", "\n")
   }
+}
+
+
+document_dynamics <- function() {
+  docs <- sprintf("- %s: %s", dynamics$marking, dynamics$velocity)
+  paste(docs, collapse = "\n")
 }
