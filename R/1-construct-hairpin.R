7ebc6eb48e714472a1ddd0e81423af39b1b043c3
flujoo <tnerrotana@gmail.com>
2023-12-20 21:48:48 +0100

    Document `Hairpin()`

--- R/1-construct-hairpin.R
+++ R/1-construct-hairpin.R
@@ -1,4 +1,43 @@
+#' Create `Hairpin` Object
+#'
+#' Create a `Hairpin` object to represent a crescendo or diminuendo symbol.
+#'
+#' @param symbol A single character, which can be `"<"` or `">"`. They
+#' represent crescendo and diminuendo respectively.
+#'
+#' @param i,j A single positive integer. They indicate the start
+#' and end position of the `Hairpin` object in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the `Hairpin` object.
+#'
+#' @param text Optional. A single character, which represents the text
+#' that will appear on the score, instead of the crescendo or diminuendo
+#' symbol.
+#'
+#' @param above Optional. A single logical, which indicates whether the
+#' `Hairpin` object should appear above or below the staff.
+#'
+#' @returns A list of class `Hairpin`.
+#'
+#' @seealso [gm::+.Music()] for adding a `Hairpin` to
+#' a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a crescendo
+#' crescendo <- Hairpin("<", 1, 3)
+#' crescendo
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4", "E4")) + crescendo
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Hairpin <- function(symbol, i, j, to = NULL, text = NULL, above = NULL) {
   # Validation
   erify::check_content(symbol, c("<", ">"))
