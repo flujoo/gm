52d7214595ad8ad65c406c90ae6799e2cdf0b3aa
flujoo <tnerrotana@gmail.com>
2023-12-20 21:18:23 +0100

    Document `Grace()`

--- R/1-construct-grace.R
+++ R/1-construct-grace.R
@@ -1,4 +1,40 @@
+#' Create `Grace` Object
+#'
+#' Create a `Grace` object. The `Grace` object can be added to an existing
+#' note or chord. It will turn the note or chord to a grace note or chord.
+#'
+#' A `Grace` object can not be added to a rest, tuplet, or note or chord
+#' that has a dotted duration. There must be a note or chord after
+#' the note or chord where the `Grace` object is added.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the `Grace` object in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the `Grace` object.
+#'
+#' @param slash Optional. A single logical, which indicates if there is
+#' a slash symbol on the grace note or chord. The default value is `TRUE`.
+#'
+#' @returns A list of class `Grace`.
+#'
+#' @seealso [gm::+.Music()] for adding a `Grace` object to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a `Grace`
+#' grace <- Grace(1)
+#' grace
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4")) + grace
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Grace <- function(i, to = NULL, slash = NULL) {
   # Validation
   check_to(to)
