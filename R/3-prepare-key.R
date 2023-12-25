94ada04f523562e4032d002d69357a4c24678a1e
flujoo <tnerrotana@gmail.com>
2023-12-25 17:23:36 +0100

    Modify code

--- R/3-prepare-key.R
+++ R/3-prepare-key.R
@@ -1,5 +1,5 @@
 add_global_key <- function(music) {
-  # check if there is already a global Key
+  # Check if there is already a global Key
   keys <- music$keys
   global_key <- keys[is.na(keys$line) & keys$bar == 1, ]
   if (NROW(global_key) != 0) return(music)
