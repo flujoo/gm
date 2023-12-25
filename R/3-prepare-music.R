d634494dcbb57f4e59f2a273c05d1077de4b442a
flujoo <tnerrotana@gmail.com>
2023-12-25 17:40:29 +0100

    Document `fill_empty_music()`

--- R/3-prepare-music.R
+++ R/3-prepare-music.R
@@ -1,3 +1,9 @@
+#' Add Rest to Empty Music
+#'
+#' Add a rest to an empty music. The value of the meter at the first bar
+#' will be the length of the rest.
+#'
+#' @noRd
 fill_empty_music <- function(music) {
   if (!is.null(music$lines)) return(music)
 
