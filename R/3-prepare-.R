a82c3bfe8af1148efd529dc4be2406fb83b63a98
flujoo <tnerrotana@gmail.com>
2023-12-25 16:40:15 +0100

    Move code

--- R/3-prepare-.R
+++ R/3-prepare-.R
@@ -16,12 +16,6 @@ prepare <- function(music) {
 }
 
 
-initialize_first_bar_meter <- function(music) {
-  if (1 %in% music$meters$bar) return(music)
-  music + Meter(4, 4)
-}
-
-
 fill_empty_music <- function(music) {
   meters <- music$meters
   first_bar_meter <- meters[meters$bar == 1, ]
---
+++ R/3-prepare-meter.R
@@ -0,0 +1,4 @@
+initialize_first_bar_meter <- function(music) {
+  if (1 %in% music$meters$bar) return(music)
+  music + Meter(4, 4)
+}
