f6c6129839bec6314acd7a95202c3f2d76ed0184
flujoo <tnerrotana@gmail.com>
2024-01-06 14:27:21 +0100

    Add `is_separate()`

---
+++ R/3-utils-segment.R
@@ -0,0 +1,10 @@
+#' Check If Top Segment Is Separate From Bottom Segment
+#' @noRd
+is_separate <- function(top, bottom) {
+  any(
+    top$start_bar > bottom$end_bar,
+    top$start_bar == bottom$end_bar && top$start_offset >= bottom$end_offset,
+    top$end_bar < bottom$start_bar,
+    top$end_bar == bottom$start_bar && top$end_offset <= bottom$start_offset
+  )
+}
