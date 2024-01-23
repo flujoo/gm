3d2442dd2e76e4df4a4e2fc6aea6491f8fdeb6b5
flujoo <tnerrotana@gmail.com>
2024-01-23 18:09:36 +0100

    Document `Lyric()`

--- R/1-construct-lyric.R
+++ R/1-construct-lyric.R
@@ -1,4 +1,52 @@
+#' Create `Lyric` Object
+#'
+#' Create `Lyric` objects to represent lyrics.
+#'
+#' @param text A character vector. Usually, the length of `text` should
+#' be 1, and it represents some word of the lyrics. If the length is
+#' larger than 1, the words will be connected with
+#' [elision slurs](https://musescore.org/en/handbook/4/lyrics#elision-slur).
+#'
+#' @param i A single positive integer, which represents the position
+#' of the `Lyric` in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the `Lyric`.
+#'
+#' @param special Optional. A single character which can be `"-"` and
+#' `"_"`.
+#'
+#' - With `"-"`, the `Lyric` will be treated as a syllable, and will
+#' be connected with the next `Lyric`. See
+#' [MuseScore](https://musescore.org/en/handbook/4/lyrics#enter-syllables).
+#'
+#' - With `"_"`, the `Lyric` will be treated as a
+#' [melisma](https://musescore.org/en/handbook/4/lyrics#enter-melisma).
+#'
+#' @param layer Optional. A positive integer which indicates the layer
+#' where to add the `Lyric`. The default value is 1.
+#'
+#' @returns A list of class `Lyric`.
+#'
+#' @seealso [gm::+.Music()] for adding a `Lyric` to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create two syllables
+#' syllable_1 <- Lyric("He", 1, special = "-")
+#' syllable_2 <- Lyric("llo", 2)
+#' syllable_1
+#' syllable_2
+#'
+#' # Add them to a `Music`
+#' music <- Music() + Line(c("C4", "D4")) + syllable_1 + syllable_2
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Lyric <- function(text, i, to = NULL, special = NULL, layer = NULL) {
   # Validation
   erify::check_type(text, "character")
