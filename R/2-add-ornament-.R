499be5acb1231db26c6ff02221f4900a3f42ef74
flujoo <tnerrotana@gmail.com>
2024-02-11 18:40:19 +0100

    Modify code

--- R/2-add-ornament-.R
+++ R/2-add-ornament-.R
@@ -4,17 +4,17 @@ add_ornament <- function(object, music) {
   lines <- music$lines
   notes <- music$notes
 
-  # validation
+  # Validation
   check_add_to(to, lines, object)
   line <- normalize_to(to, lines)
   check_i(i, line, notes)
   check_i_rest(object, line, notes)
 
-  # normalization
+  # Normalization
   names(object)[names(object) == "to"] <- "line"
   object$line <- line
 
-  # construction
+  # Construction
   update_ornaments(music, object)
 }
 
@@ -53,15 +53,15 @@ remove_ornaments <- function(music, ornament) {
   line <- ornament$line
   i <- ornament$i
 
-  # the names of the existing types of ornaments in the Music
+  # Existing ornament types in the Music
   names <- intersect(
     names(music),
     c("trills", "turns", "mordents", "tremolos", "schleifers")
   )
 
-  # remove the ornaments that have the same location
+  # Remove the ornaments that have the same location
   for (name in names) {
-    # let `update_trills()` do the job
+    # Let `update_trills()` do the job
     if (name == "trills" && inherits(ornament, "Trill")) next
 
     ornaments <- music[[name]]
@@ -69,7 +69,7 @@ remove_ornaments <- function(music, ornament) {
     is <- ornaments$i
     to_remove <- ornaments$line == line & is == i
 
-    # only "point" ornaments are dealt with
+    # Only "point" ornaments are dealt with
     if (name == "trills") {
       js <- ornaments$j
       to_remove <- to_remove & (is.na(js) | js == is)
