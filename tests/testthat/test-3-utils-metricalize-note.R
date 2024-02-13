6263a9d924ad99dae3a127afe77a1c05aa79f8ec
flujoo <tnerrotana@gmail.com>
2024-02-13 20:17:26 +0100

    Test `metricalize_note()`

---
+++ tests/testthat/test-3-utils-metricalize-note.R
@@ -0,0 +1,23 @@
+test_that("note metricalization works", {
+  music <- Music() + Line("C4", "whole", bar = 2, offset = 2.5) + Meter(3, 4)
+  music <- locate_notes(music)
+  note <- music$notes
+  meters <- music$meters
+  out <- metricalize_note(note, meters)
+
+  expected <- data_frame(
+    line = rep(1L, 3),
+    i = c(1L, NA, NA),
+    j = rep(NA_integer_, 3),
+    pitch = rep("C4", 3),
+    midi = rep(60L, 3),
+    duration = rep(NA_character_, 3),
+    length = c(0.5, 3, 0.5),
+    start_bar = c(2L, 3L, 4L),
+    start_offset = c(2.5, 0, 0),
+    end_bar = c(2L, 3L, 4L),
+    end_offset = c(3, 3, 0.5)
+  )
+
+  expect_identical(out, expected)
+})
