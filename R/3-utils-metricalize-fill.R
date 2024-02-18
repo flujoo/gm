2385f84933f40346a28d50c54c30c3da316c46b0
flujoo <tnerrotana@gmail.com>
2024-02-18 21:35:42 +0100

    Add `fill_line()`

--- R/3-utils-metricalize-fill.R
+++ R/3-utils-metricalize-fill.R
@@ -1,3 +1,81 @@
+#' Fill Gaps Before and After Line
+#' @noRd
+fill_line <- function(line, i, lines, meters) {
+  # Do not fill the gaps around a voice
+  if (line[["voice"]] > 1) return(list(before = NULL, after = NULL))
+
+  line_start_bar <- line[["start_bar"]]
+  line_start_offset <- line[["start_offset"]]
+
+  line_end_bar <- line[["end_bar"]]
+  line_end_offset <- line[["end_offset"]]
+
+  line_line <- line[["line"]]
+  location <- c(line[["part"]], line[["staff"]], line[["voice"]])
+
+  # If current segment is related to the segment before it
+  is_before_related <- if (i == 1) {
+    FALSE
+
+  } else {
+    line_before <- line[i - 1, ]
+
+    location_before <- c(
+      line_before[["part"]], line_before[["staff"]], line_before[["voice"]]
+    )
+
+    all(location == location_before)
+  }
+
+  if (is_before_related) {
+    start_bar <- line_before[["end_bar"]]
+    start_offset <- line_before[["end_offset"]]
+
+  } else {
+    start_bar <- 1L
+    start_offset <- 0
+  }
+
+  before <- fill_gap(
+    start_bar, start_offset,
+    line_start_bar, line_start_offset,
+    meters, line_line
+  )
+
+  # If current segment is related to the segment after it
+  is_after_related <- if (i == NROW(lines)) {
+    FALSE
+
+  } else {
+    line_after <- line[i + 1, ]
+
+    location_before <- c(
+      line_after[["part"]], line_after[["staff"]], line_after[["voice"]]
+    )
+
+    all(location == location_after)
+  }
+
+  if (is_after_related) {
+    # The gap will be filled when the next segment is dealt with
+    end_bar <- line_end_bar
+    end_offset <- line_end_offset
+
+  } else {
+    end_bar <- max(lines[["end_bar"]]) + 1
+    end_offset <- 0
+  }
+
+  after <- fill_gap(
+    line_end_bar, line_end_offset,
+    end_bar, end_offset,
+    meters, line_line
+  )
+
+  list(before = before, after = after)
+}
+
+
 #' Fill Gap Between Start and End Positions with Metricalized Rests
 #' @noRd
 fill_gap <- function(
