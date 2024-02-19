59f25e06495aad58f12a7332100fabacc5bb94bd
flujoo <tnerrotana@gmail.com>
2024-02-19 22:03:11 +0100

    Test `metricalize()`

---
+++ tests/testthat/test-3-utils-metricalize-.R
@@ -0,0 +1,105 @@
+test_that("chord metricalization works", {
+  music <-
+    Music() +
+    Meter(3, 4) +
+
+    Line(
+      list(90, 93:91, 94, 95:96),
+      list(1, "w", 4, 4),
+      bar = 2, offset = 0.5
+    ) +
+
+    Grace(1)
+
+  music <- locate_notes(music)
+  music <- locate_lines(music)
+  music <- group_tuplets(music)
+  music <- sort_lines(music)
+  music <- metricalize(music)
+
+  out <- music[["notes"]]
+
+  expected <- data_frame(
+    line = rep(1L, 18),
+
+    i = as.integer(c(
+      NA, NA, # Fillings before the Line
+      1, # Grace note
+      rep(2, 3), rep(NA, 3), # First chord
+      3, NA, # Note
+      rep(4, 2), rep(NA, 4), # Second chord
+      NA # Fillings after the Line
+    )),
+
+    j = as.integer(c(
+      NA, NA,
+      NA,
+      1:3, rep(NA, 3),
+      NA, NA,
+      1:2, rep(NA, 4),
+      NA
+    )),
+
+    pitch = rep(NA_character_, 18),
+
+    midi = as.integer(c(
+      NA, NA,
+      90,
+      rep(93:91, 2),
+      rep(94, 2),
+      rep(95:96, 3),
+      NA
+    )),
+
+    duration = rep(NA_character_, 18),
+
+    length = c(
+      3, 0.5,
+      1,
+      rep(2.5, 3), rep(1.5, 3),
+      1.5, 2.5,
+      rep(0.5, 2), rep(3, 2), rep(0.5, 2),
+      2.5
+    ),
+
+    start_bar = as.integer(c(
+      1, 2,
+      NA,
+      rep(2, 3), rep(3, 3),
+      3, 4,
+      rep(4, 2), rep(5, 2), rep(6, 2),
+      6
+    )),
+
+    start_offset = c(
+      0, 0,
+      NA,
+      rep(0.5, 3), rep(0, 3),
+      1.5, 0,
+      rep(2.5, 2), rep(0, 2), rep(0, 2),
+      0.5
+    ),
+
+    end_bar = as.integer(c(
+      1, 2,
+      NA,
+      rep(2, 3), rep(3, 3),
+      3, 4,
+      rep(4, 2), rep(5, 2), rep(6, 2),
+      6
+    )),
+
+    end_offset = c(
+      3, 0.5,
+      NA,
+      rep(3, 3), rep(1.5, 3),
+      3, 2.5,
+      rep(3, 2), rep(3, 2), rep(0.5, 2),
+      3
+    ),
+
+    group = rep(0L, 18)
+  )
+
+  expect_identical(out, expected)
+})
