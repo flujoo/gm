e38d15f9105ad86e5c035edd8493c469bf1757e7
flujoo <tnerrotana@gmail.com>
2024-02-26 18:15:51 +0100

    Add `MusicXML()` and functions

---
+++ R/4-musicxml-.R
@@ -0,0 +1,77 @@
+MusicXML <- function(tag, contents = NULL, attributes = NULL) {
+  structure(
+    list(
+      tag = tag,
+      contents = contents,
+      attributes = attributes
+    ),
+
+    class = "MusicXML"
+  )
+}
+
+
+#' @keywords internal
+#' @export
+print.MusicXML <- function(x, ...) {
+  cat(to_string(x), "\n")
+}
+
+
+#' @keywords internal
+#' @export
+to_string.MusicXML <- function(x, ...) {
+  tab <- strrep(" ", 2)
+  fragments <- to_fragments(x)
+  paste(prepend_tab(fragments, tab), collapse = "\n")
+}
+
+
+to_fragments <- function(musicxml) {
+  tag <- musicxml[["tag"]]
+  contents <- musicxml[["contents"]]
+  attributes <- musicxml[["attributes"]]
+
+  attributes <- if (is.null(attributes)) {
+    ""
+  } else {
+    paste0(" ", names(attributes), '="', attributes, '"', collapse = "")
+  }
+
+  if (is.null(contents)) {
+    paste0("<", tag, attributes, "/>")
+
+  } else if (is.atomic(contents)) {
+    paste0("<", tag, attributes, ">", contents, "</", tag, ">")
+
+  } else if (inherits(contents, "MusicXML")) {
+    list(
+      paste0("<", tag, attributes, ">"),
+      to_fragments(contents),
+      paste0("</", tag, ">")
+    )
+
+  } else if (is.list(contents)) {
+    append(
+      list(paste0("<", tag, attributes, ">"), paste0("</", tag, ">")),
+      lapply(contents, to_fragments),
+      after = 1
+    )
+  }
+}
+
+
+prepend_tab <- function(fragments, tab) {
+  if (is.character(fragments)) return(fragments)
+
+  l <- length(fragments)
+  contents <- list()
+
+  for (i in 2:(l - 1)) {
+    element <- paste0(tab, prepend_tab(fragments[[i]], tab))
+    contents <- c(contents, as.list(element))
+  }
+
+  # Insert contents between tags
+  append(list(fragments[[1]], fragments[[l]]), contents, after = 1)
+}
