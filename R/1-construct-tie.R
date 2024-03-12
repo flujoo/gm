a537d6e7927ff95a5d913f8a91e8baa3a81ce398
flujoo <tnerrotana@gmail.com>
2024-03-12 21:17:54 +0100

    Document `Tie()`

--- R/1-construct-tie.R
+++ R/1-construct-tie.R
@@ -1,4 +1,39 @@
+#' Create `Tie` Object
+#'
+#' Create a `Tie` to tie some notes together.
+#'
+#' @param i A single positive integer, which represents the start position
+#' of the tie in a musical line.
+#'
+#' @param j Optional. A single positive integer, which represents the
+#' start position of the tie in a chord. If not provided, all notes in the
+#' chords that have equivalent pitches are tied.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the tie.
+#'
+#' @param above Optional. A single logical, which indicates if the tie is
+#' placed above the notes. By default, the position is decided by MuseScore.
+#'
+#' @returns A list of class `Tie`.
+#'
+#' @seealso [gm::+.Music()] for adding a tie to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a tie
+#' tie <- Tie(1)
+#' tie
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "C4")) + tie
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Tie <- function(i, j = NULL, to = NULL, above = NULL) {
   # Validation
   erify::check_n(i)
