2ae330e0c48a350fb43696d46893647ebe53bba3
flujoo <tnerrotana@gmail.com>
2024-03-15 22:01:30 +0100

    Document `Breath()`

--- R/1-construct-breath.R
+++ R/1-construct-breath.R
@@ -1,3 +1,37 @@
+#' Create `Breath` Object
+#'
+#' Create a `Breath` object to represent a breath mark.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the breath mark in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the breath mark.
+#'
+#' @param symbol Optional. A single character which can be `"comma"`,
+#' `"tick"`, `"upbow"`, and `"salzedo"`. It represents the symbol
+#' used for the breath mark. The default symbol is `"comma"`. See
+#' [the MusicXML specification](`r to_url("data-types/breath-mark-value/")`).
+#'
+#' @returns A list of class `Breath`.
+#'
+#' @seealso [gm::+.Music()] for adding a breath mark to a `Music` object.
+#'
+#' @export
+#'
+#' @examples
+#' # Create a breath mark
+#' breath <- Breath(1)
+#' breath
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4")) + breath
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Breath <- function(i, to = NULL, symbol = NULL) {
   # Validation
   erify::check_n(i)
