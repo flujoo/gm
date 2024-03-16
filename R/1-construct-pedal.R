4ccb7296261e6dca352a539ae4b132d0c7790ff1
flujoo <tnerrotana@gmail.com>
2024-03-16 20:57:56 +0100

    Document `Pedal()`

--- R/1-construct-pedal.R
+++ R/1-construct-pedal.R
@@ -1,4 +1,32 @@
+#' Create `Pedal` Object
+#'
+#' Create a `Pedal` object to represent piano sustain pedal marks.
+#'
+#' @param i,j A single positive integer. They indicate the start
+#' and end position of the `Pedal` object in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the `Pedal` object.
+#'
+#' @returns A list of class `Pedal`.
+#'
+#' @seealso [gm::+.Music()] for adding a `Pedal` to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a `Pedal`
+#' pedal <- Pedal(1, 3)
+#' pedal
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4", "E4")) + pedal
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Pedal <- function(i, j, to = NULL) {
   # Validation
   check_to(to)
