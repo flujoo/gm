2b5fc36d0777e35e36cc3d54c89a29b6cbb76dbc
flujoo <tnerrotana@gmail.com>
2024-03-16 20:46:12 +0100

    Modify code

--- R/1-construct-pedal.R
+++ R/1-construct-pedal.R
@@ -1,23 +1,24 @@
 #' @export
 Pedal <- function(i, j, to = NULL) {
-  # validation
+  # Validation
   check_to(to)
   erify::check_n(i)
   erify::check_n(j)
 
-  # normalization
+  # Normalization
   i <- as.integer(i)
   j <- as.integer(j)
 
-  # construction
-  pedal <- list(to = to, i = i, j = j)
-  class(pedal) <- "Pedal"
-  pedal
+  # Construction
+  structure(
+    list(to = to, i = i, j = j),
+    class = "Pedal"
+  )
 }
 
 
 #' @export
 print.Pedal <- function(x, ...) {
   cat("Pedal", "\n\n")
-  print_to_i_j(x$to, x$i, x$j, line = TRUE)
+  print_to_i_j(x[["to"]], x[["i"]], x[["j"]], line = TRUE)
 }
--- R/2-add-pedal.R
+++ R/2-add-pedal.R
@@ -1,27 +1,27 @@
 #' @keywords internal
 #' @export
 add.Pedal <- function(object, music) {
-  to <- object$to
-  i <- object$i
-  j <- object$j
-  lines <- music$lines
-  notes <- music$notes
+  to <- object[["to"]]
+  i <- object[["i"]]
+  j <- object[["j"]]
+  lines <- music[["lines"]]
+  notes <- music[["notes"]]
 
-  # validation
+  # Validation
   check_add_to(to, lines, object)
   line <- normalize_to(to, lines)
   check_i(i, line, notes)
   check_i(j, line, notes)
 
-  # normalization
+  # Normalization
   names(object)[names(object) == "to"] <- "line"
-  object$line <- line
+  object[["line"]] <- line
 
   . <- sort(c(i, j))
-  object$i <- .[1]
-  object$j <- .[2]
+  object[["i"]] <- .[1]
+  object[["j"]] <- .[2]
 
-  # construction
-  music$pedals <- update_2d(music$pedals, object)
+  # Construction
+  music[["pedals"]] <- update_2d(music[["pedals"]], object)
   music
 }
