0c8c60aa228842e226cfddd98a22e195607edda1
flujoo <tnerrotana@gmail.com>
2024-03-18 18:44:04 +0100

    Modify code

--- R/3-utils-key.R
+++ R/3-utils-key.R
@@ -1,7 +1,4 @@
-#' Add Global C Major at First Bar
-#' @noRd
 add_global_key <- function(music) {
-  # Check if there is already a global Key
   keys <- music[["keys"]]
   global_key <- keys[is.na(keys[["line"]]) & keys[["bar"]] == 1, ]
   if (NROW(global_key) != 0) return(music)
