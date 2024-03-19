2c0b22e9d583c77583ade0be8602e05161cb85ab
flujoo <tnerrotana@gmail.com>
2024-03-19 20:21:47 +0100

    Add `specify_keys()`

--- R/3-utils-key.R
+++ R/3-utils-key.R
@@ -19,3 +19,49 @@ locate_keys <- function(keys, lines) {
 
   keys[, c("part", "staff", "bar", "key")]
 }
+
+
+#' Make Key for Each Staff Explicit
+#'
+#' @returns For each staff, the Keys are reverse-ordered on `bar`.
+#'
+#' @noRd
+specify_keys <- function(keys, lines) {
+  specified <- NULL
+
+  parts <- keys[["part"]]
+  staffs <- keys[["staff"]]
+  bars <- keys[["bar"]]
+
+  locations <- unique(lines[, c("part", "staff")])
+
+  for (k in seq_len(NROW(locations))) {
+    location <- locations[k, ]
+    part <- location[["part"]]
+    staff <- location[["staff"]]
+
+    # If a key is assigned to the staff, part, or score
+    to_staff <- parts == part & staffs == staff
+    to_part <- parts == part & staffs == 0
+    to_score <- parts == 0
+
+    # Local keys are more dominant
+    if (any(to_staff)) {
+      . <- keys[to_staff, ]
+      to_part <- to_part & bars < min(.[["bar"]])
+      to_score <- to_score & bars < min(.[["bar"]])
+    }
+
+    if (any(to_part)) {
+      to_score <- to_score & bars < min(keys[to_part, ][["bar"]])
+    }
+
+    specified_k <- keys[to_staff | to_part | to_score, ]
+    specified_k[["part"]] <- part
+    specified_k[["staff"]] <- staff
+    specified <- rbind(specified, specified_k)
+  }
+
+  . <- order(specified[["part"]], specified[["staff"]], -specified[["bar"]])
+  specified[., ]
+}
