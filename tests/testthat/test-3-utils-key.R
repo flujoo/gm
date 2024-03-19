dcfc12a30adca84f39bd5188070007e624e88641
flujoo <tnerrotana@gmail.com>
2024-03-19 20:40:43 +0100

    Test `prepare_keys()`

---
+++ tests/testthat/test-3-utils-key.R
@@ -0,0 +1,23 @@
+test_that("key preparation works", {
+  music <-
+    Music() +
+    Meter(4, 4) +
+    Line(90) +
+    Line(91, as = "staff") +
+    Line(92) +
+    Key(1, bar = 3) +
+    Key(2, to = 2, scope = "part") +
+    Key(3, bar = 4, to = 2, scope = "staff") +
+    Key(4, to = 3)
+
+  out <- as.data.frame(prepare_keys(music))
+
+  expected <- data.frame(
+    part = c(1L, 1L, 1L, 2L),
+    staff = c(1L, 2L, 2L, 1L),
+    bar = c(1L, 4L, 1L, 1L),
+    key = c(2L, 3L, 2L, 4L)
+  )
+
+  expect_identical(out, expected)
+})
