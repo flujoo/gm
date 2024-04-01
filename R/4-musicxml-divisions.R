8cbd3d95750962a8742c2247a418de8a5552695b
flujoo <tnerrotana@gmail.com>
2024-04-01 22:03:29 +0200

    Modify `to_fraction_type()`

--- R/4-musicxml-divisions.R
+++ R/4-musicxml-divisions.R
@@ -1,17 +1,13 @@
 to_fraction_type <- function(type) {
-  fraction <- c(1, 1)
-
   k <- which(type == duration_types[["name"]])
   value <- duration_types[["value"]][k]
 
-  if (k < 6) {
-    fraction[1] <- value
+  if (k <= 6) {
+    c(value, 1)
 
-  } else if (k > 6) {
-    fraction[2] <- 1 / value
+  } else {
+    c(1, 1/value)
   }
-
-  fraction
 }
 
 
