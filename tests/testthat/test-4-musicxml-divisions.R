4814400bd148ae98156f81a3566c852e13798294
flujoo <tnerrotana@gmail.com>
2024-04-09 18:51:20 +0200

    Test `infer_divisions()`

---
+++ tests/testthat/test-4-musicxml-divisions.R
@@ -0,0 +1,20 @@
+test_that("inferring divisions works", {
+  music <-
+    Music() +
+    Line(durations = c("q/3", "h/5", "q/7")) +
+    Meter(1, 8) +
+    Meter(3, 16, bar = 3)
+
+  notes <- music[["notes"]]
+  meters <- music[["meters"]]
+
+  notes[["measure_rest"]] <- c(FALSE, FALSE, TRUE)
+  out <- infer_divisions(notes, meters)
+  expected <- 60
+  expect_identical(out, expected)
+
+  notes[["measure_rest"]] <- c(FALSE, FALSE, FALSE)
+  out <- infer_divisions(notes, meters)
+  expected <- 420
+  expect_identical(out, expected)
+})
