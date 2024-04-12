8ff46f6ef53da7f6afc17ef9dbacde530d42db52
flujoo <tnerrotana@gmail.com>
2024-04-12 19:16:53 +0200

    Document `Stem()`

--- R/1-construct-stem.R
+++ R/1-construct-stem.R
@@ -1,4 +1,36 @@
+#' Create `Stem` Object
+#'
+#' Create a `Stem` object to modify the stem of some note.
+#'
+#' @param direction A single character, which can be `"down"`, `"up"`,
+#' `"double"`, and `"none"`. See
+#' [the MusicXML specification](`r to_url("data-types/stem-value/")`).
+#'
+#' @param i A single positive integer, which represents the position
+#' of the stem in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to modify the stem.
+#'
+#' @returns A list of class `Stem`.
+#'
+#' @seealso [gm::+.Music()] for adding a `Stem` to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a `Stem`
+#' stem <- Stem("double", 1)
+#' stem
+#'
+#' # Add a `Stem` to a `Music`
+#' music <- Music() + Line(c("C4", "D4")) + stem
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Stem <- function(direction, i, to = NULL) {
   # Validation
   erify::check_content(direction, c("down", "up", "double", "none"))
