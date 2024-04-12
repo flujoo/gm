8ee20450e61fd6f7f7d8a8c7ce1ba8332bf961ee
flujoo <tnerrotana@gmail.com>
2024-04-12 19:15:48 +0200

    Modify code

--- R/1-construct-tempo.R
+++ R/1-construct-tempo.R
@@ -1,11 +1,13 @@
 #' @export
-Tempo <- function(tempo,
-                  unit = NULL,
-                  bar = NULL,
-                  offset = NULL,
-                  marking = NULL,
-                  invisible = NULL) {
-  # validation
+Tempo <- function(
+    tempo,
+    unit = NULL,
+    bar = NULL,
+    offset = NULL,
+    marking = NULL,
+    invisible = NULL) {
+
+  # Validation
   erify::check_positive(tempo)
   check_tempo_unit(unit)
   if (!is.null(bar)) erify::check_n(bar)
@@ -13,29 +15,31 @@ Tempo <- function(tempo,
   if (!is.null(marking)) erify::check_string(marking)
   if (!is.null(invisible)) erify::check_bool(invisible)
 
-  # normalization
+  # Normalization
   tempo <- as.double(tempo)
 
   . <- normalize_tempo_unit(unit, tempo, marking)
-  unit <- .$unit
-  bpm <- .$bpm
+  unit <- .[["unit"]]
+  bpm <- .[["bpm"]]
 
   if (!is.null(bar)) bar <- as.integer(bar)
   if (!is.null(offset)) offset <- as.double(offset)
   if (is.null(marking)) marking <- NA_character_
 
-  # construction
-  tempo <- list(
-    bar = bar,
-    offset = offset,
-    tempo = tempo,
-    unit = unit,
-    bpm = bpm,
-    marking = marking,
-    invisible = invisible
+  # Construction
+  structure(
+    list(
+      bar = bar,
+      offset = offset,
+      tempo = tempo,
+      unit = unit,
+      bpm = bpm,
+      marking = marking,
+      invisible = invisible
+    ),
+
+    class = "Tempo"
   )
-  class(tempo) <- "Tempo"
-  tempo
 }
 
 
@@ -51,11 +55,11 @@ check_tempo_unit <- function(unit) {
   types <- c("half", "quarter", "eighth")
 
   if (is.numeric(unit)) {
-    values <- duration_types[duration_types$name %in% types, "value"]
+    values <- duration_types[duration_types[["name"]] %in% types, "value"]
     valid <- c(values, values * 1.5)
 
   } else {
-    abbrs <- duration_types[duration_types$name %in% types, "abbr"]
+    abbrs <- duration_types[duration_types[["name"]] %in% types, "abbr"]
     notations <- c(types, abbrs)
     valid <- c(notations, paste0(notations, "."))
   }
@@ -85,21 +89,24 @@ normalize_tempo_unit <- function(unit, tempo, marking) {
 
 #' @export
 print.Tempo <- function(x, ...) {
-  tempo <- x$tempo
-  unit <- x$unit
-  bpm <- x$bpm
-  bar <- x$bar
-  offset <- x$offset
-  marking <- x$marking
-  invisible <- x$invisible
+  tempo <- x[["tempo"]]
+  unit <- x[["unit"]]
+  bpm <- x[["bpm"]]
+  bar <- x[["bar"]]
+  offset <- x[["offset"]]
+  marking <- x[["marking"]]
+  invisible <- x[["invisible"]]
 
   s_tempo <- "Tempo"
   if (!is.na(marking)) s_tempo <- paste(s_tempo, marking)
   if (!is.na(unit)) s_tempo <- paste(s_tempo, unit, "=", bpm)
   cat(s_tempo, "\n")
 
-  enter <- (is.na(unit) && !is.na(marking)) || !is.null(bar) ||
-    !is.null(offset) || !is.null(invisible)
+  enter <-
+    (is.na(unit) && !is.na(marking)) ||
+    !is.null(bar) ||
+    !is.null(offset) ||
+    !is.null(invisible)
 
   if (enter) cat("\n")
 
--- R/2-add-tempo.R
+++ R/2-add-tempo.R
@@ -1,13 +1,13 @@
 #' @keywords internal
 #' @export
 add.Tempo <- function(object, music) {
-  # normalization
-  if (is.null(object$bar)) object$bar <- 1L
-  if (is.null(object$offset)) object$offset <- 0
-  if (is.null(object$invisible)) object$invisible <- FALSE
+  # Normalization
+  if (is.null(object[["bar"]])) object[["bar"]] <- 1L
+  if (is.null(object[["offset"]])) object[["offset"]] <- 0
+  if (is.null(object[["invisible"]])) object[["invisible"]] <- FALSE
 
-  # construction
-  music$tempos <- update_cases(music$tempos, object)
+  # Construction
+  music[["tempos"]] <- update_cases(music[["tempos"]], object)
   music
 }
 
@@ -15,5 +15,5 @@ add.Tempo <- function(object, music) {
 #' @keywords internal
 #' @export
 locate.Tempo <- function(object, ...) {
-  c(object$bar, object$offset)
+  c(object[["bar"]], object[["offset"]])
 }
