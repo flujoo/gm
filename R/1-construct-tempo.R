1480f2142021d16f8bd9dab89bad4b1c0a6f045c
flujoo <tnerrotana@gmail.com>
2024-04-13 10:45:28 +0200

    Redesign `Tempo()`

--- R/1-construct-tempo.R
+++ R/1-construct-tempo.R
@@ -4,120 +4,55 @@ Tempo <- function(
     unit = NULL,
     bar = NULL,
     offset = NULL,
-    marking = NULL,
-    invisible = NULL) {
+    marking = NULL) {
 
   # Validation
   erify::check_positive(tempo)
-  check_tempo_unit(unit)
+  deprecate_tempo_unit(unit)
   if (!is.null(bar)) erify::check_n(bar)
   check_offset(offset)
   if (!is.null(marking)) erify::check_string(marking)
-  if (!is.null(invisible)) erify::check_bool(invisible)
 
   # Normalization
   tempo <- as.double(tempo)
-
-  . <- normalize_tempo_unit(unit, tempo, marking)
-  unit <- .[["unit"]]
-  bpm <- .[["bpm"]]
-
   if (!is.null(bar)) bar <- as.integer(bar)
   if (!is.null(offset)) offset <- as.double(offset)
   if (is.null(marking)) marking <- NA_character_
 
   # Construction
   structure(
-    list(
-      bar = bar,
-      offset = offset,
-      tempo = tempo,
-      unit = unit,
-      bpm = bpm,
-      marking = marking,
-      invisible = invisible
-    ),
-
+    list(bar = bar, offset = offset, tempo = tempo, marking = marking),
     class = "Tempo"
   )
 }
 
 
-check_tempo_unit <- function(unit) {
+deprecate_tempo_unit <- function(unit) {
   if (is.null(unit)) return(invisible())
 
-  general <- paste(
-    "`unit` must be a duration notation or value",
-    "which represents the half, quarter, or eighth note,",
-    "or their corresponding single-dotted note."
+  warning(
+    "`unit` is deprecated. Use `marking` instead.", "\n",
+    call. = FALSE,
+    immediate. = TRUE
   )
-
-  types <- c("half", "quarter", "eighth")
-
-  if (is.numeric(unit)) {
-    values <- duration_types[duration_types[["name"]] %in% types, "value"]
-    valid <- c(values, values * 1.5)
-
-  } else {
-    abbrs <- duration_types[duration_types[["name"]] %in% types, "abbr"]
-    notations <- c(types, abbrs)
-    valid <- c(notations, paste0(notations, "."))
-  }
-
-  erify::check_content(unit, valid, NULL, general)
-}
-
-
-normalize_tempo_unit <- function(unit, tempo, marking) {
-  if (!is.null(unit)) {
-    . <- to_Duration(unit)
-    unit <- to_string(.)
-    bpm <- tempo / to_value(.)
-
-  } else if (is.null(marking)) {
-    unit <- "quarter"
-    bpm <- tempo
-
-  } else {
-    unit <- NA_character_
-    bpm <- NA_real_
-  }
-
-  list(unit = unit, bpm = bpm)
 }
 
 
 #' @export
 print.Tempo <- function(x, ...) {
-  tempo <- x[["tempo"]]
-  unit <- x[["unit"]]
-  bpm <- x[["bpm"]]
-  bar <- x[["bar"]]
-  offset <- x[["offset"]]
   marking <- x[["marking"]]
-  invisible <- x[["invisible"]]
+  tempo <- x[["tempo"]]
 
-  s_tempo <- "Tempo"
-  if (!is.na(marking)) s_tempo <- paste(s_tempo, marking)
-  if (!is.na(unit)) s_tempo <- paste(s_tempo, unit, "=", bpm)
-  cat(s_tempo, "\n")
+  cat("Tempo ")
 
-  enter <-
-    (is.na(unit) && !is.na(marking)) ||
-    !is.null(bar) ||
-    !is.null(offset) ||
-    !is.null(invisible)
+  if (is.na(marking)) {
+    cat("quarter", "=", tempo)
 
-  if (enter) cat("\n")
-
-  if (is.na(unit) && !is.na(marking)) {
-    cat("*", tempo, "quarter notes per minute", "\n")
+  } else {
+    cat(marking)
   }
 
-  print_bar_offset(bar, offset)
-
-  if (!is.null(invisible)) {
-    s_invisible <- if (invisible) "invisible" else "visible"
-    cat("* to be", s_invisible, "on the score", "\n")
-  }
+  cat("\n\n")
+  cat("*", tempo, "quarter notes per minute", "\n")
+  print_bar_offset(x[["bar"]], x[["offset"]])
 }
--- R/2-add-tempo.R
+++ R/2-add-tempo.R
@@ -4,7 +4,6 @@ add.Tempo <- function(object, music) {
   # Normalization
   if (is.null(object[["bar"]])) object[["bar"]] <- 1L
   if (is.null(object[["offset"]])) object[["offset"]] <- 0
-  if (is.null(object[["invisible"]])) object[["invisible"]] <- FALSE
 
   # Construction
   music[["tempos"]] <- update_cases(music[["tempos"]], object)
