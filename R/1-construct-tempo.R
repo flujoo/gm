e9c82fb40257d8ddfd205760e93e7c3b03663fd2
flujoo <tnerrotana@gmail.com>
2024-04-14 11:30:26 +0200

    Document `Tempo()`

--- R/1-construct-tempo.R
+++ R/1-construct-tempo.R
@@ -1,4 +1,63 @@
+#' Create `Tempo` Object
+#'
+#' Create a `Tempo` object to represent a tempo marking.
+#'
+#' The parameter `tempo` is used to specify the actual playback speed, while
+#' `marking` to represent the marking that appears on the score.
+#'
+#' Some examples:
+#'
+#' - `Tempo(50)`: the playback speed is 50 quarter notes per minute.
+#' A marking of "quarter = 50" will be added to the score.
+#'
+#' - `Tempo(50, marking = "Adagio")`: the playback speed is 50 quarter notes
+#' per minute, while the marking on the score is "Adagio".
+#'
+#' - `Tempo(50, marking = "Adagio half. = 20")`: the playback speed is
+#' 50 quarter notes per minute, while the marking on the score is
+#' "Adagio half. = 20".
+#'
+#' - `Tempo(50, marking = "Adagio (quarter = 45-80)")`: you can add a speed
+#' range and parentheses to the marking.
+#'
+#' - `Tempo(50, marking = "quarter. = quarter")`: you can also indicate
+#' metric modulations with `marking`.
+#'
+#' @param tempo A positive number, which indicates the number of quarter
+#' notes per minute.
+#'
+#' @param unit Deprecated. Was used to specify the beat unit.
+#' Please use `marking` instead.
+#'
+#' @param bar Optional. A positive integer, which indicates the number of
+#' the measure where to add the tempo. By default, it will be added at
+#' the first measure.
+#'
+#' @param offset Optional. A non-negative number, which indicates
+#' the tempo's position in a measure. The default value is `0`.
+#'
+#' @param marking Optional. A single character, which represents the marking
+#' that appears on the score. See the *Details* section.
+#'
+#' @return A list of class `Tempo`.
+#'
+#' @seealso [gm::+.Music()] for adding a tempo to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a tempo
+#' tempo <- Tempo(50, marking = "Adagio (half = 25)")
+#' tempo
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4", "E4", "F4")) + tempo
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Tempo <- function(
     tempo,
     unit = NULL,
