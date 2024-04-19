6544e976038934ccd33621ade9805de9bbf093eb
flujoo <tnerrotana@gmail.com>
2024-04-19 18:40:16 +0200

    Modify code

--- R/1-construct-tremolo.R
+++ R/1-construct-tremolo.R
@@ -1,34 +1,36 @@
 #' @export
 Tremolo <- function(number, i, to = NULL, between = NULL) {
-  # validation
+  # Validation
   erify::check_content(number, 1:4)
-  check_to(to)
   erify::check_n(i)
+  check_to(to)
   if (!is.null(between)) erify::check_bool(between)
 
-  # normalization
+  # Normalization
   number <- as.integer(number)
   i <- as.integer(i)
   if (is.null(between)) between <- FALSE
 
-  # construction
-  tremolo <- list(to = to, i = i, number = number, between = between)
-  class(tremolo) <- "Tremolo"
-  tremolo
+  # Construction
+  structure(
+    list(to = to, i = i, number = number, between = between),
+    class = "Tremolo"
+  )
 }
 
 
 #' @export
 print.Tremolo <- function(x, ...) {
-  number <- x$number
-  i <- x$i
+  number <- x[["number"]]
+  i <- x[["i"]]
 
   cat("Tremolo", "\n\n")
   cat("* with", number, if (number == 1) "stroke" else "strokes", "\n")
-  print_to_i_j(x$to)
+  print_to_i_j(x[["to"]])
 
-  if (x$between) {
+  if (x[["between"]]) {
     cat("* to be added between position", i, "and", i + 1, "\n")
+
   } else {
     cat("* to be added at position", i, "\n")
   }
