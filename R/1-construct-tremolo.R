16762b5c77d11eab4950cfd9c65b891fb963fe11
flujoo <tnerrotana@gmail.com>
2024-04-20 16:16:57 +0200

    Document `Tremolo()`

--- R/1-construct-tremolo.R
+++ R/1-construct-tremolo.R
@@ -1,4 +1,38 @@
+#' Create `Tremolo` Object
+#'
+#' Create a `Tremolo` object to represent a tremolo.
+#'
+#' @param number A single integer which can be `1`, `2`, `3`, and `4`.
+#' It indicates the speed of the tremolo.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the tremolo in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the tremolo.
+#'
+#' @param between Optional. A single logical which indicates if the
+#' tremolo is between notes.
+#'
+#' @return A list of class `Tremolo`.
+#'
+#' @seealso [gm::+.Music()] for adding a tremolo to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a tremolo
+#' tremolo <- Tremolo(3, 1, between = TRUE)
+#' tremolo
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Line(c("C4", "D4", "E4", "F4")) + tremolo
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Tremolo <- function(number, i, to = NULL, between = NULL) {
   # Validation
   erify::check_content(number, 1:4)
