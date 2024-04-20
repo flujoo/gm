f1143d666fdf89b95a930b71ba5c8daee71b9d83
flujoo <tnerrotana@gmail.com>
2024-04-20 17:23:18 +0200

    Document `Trill()`

--- R/1-construct-trill.R
+++ R/1-construct-trill.R
@@ -1,4 +1,38 @@
+#' Create `Trill` Object
+#'
+#' Create a `Trill` object to represent a trill ornament.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the trill in a musical line.
+#'
+#' @param j Optional. A single positive integer, which indicates the end
+#' position of the trill line in a musical line. If not provided, the trill
+#' will appear as a *tr* symbol above only the trilled note. Otherwise, it
+#' will appear as a *tr~~~* symbol above the notes between the start and
+#' end positions.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the trill.
+#'
+#' @returns A list of class `Trill`.
+#'
+#' @seealso [gm::+.Music()] for adding a trill to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a trill
+#' trill <- Trill(1, 3)
+#' trill
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Meter(4, 4) + Line(c("C4", "D4", "E4", "F4")) + trill
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Trill <- function(i, j = NULL, to = NULL) {
   # Validation
   erify::check_n(i)
