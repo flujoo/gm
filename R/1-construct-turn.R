9d2d202416e1cee1fe0d863fa96f99a3f5acf06b
flujoo <tnerrotana@gmail.com>
2024-04-20 16:56:22 +0200

    Modify code

--- R/1-construct-turn.R
+++ R/1-construct-turn.R
@@ -1,24 +1,25 @@
 #' @export
 Turn <- function(i, to = NULL, inverted = NULL) {
-  # validation
-  check_to(to)
+  # Validation
   erify::check_n(i)
+  check_to(to)
   if (!is.null(inverted)) erify::check_bool(inverted)
 
-  # normalization
+  # Normalization
   i <- as.integer(i)
   if (is.null(inverted)) inverted <- FALSE
 
-  # construction
-  turn <- list(to = to, i = i, inverted = inverted)
-  class(turn) <- "Turn"
-  turn
+  # Construction
+  structure(
+    list(to = to, i = i, inverted = inverted),
+    class = "Turn"
+  )
 }
 
 
 #' @export
 print.Turn <- function(x, ...) {
-  if (x$inverted) cat("Inverted ")
+  if (x[["inverted"]]) cat("Inverted ")
   cat("Turn", "\n\n")
-  print_to_i_j(x$to, x$i)
+  print_to_i_j(x[["to"]], x[["i"]])
 }
