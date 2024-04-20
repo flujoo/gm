598f1b16ea055c06b833c57f34a5fa418a167a6d
flujoo <tnerrotana@gmail.com>
2024-04-20 16:51:30 +0200

    Modify code

--- R/1-construct-trill.R
+++ R/1-construct-trill.R
@@ -1,31 +1,33 @@
 #' @export
 Trill <- function(i, j = NULL, to = NULL) {
-  # validation
-  check_to(to)
+  # Validation
   erify::check_n(i)
   if (!is.null(j)) erify::check_n(j)
+  check_to(to)
 
-  # normalization
+  # Normalization
   i <- as.integer(i)
   j <- if (!is.null(j)) as.integer(j) else NA_integer_
 
-  # construction
-  trill <- list(to = to, i = i, j = j)
-  class(trill) <- "Trill"
-  trill
+  # Construction
+  structure(
+    list(to = to, i = i, j = j),
+    class = "Trill"
+  )
 }
 
 
 #' @export
 print.Trill <- function(x, ...) {
-  i <- x$i
-  j <- x$j
+  i <- x[["i"]]
+  j <- x[["j"]]
 
   cat(if (is.na(j)) "Trill" else "Trill Line", "\n\n")
-  print_to_i_j(x$to)
+  print_to_i_j(x[["to"]])
 
   if (is.na(j) || j == i) {
     cat("* to be added at position", i, "\n")
+
   } else {
     cat("* from position", i, "to", j, "\n")
   }
