763a65f270a95dd2573f290ddc1eee35a9cffb4a
flujoo <tnerrotana@gmail.com>
2024-04-21 12:24:08 +0200

    Document `Turn()`

--- R/1-construct-turn.R
+++ R/1-construct-turn.R
@@ -1,4 +1,35 @@
+#' Create `Turn` Object
+#'
+#' Create a `Turn` object to represent a turn ornament.
+#'
+#' @param i A single positive integer, which represents the position
+#' of the turn in a musical line.
+#'
+#' @param to Optional. A single character or a single positive integer,
+#' which indicates the musical line where to add the turn.
+#'
+#' @param inverted Optional. A single logical, which indicates if it is
+#' an inverted turn. The default value is `FALSE`.
+#'
+#' @returns A list of class `Turn`.
+#'
+#' @seealso [gm::+.Music()] for adding a turn to a `Music` object.
+#'
 #' @export
+#'
+#' @examples
+#' # Create a turn
+#' turn <- Turn(1, inverted = TRUE)
+#' turn
+#'
+#' # Add it to a `Music`
+#' music <- Music() + Meter(4, 4) + Line(c("C4", "D4")) + turn
+#' music
+#'
+#' # Generate the music score
+#' if (interactive()) {
+#'   show(music)
+#' }
 Turn <- function(i, to = NULL, inverted = NULL) {
   # Validation
   erify::check_n(i)
