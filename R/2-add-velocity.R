575f858b4a9893dee7f1d0bde77c16aeaa8baa85
flujoo <tnerrotana@gmail.com>
2024-04-22 22:32:17 +0200

    Modify code

--- R/2-add-velocity.R
+++ R/2-add-velocity.R
@@ -1,29 +1,29 @@
 #' @keywords internal
 #' @export
 add.Velocity <- function(object, music) {
-  to <- object$to
-  lines <- music$lines
-  notes <- music$notes
+  to <- object[["to"]]
+  lines <- music[["lines"]]
+  notes <- music[["notes"]]
 
   # Validation
   check_add_to(to, lines)
 
   line <- normalize_to(to, lines)
-  i <- object$i
-  j <- object$j
+  i <- object[["i"]]
+  j <- object[["j"]]
 
   check_i(i, line, notes)
   check_j(j, line, i, notes)
 
   # Normalization
   names(object)[names(object) == "to"] <- "line"
-  object$line <- line
+  object[["line"]] <- line
 
   . <- notes[["line"]] == line & notes[["i"]] == i
   if (NROW(notes[., ]) == 1) object[["j"]] <- NA_integer_
 
   # Construction
-  music$velocities <- update_cases(music$velocities, object)
+  music[["velocities"]] <- update_cases(music[["velocities"]], object)
   music
 }
 
@@ -31,7 +31,7 @@ add.Velocity <- function(object, music) {
 #' @keywords internal
 #' @export
 locate.Velocity <- function(object, ...) {
-  location <- c(object$line, object$i, object$j)
+  location <- c(object[["line"]], object[["i"]], object[["j"]])
   location[is.na(location)] <- 0L
   location
 }
