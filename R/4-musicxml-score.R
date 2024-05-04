286af2b8162f231465e44403de43fb81641b4f91
flujoo <tnerrotana@gmail.com>
2024-05-04 22:28:28 +0200

    Add `to_MusicXML_part()`

--- R/4-musicxml-score.R
+++ R/4-musicxml-score.R
@@ -17,6 +17,39 @@ to_MusicXML_score <- function(lines, notes, meters) {
 }
 
 
+to_MusicXML_part <- function(part, divisions) {
+  contents <- list()
+
+  measure_numbers <- part[["start_bar"]]
+  last_measure_number <- max(measure_numbers)
+
+  for (measure_number in sort(unique(measure_numbers))) {
+    measure <- part[measure_numbers == measure_number, ]
+    musicxml_measure <- to_MusicXML_measure(measure)
+
+    if (measure_number == 1) {
+      staves <- length(unique(part[["staff"]]))
+      attributes <- Attributes(divisions = divisions, staves = staves)
+      musicxml_measure <- insert(attributes, musicxml_measure)
+    }
+
+    if (measure_number == last_measure_number) {
+      # Insert a Barline
+    }
+
+    contents <- c(contents, list(musicxml_measure))
+  }
+
+  id <- paste0("P", part[["part"]][1])
+  musicxml <- MusicXML("part", contents, list(id = id))
+
+  # Retain line numbers for inserting components
+  musicxml[["lines"]] <- unique(part[["line"]])
+
+  musicxml
+}
+
+
 to_MusicXML_part_list <- function(lines) {
   contents <- list()
   part_numbers <- lines[["part"]]
