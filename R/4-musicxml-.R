36b13f73c46b4c90a4ed8758d4f13a1be39661b3
flujoo <tnerrotana@gmail.com>
2024-06-03 20:49:04 +0200

    Move functions

---
+++ R/4-musicxml-.R
@@ -0,0 +1,8 @@
+to_MusicXML_backup <- function(duration) {
+  MusicXML("backup", MusicXML("duration", duration))
+}
+
+
+to_MusicXML_forward <- function(duration) {
+  MusicXML("forward", MusicXML("duration", duration))
+}
--- R/4-musicxml-score.R
+++ R/4-musicxml-score.R
@@ -124,13 +124,3 @@ to_MusicXML_part_list <- function(lines) {
 
   MusicXML("part-list", contents)
 }
-
-
-to_MusicXML_backup <- function(duration) {
-  MusicXML("backup", MusicXML("duration", duration))
-}
-
-
-to_MusicXML_forward <- function(duration) {
-  MusicXML("forward", MusicXML("duration", duration))
-}
