be3535283c5886a6359c9879dcc7e69079bb6af7
flujoo <tnerrotana@gmail.com>
2024-06-06 21:13:45 +0200

    Add `insert_attribute()`

--- R/4-musicxml-music.R
+++ R/4-musicxml-music.R
@@ -25,6 +25,39 @@ to_MusicXML.Music <- function(x, ...) {
 }
 
 
+#' Insert Attribute Object (Keys, Meters, ...) to `<score-partwise>`
+#' @param i The index of target `<part>`. For now, it's part number plus one.
+#' @noRd
+insert_attribute <- function(object, score, i, bar) {
+  measures <- score[["contents"]][[i]][["contents"]]
+  if (length(measures) < bar) return(score)
+
+  measure <- measures[[bar]]
+  notes <- measure[["contents"]]
+  first <- notes[[1]]
+
+  if (first[["tag"]] == "attributes") {
+    attributes <- first[["contents"]]
+    musicxml <- to_MusicXML(object)
+
+    score$contents[[i]]$contents[[bar]]$contents[[1]]$contents <- append(
+      attributes,
+      list(musicxml),
+      locate_attribute(musicxml[["tag"]], attributes)
+    )
+
+  } else {
+    score$contents[[i]]$contents[[bar]]$contents <- append(
+      notes,
+      list(to_MusicXML(Attributes(object))),
+      0
+    )
+  }
+
+  score
+}
+
+
 #' @description Elements in `<attributes>` should follow the order:
 #' `to_url("elements/attributes")`. Find the insertion position for the
 #' given tag. Initially, `<attributes>` contains only `<divisions>` and
