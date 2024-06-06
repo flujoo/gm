54697861376b3a7260ecda4281e760e704c09bd1
flujoo <tnerrotana@gmail.com>
2024-06-06 21:15:16 +0200

    Modify `insert.Key()`

--- R/4-musicxml-key.R
+++ R/4-musicxml-key.R
@@ -12,32 +12,5 @@ to_MusicXML.Key <- function(x, ...) {
 #' @keywords internal
 #' @export
 insert.Key <- function(x, to, ...) {
-  part <- x[["part"]]
-  bar <- x[["bar"]]
-
-  measures <- to[["contents"]][[part + 1]][["contents"]]
-  if (length(measures) < bar) return(to)
-
-  measure <- measures[[bar]]
-  notes <- measure[["contents"]]
-  first <- notes[[1]]
-
-  if (first[["tag"]] == "attributes") {
-    attributes <- first[["contents"]]
-
-    to$contents[[part + 1]]$contents[[bar]]$contents[[1]]$contents <- append(
-      attributes,
-      list(to_MusicXML(x)),
-      locate_attribute("key", attributes)
-    )
-
-  } else {
-    to$contents[[part + 1]]$contents[[bar]]$contents <- append(
-      notes,
-      list(to_MusicXML(Attributes(x))),
-      0
-    )
-  }
-
-  to
+  insert_attribute(x, to, x[["part"]] + 1, x[["bar"]])
 }
