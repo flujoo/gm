805d0318ba08248905b57458d98302a5bb602028
flujoo <tnerrotana@gmail.com>
2024-06-08 21:41:10 +0200

    Add `convert_musicxml()`

---
+++ R/5-musescore.R
@@ -0,0 +1,16 @@
+
+
+#' Call MuseScore to Convert MusicXML File
+#'
+#' @param from,to Input and output file paths.
+#' @param musescore MuseScore command line options.
+#'
+#' @noRd
+convert_musicxml <- function(from, to, musescore = NULL) {
+  system2(
+    options("gm.musescore_path"),
+    c(from, "-o", to, musescore),
+    stderr = NULL,
+    stdout = NULL
+  )
+}
