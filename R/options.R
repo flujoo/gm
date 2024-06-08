af8e8644617af4daa0d89be5f022ea6ebddfbd3c
flujoo <tnerrotana@gmail.com>
2024-06-08 11:28:36 +0200

    Create options.R

---
+++ R/options.R
@@ -0,0 +1,19 @@
+.onLoad <- function(libname, pkgname) {
+  settings <- list(
+    gm.musescore_path = NULL
+  )
+
+  to_set <- !names(settings) %in% names(options())
+  if (any(to_set)) options(settings[to_set])
+  invisible()
+}
+
+
+.onUnload <- function(libpath) {
+  settings <- list(
+    gm.musescore_path = NULL
+  )
+
+  options(settings)
+  invisible()
+}
