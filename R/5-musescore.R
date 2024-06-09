0d28a82969a8cc6d4356fe4d26ac52085c45a535
flujoo <tnerrotana@gmail.com>
2024-06-09 12:44:49 +0200

    Add `set_musescore_path()`

--- R/5-musescore.R
+++ R/5-musescore.R
@@ -1,3 +1,51 @@
+set_musescore_path <- function() {
+  if (!is.null(getOption("gm.musescore_path"))) return(invisible())
+  custom_path <- Sys.getenv("MUSESCORE_PATH")
+
+  if (custom_path != "" && file.exists(custom_path)) {
+    options(gm.musescore_path = custom_path)
+
+    # Create a Music to test the custom path
+    music <- Music() + Meter(1, 4) + Line(60)
+    music <- prepare(music)
+    musicxml <- to_MusicXML(music)
+    export(musicxml, tempdir(), "test", "png")
+
+    if (any(grepl("^test.png$", list.files(tempdir())))) {
+      return(invisible())
+
+    } else {
+      options(gm.musescore_path = NULL)
+    }
+  }
+
+  musescore_versions <- c(4, 3)
+
+  default_paths <- switch(
+    Sys.info()["sysname"],
+
+    Darwin = sapply(musescore_versions, function(version) sprintf(
+      "/Applications/MuseScore\ %s.app/Contents/MacOS/mscore",
+      version
+    )),
+
+    Windows = sapply(musescore_versions, function(version) sprintf(
+      "C:/Program Files/MuseScore %s/bin/MuseScore%s.exe",
+      version, version
+    )),
+
+    Linux = c("mscore", "musescore", "mscore4portable")
+  )
+
+  for (default_path in default_paths) {
+    if (file.exists(default_path)) {
+      options(gm.musescore_path = default_path)
+      return(invisible())
+    }
+  }
+
+  erify::throw("MuseScore is not found.")
+}
 
 
 #' Call MuseScore to Convert MusicXML File
