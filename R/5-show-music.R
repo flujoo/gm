2e50d2603e4d341c4e119da05de2f7585f202cd6
flujoo <tnerrotana@gmail.com>
2024-06-13 21:25:39 +0200

    Document `show.Music()`

---
+++ R/5-show-music.R
@@ -0,0 +1,34 @@
+#' Show `Music` Object
+#'
+#' Display a `Music` object as a music score or audio file.
+#'
+#' This function works in
+#'
+#' - RStudio
+#' - R Markdown files
+#' - Jupyter Notebooks
+#' - Shiny applications
+#' - R.app GUI
+#'
+#' @param x A `Music` object.
+#'
+#' @param to Optional. A character vector, which can be `"score"`,
+#' `"audio"`, or both. It specifies the output format. By default, both
+#' are displayed. You can change the default behavior by setting the
+#' `gm.show_to` option with `options()`.
+#'
+#' @param musescore Optional. A character vector, which represents
+#' the command line options passed to MuseScore. See
+#' [MuseScore command line usage](
+#' https://musescore.org/en/handbook/4/command-line-options) for details.
+#'
+#' @returns An invisible `NULL`. A music score or audio file will be
+#' displayed.
+#'
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'   music <- Music() + Meter(4, 4) + Line("C4")
+#'   show(music, musescore = "-r 800 -T 5")
+#' }
