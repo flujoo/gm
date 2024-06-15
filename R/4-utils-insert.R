d26748694b9f5dc7e4dccb87f76a63ddcfb342c3
flujoo <tnerrotana@gmail.com>
2024-06-15 12:59:52 +0200

    Document `locate_element()`

--- R/4-utils-insert.R
+++ R/4-utils-insert.R
@@ -5,6 +5,11 @@ insert <- function(x, to, ...) {
 }
 
 
+#' @description For elements like `<attributes>` and `<note>`, their
+#' child elements must follow some order. This is a general function
+#' for deciding where to insert a new element.
+#'
+#' @noRd
 locate_element <- function(tag, elements, tags) {
   before <- tags[seq_along(tags) <= which(tags == tag)]
 
