be8485bcc5ba46e2f2eec880f4ac1b67658dd0a1
flujoo <tnerrotana@gmail.com>
2024-06-29 20:44:06 +0200

    Add argument `type`

--- R/4-utils-insert-direction.R
+++ R/4-utils-insert-direction.R
@@ -1,15 +1,16 @@
-insert_direction <- function(object, score, scope) {
-  musicxml <- to_MusicXML(object)
+insert_direction <- function(object, score, scope, type = "start") {
+  musicxml <- to_MusicXML(object, type)
 
   for (location in locate_notes(object, score, scope)) {
     i <- location[1]
     j <- location[2]
     k <- location[3]
+    if (type == "start") k <- k - 1
 
     score$contents[[i]]$contents[[j]]$contents <- append(
       score$contents[[i]]$contents[[j]]$contents,
       list(musicxml),
-      k - 1
+      k
     )
   }
 
